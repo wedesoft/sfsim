0c5fe6c9d98f0fd939cd54a88996e4a8de5ead9b
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-09-23 21:58:09 +0100

    Add Leiningen project

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject sfsim25 "0.1.0-SNAPSHOT"
+  :description "Spaceflight Simulator Game"
+  :url "https://github.com/wedesoft/sfsim25"
+  :license {:name "GPL-3.0-or-later"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :main ^:skip-aot sfsim25.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
