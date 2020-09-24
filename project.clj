d577348f55b2dfa0f03306484a8930bcc91ca684
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-09-24 22:33:48 +0100

    Implemented Clojure program opening a window
    The window can be closed by pressing ESC.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot sfsim25.core
-  :resource-paths ["resources/jogamp-fat.jar"]
+  :resource-paths ["resources/jogamp-fat.jar"
+                   "/usr/share/java/gluegen2-rt.jar"]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/sfsim25/core.clj
+++ src/sfsim25/core.clj
@@ -1,7 +1,23 @@
 (ns sfsim25.core
+  (:import [com.jogamp.opengl GLProfile GLCapabilities]
+           [com.jogamp.newt.opengl GLWindow]
+           [com.jogamp.newt.event KeyListener KeyEvent])
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Main program opening a window"
   [& args]
-  (println "Hello, World!"))
+  (let [gl-capabilities (-> GLProfile/GL3 GLProfile/get GLCapabilities.)
+        window          (GLWindow/create gl-capabilities)
+        finished        (ref false)
+        key-listener    (reify KeyListener
+                          (keyPressed [this event]
+                            (if (= (.getKeyCode event) KeyEvent/VK_ESCAPE)
+                              (dosync (ref-set finished true)))))]
+    (doto window
+      (.setTitle "sfsim25")
+      (.setSize 640 480)
+      (.setVisible true)
+      (.addKeyListener key-listener))
+    (while (not @finished))
+    (.destroy window)))
