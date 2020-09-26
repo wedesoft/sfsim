b62fb87274fb1e797cbfca5b07a09d3dc98696cd
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-09-26 21:38:32 +0100

    Create separate entry point

--- project.clj
+++ project.clj
@@ -4,8 +4,10 @@
   :license {:name "GPL-3.0-or-later"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
-  :main ^:skip-aot sfsim25.core
+  :profiles {:run-sfsim25 {:main sfsim25.core}
+             :uberjar {:aot :all}}
+  :aliases {"run-sfsim25" ["with-profile" "run-sfsim25" "run"]}
   :resource-paths ["resources/jogamp-fat.jar"
-                   "/usr/share/java/gluegen2-rt.jar"]
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+                   "/usr/share/java/gluegen2-rt.jar"
+                   "/usr/share/java/jmagick.jar"]
+  :target-path "target/%s")
