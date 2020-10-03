2bccde3123a662dbc3cad959f15f4f1e94b7b74a
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-10-03 23:40:05 +0100

    Add type hints

--- src/sfsim25/quaternion.clj
+++ src/sfsim25/quaternion.clj
@@ -1,8 +1,11 @@
 (ns sfsim25.quaternion)
 
-(deftype Quaternion [a b c d]
+(deftype Quaternion [^double a ^double b ^double c ^double d]
   Object
   (equals [this other] (and (= a (.a other)) (= b (.b other)) (= c (.c other)) (= d (.d other)))))
 
-(defn add [p q]
+(defn make-quaternion [^double a ^double b ^double c ^double d]
+  (Quaternion. a b c d))
+
+(defn add [^Quaternion p ^Quaternion q]
   (Quaternion. (+ (.a p) (.a q)) (+ (.b p) (.b q)) (+ (.c p) (.c q)) (+ (.d p) (.d q))))
