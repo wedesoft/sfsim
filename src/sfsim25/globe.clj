3914a691557ffbba859688583829a43ec147ef6f
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-11-01 22:01:14 +0000

    Determine elevation value for a point

--- src/sfsim25/globe.clj
+++ src/sfsim25/globe.clj
@@ -2,7 +2,7 @@
   (:import [java.io File])
   (:require [clojure.core.memoize :as m]
             [sfsim25.cubemap :refer (cube-map-x cube-map-y cube-map-z longitude latitude map-pixels-x map-pixels-y)]
-            [sfsim25.util :refer (tile-path slurp-image spit-image get-pixel set-pixel! cube-dir cube-path)])
+            [sfsim25.util :refer (tile-path slurp-image spit-image slurp-shorts get-pixel set-pixel! cube-dir cube-path)])
   (:gen-class))
 
 (defn cube-coordinate [level tilesize tile pixel]
@@ -35,6 +35,31 @@
     [(int (+ (* c0r yfrac0 xfrac0) (* c1r yfrac0 xfrac1) (* c2r yfrac1 xfrac0) (* c3r yfrac1 xfrac1)))
      (int (+ (* c0g yfrac0 xfrac0) (* c1g yfrac0 xfrac1) (* c2g yfrac1 xfrac0) (* c3g yfrac1 xfrac1)))
      (int (+ (* c0b yfrac0 xfrac0) (* c1b yfrac0 xfrac1) (* c2b yfrac1 xfrac0) (* c3b yfrac1 xfrac1)))]))
+
+(def elevation-tile
+  (m/lru
+    (fn [in-level ty tx]
+      (slurp-shorts (tile-path "elevation" in-level ty tx ".raw")))
+    :lru/threshold 16))
+
+(defn elevation-pixel [dy dx in-level width]
+  (let [ty  (quot dy width)
+        tx  (quot dx width)
+        py  (mod dy width)
+        px  (mod dx width)
+        img (elevation-tile in-level ty tx)]
+    (aget img (+ (* py width) px))))
+
+(defn elevation-for-point [in-level width x y z]
+  (let [lon                     (longitude x y z)
+        lat                     (latitude x y z)
+        [dx0 dx1 xfrac0 xfrac1] (map-pixels-x lon width in-level)
+        [dy0 dy1 yfrac0 yfrac1] (map-pixels-y lat width in-level)
+        e0                      (elevation-pixel dy0 dx0 in-level width)
+        e1                      (elevation-pixel dy0 dx1 in-level width)
+        e2                      (elevation-pixel dy1 dx0 in-level width)
+        e3                      (elevation-pixel dy1 dx1 in-level width)]
+    (int (+ (* e0 yfrac0 xfrac0) (* e1 yfrac0 xfrac1) (* e2 yfrac1 xfrac0) (* e3 yfrac1 xfrac1)))))
 
 (defn -main
   "Program to generate tiles for cube map"
