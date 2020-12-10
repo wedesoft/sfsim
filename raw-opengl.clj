1d8c493cd320c1522b88624115f5bdab27a9d790
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-12-10 22:01:54 +0000

    Minimal OpenGL example is working

--- raw-opengl.clj
+++ raw-opengl.clj
@@ -1,7 +1,7 @@
-; clojure -cp /usr/share/java/lwjgl.jar raw-opengl.clj<Paste>
+; clojure -cp /usr/share/java/lwjgl.jar raw-opengl.clj
 (ns raw-opengl
   (:import [org.lwjgl BufferUtils]
-           [org.lwjgl.opengl Display GL11 GL15 GL20 GL30]
+           [org.lwjgl.opengl Display DisplayMode GL11 GL12 GL13 GL15 GL20 GL30]
            [org.lwjgl.input Keyboard]))
 
 (def vertex-source "#version 130
@@ -31,6 +31,12 @@ void main()
 (def indices
   (int-array [0 1 2]))
 
+(def pixels
+  (float-array [0.0 0.0 1.0
+                0.0 1.0 0.0
+                1.0 0.0 0.0
+                1.0 1.0 1.0]))
+
 (defn make-shader [source shader-type]
   (let [shader (GL20/glCreateShader shader-type)]
     (GL20/glShaderSource shader source)
@@ -48,19 +54,18 @@ void main()
       (throw (Exception. (GL20/glGetShaderInfoLog program 1024))))
     program))
 
-(defn make-float-buffer [data]
-  (let [buffer (BufferUtils/createFloatBuffer (count data))]
-    (.put buffer data)
-    (.flip buffer)
-    buffer))
+(defmacro def-make-buffer [method create-buffer]
+  `(defn ~method [data#]
+     (let [buffer# (~create-buffer (count data#))]
+       (.put buffer# data#)
+       (.flip buffer#)
+       buffer#)))
 
-(defn make-int-buffer [data]
-  (let [buffer (BufferUtils/createIntBuffer (count data))]
-    (.put buffer data)
-    (.flip buffer)
-    buffer))
+(def-make-buffer make-float-buffer BufferUtils/createFloatBuffer)
+(def-make-buffer make-int-buffer BufferUtils/createIntBuffer)
 
 (Display/setTitle "mini")
+(Display/setDisplayMode (DisplayMode. 320 240))
 (Display/create)
 
 (def vertex-shader (make-shader vertex-source GL20/GL_VERTEX_SHADER))
@@ -80,12 +85,50 @@ void main()
 (def indices-buffer (make-int-buffer indices))
 (GL15/glBufferData GL15/GL_ELEMENT_ARRAY_BUFFER indices-buffer GL15/GL_STATIC_DRAW)
 
-(GL20/glVertexAttribPointer (GL20/glGetAttribLocation program "point"   ) 3 GL11/GL_FLOAT false (* 5 4) (* 0 4))
-(GL20/glVertexAttribPointer (GL20/glGetAttribLocation program "texcoord") 2 GL11/GL_FLOAT false (* 5 4) (* 3 4))
-
+(def float-size 4)
+(GL20/glVertexAttribPointer (GL20/glGetAttribLocation program "point"   ) 3 GL11/GL_FLOAT false (* 5 float-size) (* 0 float-size))
+(GL20/glVertexAttribPointer (GL20/glGetAttribLocation program "texcoord") 2 GL11/GL_FLOAT false (* 5 float-size) (* 3 float-size))
 (GL20/glEnableVertexAttribArray 0)
 (GL20/glEnableVertexAttribArray 1)
 
-(GL11/glGetError)
+(def tex (GL11/glGenTextures))
+(GL13/glActiveTexture GL13/GL_TEXTURE0)
+(GL11/glBindTexture GL11/GL_TEXTURE_2D tex)
+(GL20/glUniform1i (GL20/glGetUniformLocation program "tex") 0)
+(def pixel-buffer (make-float-buffer pixels))
+(GL11/glTexImage2D GL11/GL_TEXTURE_2D 0 GL11/GL_RGB 2 2 0 GL12/GL_BGR GL11/GL_FLOAT pixel-buffer)
+(GL11/glTexParameteri GL11/GL_TEXTURE_2D GL11/GL_TEXTURE_WRAP_S GL11/GL_REPEAT)
+(GL11/glTexParameteri GL11/GL_TEXTURE_2D GL11/GL_TEXTURE_MIN_FILTER GL11/GL_NEAREST)
+(GL11/glTexParameteri GL11/GL_TEXTURE_2D GL11/GL_TEXTURE_MAG_FILTER GL11/GL_NEAREST)
+(GL30/glGenerateMipmap GL11/GL_TEXTURE_2D)
 
-(GL20/glGetAttribLocation program "point")
+(while (not (Display/isCloseRequested))
+  (GL11/glClearColor 0.0 0.0 0.0 0.0)
+  (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT))
+  (GL20/glUseProgram program)
+  (GL11/glDrawElements GL11/GL_TRIANGLES 3 GL11/GL_UNSIGNED_INT 0)
+  (Display/update)
+  (Thread/sleep 40))
+
+(GL20/glDisableVertexAttribArray 1)
+(GL20/glDisableVertexAttribArray 0)
+
+(GL11/glBindTexture GL11/GL_TEXTURE_2D 0)
+(GL11/glDeleteTextures tex)
+
+(GL15/glBindBuffer GL15/GL_ELEMENT_ARRAY_BUFFER 0)
+(GL15/glDeleteBuffers idx)
+
+(GL15/glBindBuffer GL15/GL_ARRAY_BUFFER 0)
+(GL15/glDeleteBuffers vbo)
+
+(GL30/glBindVertexArray 0)
+(GL30/glDeleteVertexArrays vao)
+
+(GL20/glDetachShader program vertex-shader)
+(GL20/glDetachShader program fragment-shader)
+(GL20/glDeleteProgram program)
+(GL20/glDeleteShader vertex-shader)
+(GL20/glDeleteShader fragment-shader)
+
+(Display/destroy)
