1f62d16712dfc04f9316663776bcd3df0808ef5d
Jan Wedekind (Dr) <jan@wedesoft.de>
2021-01-21 22:55:57 +0000

    Use ->Quaternion instead of quaternion

--- src/sfsim25/quaternion.clj
+++ src/sfsim25/quaternion.clj
@@ -14,11 +14,6 @@
 
 (set! *warn-on-reflection* true)
 
-(defn quaternion
-  "Construct a quaternion"
-  ^Quaternion [^double a ^double b ^double c ^double d]
-  (Quaternion. a b c d))
-
 (defn real ^double [^Quaternion q] (.a q))
 (defn imag ^double [^Quaternion q] (.b q))
 (defn jmag ^double [^Quaternion q] (.c q))
@@ -27,17 +22,17 @@
 (defn +
   "Add two quaternions"
   ^Quaternion [^Quaternion p ^Quaternion q]
-  (Quaternion. (c/+ (.a p) (.a q)) (c/+ (.b p) (.b q)) (c/+ (.c p) (.c q)) (c/+ (.d p) (.d q))))
+  (->Quaternion (c/+ (.a p) (.a q)) (c/+ (.b p) (.b q)) (c/+ (.c p) (.c q)) (c/+ (.d p) (.d q))))
 
 (defn -
   "Subtract two quaternions"
   ^Quaternion [^Quaternion p ^Quaternion q]
-  (Quaternion. (c/- (.a p) (.a q)) (c/- (.b p) (.b q)) (c/- (.c p) (.c q)) (c/- (.d p) (.d q))))
+  (->Quaternion (c/- (.a p) (.a q)) (c/- (.b p) (.b q)) (c/- (.c p) (.c q)) (c/- (.d p) (.d q))))
 
 (defn *
   "Multiply two quaternions"
   ^Quaternion [^Quaternion p ^Quaternion q]
-  (quaternion
+  (->Quaternion
     (c/- (c/* (.a p) (.a q)) (c/* (.b p) (.b q)) (c/* (.c p) (.c q)) (c/* (.d p) (.d q)))
     (c/- (c/+ (c/* (.a p) (.b q)) (c/* (.b p) (.a q)) (c/* (.c p) (.d q))) (c/* (.d p) (.c q)))
     (c/+ (c/- (c/* (.a p) (.c q)) (c/* (.b p) (.d q))) (c/* (.c p) (.a q)) (c/* (.d p) (.b q)))
@@ -57,23 +52,23 @@
   "Normalize quaternion to create unit quaternion"
   ^Quaternion [^Quaternion q]
   (let [factor (/ 1.0 (norm q))]
-    (quaternion (c/* (.a q) factor) (c/* (.b q) factor) (c/* (.c q) factor) (c/* (.d q) factor))))
+    (->Quaternion (c/* (.a q) factor) (c/* (.b q) factor) (c/* (.c q) factor) (c/* (.d q) factor))))
 
 (defn conjugate
   "Return conjugate of quaternion"
   ^Quaternion [^Quaternion q]
-  (quaternion (.a q) (c/- (.b q)) (c/- (.c q)) (c/- (.d q))))
+  (->Quaternion (.a q) (c/- (.b q)) (c/- (.c q)) (c/- (.d q))))
 
 (defn inverse
   "Return inverse of quaternion"
   ^Quaternion [^Quaternion q]
   (let [factor (/ 1.0 (norm2 q))]
-    (quaternion (c/* (.a q) factor) (c/* (c/- (.b q)) factor) (c/* (c/- (.c q)) factor) (c/* (c/- (.d q)) factor))))
+    (->Quaternion (c/* (.a q) factor) (c/* (c/- (.b q)) factor) (c/* (c/- (.c q)) factor) (c/* (c/- (.d q)) factor))))
 
 (defn vector3->quaternion
   "Convert 3D vector to quaternion"
   ^Quaternion [^Vector3 v]
-  (quaternion 0.0 (.x v) (.y v) (.z v)))
+  (->Quaternion 0.0 (.x v) (.y v) (.z v)))
 
 (defn quaternion->vector3
   "Convert quaternion to 3D vector"
@@ -87,7 +82,7 @@
         rotation   (v/norm (quaternion->vector3 q))
         cos-scale  (c/* scale (Math/cos rotation))
         sinc-scale (c/* scale (sinc rotation))]
-    (quaternion cos-scale (c/* sinc-scale (.b q)) (c/* sinc-scale (.c q)) (c/* sinc-scale (.d q)))))
+    (->Quaternion cos-scale (c/* sinc-scale (.b q)) (c/* sinc-scale (.c q)) (c/* sinc-scale (.d q)))))
 
 (defn rotation
   "Generate quaternion to represent rotation"
--- test/sfsim25/quaternion_test.clj
+++ test/sfsim25/quaternion_test.clj
@@ -5,33 +5,33 @@
             [sfsim25.quaternion :refer :all]
             [sfsim25.vector3 :refer (vector3) :as v]))
 
-(def o (quaternion 1 0 0 0))
-(def -o (quaternion -1 0 0 0))
-(def i (quaternion 0 1 0 0))
-(def -i (quaternion 0 -1 0 0))
-(def j (quaternion 0 0 1 0))
-(def -j (quaternion 0 0 -1 0))
-(def k (quaternion 0 0 0 1))
-(def -k (quaternion 0 0 0 -1))
+(def o (->Quaternion 1 0 0 0))
+(def -o (->Quaternion -1 0 0 0))
+(def i (->Quaternion 0 1 0 0))
+(def -i (->Quaternion 0 -1 0 0))
+(def j (->Quaternion 0 0 1 0))
+(def -j (->Quaternion 0 0 -1 0))
+(def k (->Quaternion 0 0 0 1))
+(def -k (->Quaternion 0 0 0 -1))
 
 (deftest display-test
   (testing "Display quaternion"
-    (is (= "(quaternion 2.0 3.0 5.0 7.0)" (str (quaternion 2 3 5 7))))))
+    (is (= "(quaternion 2.0 3.0 5.0 7.0)" (str (->Quaternion 2 3 5 7))))))
 
 (deftest component-test
   (testing "Get components of quaternion"
-    (is (= 2.0 (real (quaternion 2 3 5 7))))
-    (is (= 3.0 (imag (quaternion 2 3 5 7))))
-    (is (= 5.0 (jmag (quaternion 2 3 5 7))))
-    (is (= 7.0 (kmag (quaternion 2 3 5 7))))))
+    (is (= 2.0 (real (->Quaternion 2 3 5 7))))
+    (is (= 3.0 (imag (->Quaternion 2 3 5 7))))
+    (is (= 5.0 (jmag (->Quaternion 2 3 5 7))))
+    (is (= 7.0 (kmag (->Quaternion 2 3 5 7))))))
 
 (deftest add-test
   (testing "Add two quaternions"
-    (is (= (quaternion 6 8 10 12) (+ (quaternion 1 2 3 4) (quaternion 5 6 7 8))))))
+    (is (= (->Quaternion 6 8 10 12) (+ (->Quaternion 1 2 3 4) (->Quaternion 5 6 7 8))))))
 
 (deftest subtract-test
   (testing "Subtract two quaternions"
-    (is (= (quaternion 1 2 3 4) (- (quaternion 6 8 10 12) (quaternion 5 6 7 8))))))
+    (is (= (->Quaternion 1 2 3 4) (- (->Quaternion 6 8 10 12) (->Quaternion 5 6 7 8))))))
 
 (deftest multiply-test
   (testing "Multiply two quaternions"
@@ -55,27 +55,27 @@
 
 (deftest norm-test
   (testing "Norm of quaternion"
-    (is (= 1.0 (norm (quaternion 0.216 0.288 0.48 0.8))))))
+    (is (= 1.0 (norm (->Quaternion 0.216 0.288 0.48 0.8))))))
 
 (deftest normalize-test
   (testing "Normalize a quaternion"
     (is
-      (let [q (quaternion 0.216 0.288 0.48 0.8)]
-        (= q (normalize (* (quaternion 2 0 0 0) q)))))))
+      (let [q (->Quaternion 0.216 0.288 0.48 0.8)]
+        (= q (normalize (* (->Quaternion 2 0 0 0) q)))))))
 
 (deftest conjugate-test
   (testing "Conjugate of quaternion"
-    (is (= (quaternion 2 -3 -5 -7) (conjugate (quaternion 2 3 5 7))))))
+    (is (= (->Quaternion 2 -3 -5 -7) (conjugate (->Quaternion 2 3 5 7))))))
 
 (deftest inverse-test
   (testing "Inverse of quaternion"
-    (is (= (quaternion 0.216 -0.288 -0.48 -0.8) (inverse (quaternion 0.216 0.288 0.48 0.8))))
-    (is (= (quaternion 0.108 -0.144 -0.24 -0.4) (inverse (quaternion 0.432 0.576 0.96 1.6))))))
+    (is (= (->Quaternion 0.216 -0.288 -0.48 -0.8) (inverse (->Quaternion 0.216 0.288 0.48 0.8))))
+    (is (= (->Quaternion 0.108 -0.144 -0.24 -0.4) (inverse (->Quaternion 0.432 0.576 0.96 1.6))))))
 
 (deftest vector-conversion-test
   (testing "Convert 3D vector to quaternion and back"
-    (is (= (quaternion 0 2 3 5) (vector3->quaternion (vector3 2 3 5))))
-    (is (= (vector3 2 3 5) (quaternion->vector3 (quaternion 0 2 3 5))))))
+    (is (= (->Quaternion 0 2 3 5) (vector3->quaternion (vector3 2 3 5))))
+    (is (= (vector3 2 3 5) (quaternion->vector3 (->Quaternion 0 2 3 5))))))
 
 (def pi Math/PI)
 (def e (Math/exp 1))
@@ -84,16 +84,16 @@
 (deftest exp-test
   (testing "Exponentiation of quaternions"
     (are [result component q] (< (Math/abs (c/- result (component (exp q)))) 1e-6)
-      -1.0 real (quaternion 0 0 pi 0)
-       0.0 jmag (quaternion 0 0 pi 0)
-       0.0 real (quaternion 0 0 (/ pi 2) 0)
-       1.0 jmag (quaternion 0 0 (/ pi 2) 0)
-      -e   real (quaternion 1 0 pi 0)
-       e   jmag (quaternion 1 0 (/ pi 2) 0)
-       0.0 kmag (quaternion 0 0 0 pi)
-       1.0 kmag (quaternion 0 0 0 (/ pi 2))
-       0.8 imag (quaternion 0 (c/* 0.4 pi) (c/* 0.3 pi) 0)
-       0.6 jmag (quaternion 0 (c/* 0.4 pi) (c/* 0.3 pi) 0))))
+      -1.0 real (->Quaternion 0 0 pi 0)
+       0.0 jmag (->Quaternion 0 0 pi 0)
+       0.0 real (->Quaternion 0 0 (/ pi 2) 0)
+       1.0 jmag (->Quaternion 0 0 (/ pi 2) 0)
+      -e   real (->Quaternion 1 0 pi 0)
+       e   jmag (->Quaternion 1 0 (/ pi 2) 0)
+       0.0 kmag (->Quaternion 0 0 0 pi)
+       1.0 kmag (->Quaternion 0 0 0 (/ pi 2))
+       0.8 imag (->Quaternion 0 (c/* 0.4 pi) (c/* 0.3 pi) 0)
+       0.6 jmag (->Quaternion 0 (c/* 0.4 pi) (c/* 0.3 pi) 0))))
 
 (deftest rotation-test
   (testing "Represent rotation using quaternion"
