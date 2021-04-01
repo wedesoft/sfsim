068691f18748aaab8eb784cc21d113b156e7003a
Jan Wedekind (Dr) <jan@wedesoft.de>
2021-04-01 23:07:52 +0100

    Minor changes

--- etc/scratch.clj
+++ etc/scratch.clj
@@ -4,6 +4,17 @@
          '[sfsim25.cubemap :refer :all]
          '[sfsim25.quadtree :refer :all])
 
+(import '[org.lwjgl.opengl Display DisplayMode GL11 GL12 GL13 GL15 GL20 GL30 GL32 GL40])
+
+(Display/setTitle "scratch")
+(Display/setDisplayMode (DisplayMode. 640 480))
+(Display/create)
+
+(GL11/glClearColor 0.0 0.0 0.0 0.0)
+(GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT))
+
+(Display/update)
+
 (def radius1 6378000.0)
 (def radius2 6357000.0)
 
@@ -12,8 +23,6 @@
 
 (def position (atom (->Vector3 (* 3 radius1) 0 0)))
 (def tree {})
-
-(>!! tree-state tree)
 
 (go
   (loop [tree (<! tree-state)]
@@ -25,6 +34,13 @@
         (>! changes {:drop drop-list :load load-list :tiles tiles})
         (recur (<! tree-state))))))
 
+(def vao (GL30/glGenVertexArrays))
+(GL30/glBindVertexArray vao)
+
+(>!! tree-state tree)
+
 (def data (poll! changes))
 
 (def tree (quadtree-add (quadtree-drop tree (:drop data)) (:load data) (:tiles data)))
+
+(Display/destroy)
