83af21a91f6c841a8c18b096a70b467a0c1951f2
Jan Wedekind (Dr) <jan@wedesoft.de>
2021-04-07 23:11:01 +0100

    Increase tesselation level

--- etc/scratch.clj
+++ etc/scratch.clj
@@ -26,12 +26,12 @@ out mediump vec2 texcoord_tes[];
 void main(void)
 {
   if (gl_InvocationID == 0) {
-    gl_TessLevelOuter[0] = 4.0;
-    gl_TessLevelOuter[1] = 4.0;
-    gl_TessLevelOuter[2] = 4.0;
-    gl_TessLevelOuter[3] = 4.0;
-    gl_TessLevelInner[0] = 4.0;
-    gl_TessLevelInner[1] = 4.0;
+    gl_TessLevelOuter[0] = 32.0;
+    gl_TessLevelOuter[1] = 32.0;
+    gl_TessLevelOuter[2] = 32.0;
+    gl_TessLevelOuter[3] = 32.0;
+    gl_TessLevelInner[0] = 32.0;
+    gl_TessLevelInner[1] = 32.0;
   }
   gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
   texcoord_tes[gl_InvocationID] = texcoord_tcs[gl_InvocationID];
