9ca704aebee5ada44c7d8c1e0fbb555d07bed2d8
Jan Wedekind (Dr) <jan@wedesoft.de>
2021-04-26 22:06:43 +0100

    Working on neighbours across cube faces

--- src/sfsim25/quadtree.clj
+++ src/sfsim25/quadtree.clj
@@ -126,27 +126,27 @@
 (defn neighbour-path
   "Determine path of neighbouring tile at same level"
   ([path dy dx top-level]
-   (if top-level
-     (if (= (count path) 1)
-       [[(case (first path)
-           :0 (case dy -1 :3, 0 (case dx -1 :4, 0 :0, 1 :2), 1 :1)
-           :1 (case dy -1 :0, 0 (case dx -1 :4, 0 :1, 1 :2), 1 :5)
-           :2 (case dy -1 :0, 0 (case dx -1 :1, 0 :2, 1 :3), 1 :5)
-           :3 (case dy -1 :0, 0 (case dx -1 :2, 0 :3, 1 :4), 1 :5)
-           :4 (case dy -1 :0, 0 (case dx -1 :3, 0 :4, 1 :1), 1 :5)
-           :5 (case dy -1 :1, 0 (case dx -1 :4, 0 :5, 1 :2), 1 :3))] 0 0]
-       [(cons (first path) (first (neighbour-path (rest path) dy dx false))) 0 0])
-     (if (empty? path)
-       [() dy dx]
-       (let [[tail dy dx] (neighbour-path (rest path) dy dx false)
-             tile         (first path)
-             [replacement propagate]
-               (case tile
-                 :0 (case dy -1 [:2 true ], 0 (case dx -1 [:1 true ] 0 [:0 false] 1 [:1 false]), 1 [:2 false])
-                 :1 (case dy -1 [:3 true ], 0 (case dx -1 [:0 false] 0 [:1 false] 1 [:0 true ]), 1 [:3 false])
-                 :2 (case dy -1 [:0 false], 0 (case dx -1 [:3 true ] 0 [:2 false] 1 [:3 false]), 1 [:0 true ])
-                 :3 (case dy -1 [:1 false], 0 (case dx -1 [:2 false] 0 [:3 false] 1 [:2 true ]), 1 [:1 true ]))]
-           [(cons replacement tail) (if propagate dy 0) (if propagate dx 0)]))))
+   (if (empty? path)
+     [() dy dx]
+     (let [[tail dy dx] (neighbour-path (rest path) dy dx false)
+           tile         (first path)]
+       (if top-level
+         (let [replacement
+                 (case (first path)
+                   :0 (case dy -1 :3, 0 (case dx -1 :4, 0 :0, 1 :2), 1 :1)
+                   :1 (case dy -1 :0, 0 (case dx -1 :4, 0 :1, 1 :2), 1 :5)
+                   :2 (case dy -1 :0, 0 (case dx -1 :1, 0 :2, 1 :3), 1 :5)
+                   :3 (case dy -1 :0, 0 (case dx -1 :2, 0 :3, 1 :4), 1 :5)
+                   :4 (case dy -1 :0, 0 (case dx -1 :3, 0 :4, 1 :1), 1 :5)
+                   :5 (case dy -1 :1, 0 (case dx -1 :4, 0 :5, 1 :2), 1 :3))]
+           [(cons replacement tail) 0 0])
+         (let [[replacement propagate]
+                 (case tile
+                   :0 (case dy -1 [:2 true ], 0 (case dx -1 [:1 true ] 0 [:0 false] 1 [:1 false]), 1 [:2 false])
+                   :1 (case dy -1 [:3 true ], 0 (case dx -1 [:0 false] 0 [:1 false] 1 [:0 true ]), 1 [:3 false])
+                   :2 (case dy -1 [:0 false], 0 (case dx -1 [:3 true ] 0 [:2 false] 1 [:3 false]), 1 [:0 true ])
+                   :3 (case dy -1 [:1 false], 0 (case dx -1 [:2 false] 0 [:3 false] 1 [:2 true ]), 1 [:1 true ]))]
+             [(cons replacement tail) (if propagate dy 0) (if propagate dx 0)])))))
   ([path dy dx]
    (first (neighbour-path path dy dx true))))
 
