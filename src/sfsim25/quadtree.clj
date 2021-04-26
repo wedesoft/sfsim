bb131ad6d47d4118ea0845e9856d2a4dc9bb337e
Jan Wedekind (Dr) <jan@wedesoft.de>
2021-04-26 23:08:43 +0100

    Completed neighbour-path method

--- src/sfsim25/quadtree.clj
+++ src/sfsim25/quadtree.clj
@@ -135,7 +135,6 @@
                c1           {:0 :2, :1 :0, :3 :1, :2 :3}
                c2           {:0 :3, :1 :2, :3 :0, :2 :1}
                c3           {:0 :1, :1 :3, :3 :2, :2 :0}
-               cx           {:0 :x  :1 :x  :3 :x  :2 :x}
                [replacement rotation]
                  (case (first path)
                    :0 (case dy -1 [:3 c2], 0 (case dx -1 [:4 c1], 0 [:0 c0], 1 [:2 c3]), 1 [:1 c0])
@@ -143,7 +142,7 @@
                    :2 (case dy -1 [:0 c1], 0 (case dx -1 [:1 c0], 0 [:2 c0], 1 [:3 c0]), 1 [:5 c3])
                    :3 (case dy -1 [:0 c2], 0 (case dx -1 [:2 c0], 0 [:3 c0], 1 [:4 c0]), 1 [:5 c2])
                    :4 (case dy -1 [:0 c3], 0 (case dx -1 [:3 c0], 0 [:4 c0], 1 [:1 c0]), 1 [:5 c1])
-                   :5 (case dy -1 [:1 cx], 0 (case dx -1 [:4 cx], 0 [:5 c0], 1 [:2 cx]), 1 [:3 cx]))]
+                   :5 (case dy -1 [:1 c0], 0 (case dx -1 [:4 c3], 0 [:5 c0], 1 [:2 c1]), 1 [:3 c2]))]
            (cons replacement (map rotation tail)))
          (let [[replacement propagate]
                  (case tile
