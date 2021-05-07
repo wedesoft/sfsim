554e4114e2696e2435eb482ee2a3815f39acc759
Jan Wedekind (Dr) <jan@wedesoft.de>
2021-05-07 21:41:17 +0100

    Port Vector4 tests to Midje

--- test/sfsim25/vector4_test.clj
+++ test/sfsim25/vector4_test.clj
@@ -1,16 +1,13 @@
 (ns sfsim25.vector4-test
-  (:require [clojure.test :refer :all]
+  (:require [midje.sweet :refer :all]
             [sfsim25.vector4 :refer :all]
             [sfsim25.vector3 :refer (->Vector3)]))
 
-(deftest equality-test
-  (testing "Equality of 4D vectors"
-    (is (= (->Vector4 2 3 5 7) (->Vector4 2 3 5 7)))))
+(fact "Equality of 4D vectors"
+  (->Vector4 2 3 5 7) => (->Vector4 2 3 5 7))
 
-(deftest seqable-test
-  (testing "Convert vector to sequence"
-    (is (= [2.0 3.0 5.0 7.0] (vals (->Vector4 2 3 5 7))))))
+(fact "Convert vector to sequence"
+  (vals (->Vector4 2 3 5 7)) => [2.0 3.0 5.0 7.0])
 
-(deftest project-test
-  (testing "Project homogeneous coordinate to cartesian"
-    (is (= (->Vector3 2 3 5) (project (->Vector4 4 6 10 2))))))
+(fact "Project homogeneous coordinate to cartesian"
+  (project (->Vector4 4 6 10 2)) => (->Vector3 2 3 5))
