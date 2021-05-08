25b5ff238d9038d4a24dca212072c542996a1856
Jan Wedekind (Dr) <jan@wedesoft.de>
2021-05-08 23:00:07 +0100

    Ported quadtree tests to Midje

--- test/sfsim25/quadtree_test.clj
+++ test/sfsim25/quadtree_test.clj
@@ -68,121 +68,118 @@
     (tiles-to-load sub-quad (fn [f l y x] (contains? #{[5 1] [5 2]} [f l]))) => [[:5 :1 :0] [:5 :1 :1] [:5 :1 :2] [:5 :1 :3]]
     (tiles-to-load sub-quad (fn [face level y x] (= [5 2] [face level]))) => []))
 
-(deftest tile-meta-data-test
-  (testing "Convert tile path to face, level, y and x"
-    (are [face level y x path] (= {:face face :level level :y y :x x} (tile-meta-data path))
-      5 0 0 0 [:5]
-      5 1 0 0 [:5 :0]
-      5 1 0 1 [:5 :1]
-      5 1 1 0 [:5 :2]
-      5 1 1 1 [:5 :3]
-      5 2 0 2 [:5 :1 :0]
-      5 2 2 1 [:5 :2 :1])))
+(tabular
+  (fact "Convert tile path to face, level, y and x"
+    (tile-meta-data path?) => {:face face? :level level? :y y? :x x?})
+  path?      face? level? y? x?
+  [:5]       5     0      0  0
+  [:5 :0]    5     1      0  0
+  [:5 :1]    5     1      0  1
+  [:5 :2]    5     1      1  0
+  [:5 :3]    5     1      1  1
+  [:5 :1 :0] 5     2      0  2
+  [:5 :2 :1] 5     2      2  1)
 
-(deftest tiles-meta-data-test
-  (testing "Get tile metadata for multiple tiles"
-    (is (= [] (tiles-meta-data [])))
-    (is (= [{:face 5 :level 2 :y 2 :x 1}] (tiles-meta-data [[:5 :2 :1]])))))
+(facts "Get tile metadata for multiple tiles"
+  (tiles-meta-data []) => []
+  (tiles-meta-data [[:5 :2 :1]]) => [{:face 5 :level 2 :y 2 :x 1}])
 
-(deftest quadtree-add-test
-  (testing "Add tiles to the quad tree"
-    (is (= {}  (quadtree-add {} [] [])))
-    (is (= {:5 {:2 {:face 5 :level 1 :y 1 :x 0}}} (quadtree-add {} [[:5 :2]] [{:face 5 :level 1 :y 1 :x 0}])))))
+(facts "Add tiles to the quad tree"
+  (quadtree-add {} [] []) => {}
+  (quadtree-add {} [[:5 :2]] [{:face 5 :level 1 :y 1 :x 0}]) => {:5 {:2 {:face 5 :level 1 :y 1 :x 0}}})
 
-(deftest quadtree-extract-test
-  (testing "Extract list of nodes from quad tree"
-    (is (= [] (quadtree-extract {} [])))
-    (is (= [{:level 2}] (quadtree-extract {:5 {:2 {:level 2}}} [[:5 :2]])))))
+(facts "Extract list of nodes from quad tree"
+  (quadtree-extract {} []) => []
+  (quadtree-extract {:5 {:2 {:level 2}}} [[:5 :2]]) => [{:level 2}])
 
-(deftest quadtree-drop-test
-  (testing "Remove tiles from quad tree"
-    (is (= {:3 {}} (quadtree-drop {:3 {}} [])))
-    (is (= {} (quadtree-drop {:3 {}} [[:3]])))))
+(facts "Remove tiles from quad tree"
+  (quadtree-drop {:3 {}} []) => {:3 {}}
+  (quadtree-drop {:3 {}} [[:3]]) => {})
 
 (defn keyword->int [x] (Integer/parseInt (name x)))
 (defn int->keyword [x] (keyword (str x)))
 
-(deftest neighbour-path-test
-  (testing "Check consistency of neighbouring faces with cube-map coordinates"
-    (doseq [face (range 6) [dy dx] [[0 -1] [0 1] [-1 0] [1 0]]]
-      (is (= (cube-map face (+ 0.5 (* dy 0.5)) (+ 0.5 (* dx 0.5)))
-             (v/+ (cube-map face 0.5 0.5)
-                  (cube-map (keyword->int (first (neighbour-path [(int->keyword face)] dy dx))) 0.5 0.5))))))
-  (testing "Same tile or neighbouring tiles on the same face"
-    (are [result path dy dx] (= result (neighbour-path path dy dx))
-      [:0]       [:0]        0  0
-      [:1]       [:1]        0  0
-      [:2]       [:2]        0  0
-      [:3]       [:3]        0  0
-      [:4]       [:4]        0  0
-      [:5]       [:5]        0  0
-      [:5 :0]    [:5 :0]     0  0
-      [:5 :1]    [:5 :1]     0  0
-      [:5 :2]    [:5 :2]     0  0
-      [:5 :3]    [:5 :3]     0  0
-      [:5 :0]    [:5 :2]    -1  0
-      [:5 :1]    [:5 :3]    -1  0
-      [:5 :0]    [:5 :1]     0 -1
-      [:5 :2]    [:5 :3]     0 -1
-      [:5 :2]    [:5 :0]     1  0
-      [:5 :3]    [:5 :1]     1  0
-      [:5 :1]    [:5 :0]     0  1
-      [:5 :3]    [:5 :2]     0  1
-      [:5 :3 :0] [:5 :3 :0]  0  0
-      [:5 :3 :0] [:5 :3 :2] -1  0
-      [:5 :3 :1] [:5 :3 :0]  0  1
-      [:5 :1 :2] [:5 :3 :0] -1  0
-      [:5 :1 :3] [:5 :3 :1] -1  0
-      [:5 :2 :1] [:5 :3 :0]  0 -1
-      [:5 :2 :3] [:5 :3 :2]  0 -1
-      [:5 :3 :0] [:5 :1 :2]  1  0
-      [:5 :3 :1] [:5 :1 :3]  1  0
-      [:5 :3 :0] [:5 :2 :1]  0  1
-      [:5 :3 :2] [:5 :2 :3]  0  1
-      [:0 :2]    [:1 :0]    -1  0
-      [:0 :3]    [:2 :0]    -1  0
-      [:0 :1]    [:3 :0]    -1  0
-      [:0 :0]    [:4 :0]    -1  0
-      [:4 :1]    [:1 :0]     0 -1
-      [:1 :1]    [:2 :0]     0 -1
-      [:2 :1]    [:3 :0]     0 -1
-      [:3 :1]    [:4 :0]     0 -1
-      [:2 :0]    [:1 :1]     0  1
-      [:3 :0]    [:2 :1]     0  1
-      [:4 :0]    [:3 :1]     0  1
-      [:1 :0]    [:4 :1]     0  1
-      [:5 :0]    [:1 :2]     1  0
-      [:5 :1]    [:2 :2]     1  0
-      [:5 :3]    [:3 :2]     1  0
-      [:5 :2]    [:4 :2]     1  0
-      [:1 :0]    [:0 :2]     1  0
-      [:2 :0]    [:0 :3]     0  1
-      [:3 :0]    [:0 :1]    -1  0
-      [:4 :0]    [:0 :0]     0 -1
-      [:1 :2]    [:5 :0]    -1  0
-      [:2 :2]    [:5 :1]     0  1
-      [:3 :2]    [:5 :3]     1  0
-      [:4 :2]    [:5 :2]     0 -1)))
+(doseq [face (range 6) [dy dx] [[0 -1] [0 1] [-1 0] [1 0]]]
+  (fact "Check consistency of neighbouring faces with cube-map coordinates"
+    (cube-map face (+ 0.5 (* dy 0.5)) (+ 0.5 (* dx 0.5))) =>
+      (v/+ (cube-map face 0.5 0.5) (cube-map (keyword->int (first (neighbour-path [(int->keyword face)] dy dx))) 0.5 0.5))))
 
-(deftest neighbour-paths-test
-  (testing "Get the four neighbours for a given path of a tile"
-    (let [neighbours (neighbour-paths [:5 :1 :2])]
-      (is (= [:5 :1 :0] (:up    neighbours)))
-      (is (= [:5 :0 :3] (:left  neighbours)))
-      (is (= [:5 :3 :0] (:down  neighbours)))
-      (is (= [:5 :1 :3] (:right neighbours))))))
+(tabular
+  (fact "Same tile or neighbouring tiles on the same face"
+    (neighbour-path path? dy? dx?) => result?)
+  path?      dy? dx? result?
+  [:0]        0   0  [:0]
+  [:1]        0   0  [:1]
+  [:2]        0   0  [:2]
+  [:3]        0   0  [:3]
+  [:4]        0   0  [:4]
+  [:5]        0   0  [:5]
+  [:5 :0]     0   0  [:5 :0]
+  [:5 :1]     0   0  [:5 :1]
+  [:5 :2]     0   0  [:5 :2]
+  [:5 :3]     0   0  [:5 :3]
+  [:5 :2]    -1   0  [:5 :0]
+  [:5 :3]    -1   0  [:5 :1]
+  [:5 :1]     0  -1  [:5 :0]
+  [:5 :3]     0  -1  [:5 :2]
+  [:5 :0]     1   0  [:5 :2]
+  [:5 :1]     1   0  [:5 :3]
+  [:5 :0]     0   1  [:5 :1]
+  [:5 :2]     0   1  [:5 :3]
+  [:5 :3 :0]  0   0  [:5 :3 :0]
+  [:5 :3 :2] -1   0  [:5 :3 :0]
+  [:5 :3 :0]  0   1  [:5 :3 :1]
+  [:5 :3 :0] -1   0  [:5 :1 :2]
+  [:5 :3 :1] -1   0  [:5 :1 :3]
+  [:5 :3 :0]  0  -1  [:5 :2 :1]
+  [:5 :3 :2]  0  -1  [:5 :2 :3]
+  [:5 :1 :2]  1   0  [:5 :3 :0]
+  [:5 :1 :3]  1   0  [:5 :3 :1]
+  [:5 :2 :1]  0   1  [:5 :3 :0]
+  [:5 :2 :3]  0   1  [:5 :3 :2]
+  [:1 :0]    -1   0  [:0 :2]
+  [:2 :0]    -1   0  [:0 :3]
+  [:3 :0]    -1   0  [:0 :1]
+  [:4 :0]    -1   0  [:0 :0]
+  [:1 :0]     0  -1  [:4 :1]
+  [:2 :0]     0  -1  [:1 :1]
+  [:3 :0]     0  -1  [:2 :1]
+  [:4 :0]     0  -1  [:3 :1]
+  [:1 :1]     0   1  [:2 :0]
+  [:2 :1]     0   1  [:3 :0]
+  [:3 :1]     0   1  [:4 :0]
+  [:4 :1]     0   1  [:1 :0]
+  [:1 :2]     1   0  [:5 :0]
+  [:2 :2]     1   0  [:5 :1]
+  [:3 :2]     1   0  [:5 :3]
+  [:4 :2]     1   0  [:5 :2]
+  [:0 :2]     1   0  [:1 :0]
+  [:0 :3]     0   1  [:2 :0]
+  [:0 :1]    -1   0  [:3 :0]
+  [:0 :0]     0  -1  [:4 :0]
+  [:5 :0]    -1   0  [:1 :2]
+  [:5 :1]     0   1  [:2 :2]
+  [:5 :3]     1   0  [:3 :2]
+  [:5 :2]     0  -1  [:4 :2])
 
-(deftest leaf-paths-test
-  (testing "Get a list of paths of the leaves"
-    (is (= [[:1] [:2]] (leaf-paths {:1 {} :2 {}})))
-    (is (= [[:5 :1]] (leaf-paths {:5 {:1 {}}})))
-    (is (= [[:5 :1]] (leaf-paths {:5 {:vao 42 :1 {}}})))))
+(tabular
+  (facts "Get the four neighbours for a given path of a tile"
+    (k? (neighbour-paths [:5 :1 :2])) => path?)
+  k?     path?
+  :up    [:5 :1 :0]
+  :left  [:5 :0 :3]
+  :down  [:5 :3 :0]
+  :right [:5 :1 :3])
 
-(deftest check-neighbours-test
-  (testing "Update quad tree with neighbourhood information"
-    (is (= 1 (get-in (check-neighbours {:5 {} :1 {}}) [:5 :up])))
-    (is (= 0 (get-in (check-neighbours {:5 {}}) [:5 :up])))
-    (is (= 1 (get-in (check-neighbours {:5 {} :4 {}}) [:5 :left])))
-    (is (= 0 (get-in (check-neighbours {:5 {}}) [:5 :left])))
-    (is (= 1 (get-in (check-neighbours {:1 {} :5 {}}) [:1 :down])))
-    (is (= 1 (get-in (check-neighbours {:5 {:1 {} :3 {}}}) [:5 :1 :down])))))
+(facts "Get a list of paths of the leaves"
+  (leaf-paths {:1 {} :2 {}})        => [[:1] [:2]]
+  (leaf-paths {:5 {:1 {}}})         => [[:5 :1]]
+  (leaf-paths {:5 {:vao 42 :1 {}}}) => [[:5 :1]])
+
+(facts "Update quad tree with neighbourhood information"
+  (get-in (check-neighbours {:5 {} :1 {}})      [:5 :up]     ) => 1
+  (get-in (check-neighbours {:5 {}})            [:5 :up]     ) => 0
+  (get-in (check-neighbours {:5 {} :4 {}})      [:5 :left]   ) => 1
+  (get-in (check-neighbours {:5 {}})            [:5 :left]   ) => 0
+  (get-in (check-neighbours {:1 {} :5 {}})      [:1 :down]   ) => 1
+  (get-in (check-neighbours {:5 {:1 {} :3 {}}}) [:5 :1 :down]) => 1)
