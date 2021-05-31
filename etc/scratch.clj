bdcaf5af03c7bf459c5211f47fc5f5640e0ded47
Jan Wedekind (Dr) <jan@wedesoft.de>
2021-05-31 21:59:24 +0100

    Increase level of detail

--- etc/scratch.clj
+++ etc/scratch.clj
@@ -165,7 +165,7 @@ void main()
 
 (go-loop []
   (if-let [tree (<! tree-state)]
-    (let [increase? (partial increase-level? tilesize radius1 radius2 640 60 20 6 @position)]
+    (let [increase? (partial increase-level? tilesize radius1 radius2 640 60 8 6 @position)]
       (>! changes (update-level-of-detail tree increase? true))
       (recur))))
 
