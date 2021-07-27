08cebbfa0d22cfa283370306489494e206737c94
Jan Wedekind (Dr) <jan@wedesoft.de>
2021-07-27 22:19:10 +0100

    Make ray_sphere work with non-unit vectors

--- etc/scratch.clj
+++ etc/scratch.clj
@@ -438,11 +438,12 @@ uniform sampler2D dep;
 
 vec2 ray_sphere(vec3 centre, float radius, vec3 origin, vec3 direction) {
   vec3 offset = origin - centre;
-  float discr = pow(dot(direction, offset), 2) - (dot(offset, offset) - radius * radius);
-  if (discr > 0) {
-    float d2 = sqrt(discr);
-    float m = -dot(direction, offset);
-    vec2 result = vec2(m - d2, 2 * d2);
+  float direction_sqr = dot(direction, direction);
+  float discriminant = pow(dot(direction, offset), 2) - direction_sqr * (dot(offset, offset) - radius * radius);
+  if (discriminant > 0) {
+    float length2 = sqrt(discriminant) / direction_sqr;
+    float middle = -dot(direction, offset) / direction_sqr;
+    vec2 result = vec2(middle - length2, 2 * length2);
     if (result.x < 0) {
       result.y = max(0, result.y + result.x);
       result.x = 0;
