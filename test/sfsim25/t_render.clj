64edb38c485d47539e30b385cf43dfef58d1b080
Jan Wedekind (Dr) <jan@wedesoft.de>
2021-08-03 21:13:29 +0100

    Bugfix for test function

--- test/sfsim25/t_render.clj
+++ test/sfsim25/t_render.clj
@@ -10,7 +10,7 @@
     (let [img (slurp-image filename)]
       (and (= (:width img) (:width other))
            (= (:height img) (:height other))
-           (= (take 1 (seq (:data img))) (take 1 (seq (:data other))))))))
+           (= (seq (:data img)) (seq (:data other)))))))
 
 (fact "Render background color"
   (offscreen-render 160 120 (clear (->RGB 1.0 0.0 0.0))) => (is-image "test/sfsim25/fixtures/red.png"))
