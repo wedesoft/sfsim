ff0767d141ddbfafefdcebf34336d857ef93ef31
Jan Wedekind (Dr) <jan@wedesoft.de>
2021-12-12 23:12:18 +0000

    Load lookup tables into Clojure program

--- etc/scratch.clj
+++ etc/scratch.clj
@@ -691,28 +691,39 @@ void main()
          ; (spit-image (format "sun%04d.png" @n) img)
          (show-image img)
          (println (swap! n inc))))
-
+;
+; ---
 ; render planet
+
+(def w2 119)
+(def -w2 (- w2))
+(def w (inc (* 2 w2)))
+
+(def img {:width w :height w :data (int-array (sqr w))})
+
 (def radius 6378000.0)
 (def height 100000.0)
 (def earth #:sfsim25.sphere{:centre (matrix [0 0 0]) :radius radius :sfsim25.atmosphere/height height :sfsim25.atmosphere/brightness (matrix [0.3 0.3 0.3])})
 (def mie #:sfsim25.atmosphere{:scatter-base (matrix [2e-5 2e-5 2e-5]) :scatter-scale 1200 :scatter-g 0.76 :scatter-quotient 0.9})
 (def rayleigh #:sfsim25.atmosphere{:scatter-base (matrix [5.8e-6 13.5e-6 33.1e-6]) :scatter-scale 8000})
 
+; TODO: need to reverse order of components of each matrix
+
 (def data (slurp-floats "data/atmosphere/transmittance.scatter"))
 (def size (int (Math/sqrt (/ (count data) 3))))
 (def transmittance-space-earth (transmittance-space earth size))
-(def T (interpolation-table (mapv vec (partition size (map (comp matrix vector) (partition 3 data)))) transmittance-space-earth))
+(def T (interpolation-table (mapv vec (partition size (map (comp matrix reverse) (partition 3 data)))) transmittance-space-earth))
 
 (def data (slurp-floats "data/atmosphere/surface-radiance.scatter"))
 (def size (int (Math/sqrt (/ (count data) 3))))
 (def surface-radiance-space-earth (surface-radiance-space earth size))
-(def E (atom (interpolation-table (mapv vec (partition size (map (comp matrix vector) (partition 3 data)))) surface-radiance-space-earth)))
+(def E (atom (interpolation-table (mapv vec (partition size (map (comp matrix reverse) (partition 3 data)))) surface-radiance-space-earth)))
 
 (def data (slurp-floats "data/atmosphere/ray-scatter.scatter"))
 (def size (int (Math/pow (/ (count data) 3) 0.25)))
 (def ray-scatter-space-earth (ray-scatter-space earth size))
-(def flat (mapv (comp matrix vector) (partition 3 data)))
+(def arr (mapv vec (partition (* size size) (mapv (comp matrix reverse) (partition 3 data)))))
+(def S (atom (interpolation-table (convert-2d-to-4d arr) ray-scatter-space-earth)))
 
 (def m 0.5)
 
