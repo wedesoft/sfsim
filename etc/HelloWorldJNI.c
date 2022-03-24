81f2e1668a2cd6d8da9a75c4e768137658ccc9bc
Jan Wedekind (Dr) <jan@wedesoft.de>
2022-03-24 20:40:48 +0000

    Remove hello world JNI example

--- etc/HelloWorldJNI.c
+++
@@ -1,6 +0,0 @@
-#include "HelloWorldJNI.h"
-
-JNIEXPORT void JNICALL Java_HelloWorldJNI_sayHello(JNIEnv *env, jobject thisObject)
-{
-  printf("Hello world from C!\n");
-}
--- etc/test.clj
+++
@@ -1,2 +0,0 @@
-(import 'HelloWorldJNI)
-(.sayHello (HelloWorldJNI.))
