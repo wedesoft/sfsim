987432da5e0ec133ac77db95564e4882be7c7a75
Jan Wedekind (Dr) <jan@wedesoft.de>
2022-06-01 23:08:09 +0100

    Implemented intersections of ray with shell

--- resources/shaders/core/ray_shell.glsl
+++ resources/shaders/core/ray_shell.glsl
@@ -5,15 +5,15 @@ vec4 ray_shell(vec3 centre, float inner_radius, float outer_radius, vec3 origin,
 {
   vec2 outer_intersection = ray_sphere(centre, outer_radius, origin, direction);
   vec2 inner_intersection = ray_sphere(centre, inner_radius, origin, direction);
-  if (outer_intersection.t > 0) {
-    if (inner_intersection.t > 0) {
-      float a = outer_intersection.s;
-      float b = inner_intersection.s;
-      float c = inner_intersection.s + inner_intersection.t;
-      float d = outer_intersection.s + outer_intersection.t;
+  if (inner_intersection.t > 0) {
+    float a = outer_intersection.s;
+    float b = inner_intersection.s;
+    float c = inner_intersection.s + inner_intersection.t;
+    float d = outer_intersection.s + outer_intersection.t;
+    if (b > a)
       return vec4(a, b - a, c, d - c);
-    } else
-      return vec4(outer_intersection.s, outer_intersection.t, 0, 0);
+    else
+      return vec4(c, d - c, 0, 0);
   } else
-    return vec4(0, 0, 0, 0);
+    return vec4(outer_intersection.s, outer_intersection.t, 0, 0);
 }
