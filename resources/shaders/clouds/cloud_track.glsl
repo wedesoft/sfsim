a4cfa6bb8aa66048d6ddd01f431b4fcd09ab5df8
Jan Wedekind (Dr) <jan@wedesoft.de>
2022-06-12 21:55:49 +0100

    Determine direction vector in cloud shader

--- resources/shaders/clouds/cloud_track.glsl
+++ resources/shaders/clouds/cloud_track.glsl
@@ -4,7 +4,9 @@ float transmittance_forward(vec3 point, vec3 direction);
 
 vec3 cloud_track(vec3 p, vec3 q, int n, vec3 light)
 {
-  float transmittance_p = transmittance_forward(p, vec3(0, 0, 0));
-  float transmittance_q = transmittance_forward(q, vec3(0, 0, 0));
+  float dist = distance(p, q);
+  vec3 direction = (q - p) / dist;
+  float transmittance_p = transmittance_forward(p, direction);
+  float transmittance_q = transmittance_forward(q, direction);
   return light * transmittance_p / transmittance_q;
 }
