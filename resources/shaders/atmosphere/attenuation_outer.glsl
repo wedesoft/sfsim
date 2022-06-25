42a21400fc3d4030811980c982fa7ff52c7b4455
Jan Wedekind (Dr) <jan@wedesoft.de>
2022-06-25 22:03:29 +0100

    Move amplification uniform back into atmospheric fragment shader.

--- resources/shaders/atmosphere/attenuation_outer.glsl
+++ resources/shaders/atmosphere/attenuation_outer.glsl
@@ -1,14 +1,13 @@
 #version 410 core
 
-uniform float amplification;
-
 vec3 transmittance_outer(vec3 point, vec3 direction);
 vec3 ray_scatter_outer(vec3 light_direction, vec3 point, vec3 direction);
 
+// Shader combining atmospheric transmittance and in-scattering.
 vec3 attenuation_outer(vec3 light_direction, vec3 point, vec3 direction, vec3 incoming)
 {
   vec3 atmospheric_contribution = ray_scatter_outer(light_direction, point, direction);
   vec3 remaining_incoming = incoming * transmittance_outer(point, direction);
-  return atmospheric_contribution * amplification + remaining_incoming;
+  return atmospheric_contribution + remaining_incoming;
 }
 
--- resources/shaders/atmosphere/fragment.glsl
+++ resources/shaders/atmosphere/fragment.glsl
@@ -6,6 +6,7 @@ uniform float polar_radius;
 uniform float max_height;
 uniform float specular;
 uniform vec3 origin;
+uniform float amplification;
 
 in VS_OUT
 {
@@ -26,15 +27,15 @@ vec3 stretch(vec3 v)
 void main()
 {
   vec3 direction = normalize(fs_in.direction);
-  float glare = pow(max(0, dot(direction, light)), specular);
+  float glare = pow(max(0, dot(direction, light)), specular) / amplification;
   vec3 scaled_origin = stretch(origin);
   vec3 scaled_direction = normalize(stretch(direction));
   vec2 atmosphere_intersection = ray_sphere(vec3(0, 0, 0), radius + max_height, scaled_origin, scaled_direction);
   if (atmosphere_intersection.y > 0) {
     vec3 scaled_point = scaled_origin + atmosphere_intersection.x * scaled_direction;
     vec3 scaled_light = normalize(stretch(light));
-    fragColor = attenuation_outer(scaled_light, scaled_point, scaled_direction, vec3(glare, glare, glare));
+    fragColor = amplification * attenuation_outer(scaled_light, scaled_point, scaled_direction, vec3(glare, glare, glare));
   } else {
-    fragColor = vec3(glare, glare, glare);
+    fragColor = amplification * vec3(glare, glare, glare);
   };
 }
