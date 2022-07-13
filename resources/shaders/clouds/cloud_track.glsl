2f58d5493210f6fb1667d6bf863632d7f88944bf
Jan Wedekind (Dr) <jan@wedesoft.de>
2022-07-13 20:44:51 +0100

    Use ray_scatter_track in cloud_track.

--- resources/shaders/clouds/cloud_track.glsl
+++ resources/shaders/clouds/cloud_track.glsl
@@ -5,7 +5,7 @@ uniform int cloud_samples;
 
 bool is_above_horizon(vec3 point, vec3 direction);
 vec3 transmittance_track(vec3 p, vec3 q);
-vec3 ray_scatter_forward(vec3 point, vec3 direction, vec3 light_direction, bool above_horizon);
+vec3 ray_scatter_track(vec3 light_direction, vec3 p, vec3 q);
 float cloud_density(vec3 point);
 vec3 cloud_shadow(vec3 point, vec3 light_direction);
 float phase(float g, float mu);
@@ -23,9 +23,7 @@ vec3 cloud_track(vec3 light_direction, vec3 p, vec3 q, vec3 incoming)
       vec3 b = a + delta;
       vec3 c = 0.5 * (a + b);
       vec3 transmittance_atmosphere = transmittance_track(a, b);
-      vec3 ray_scatter_a = ray_scatter_forward(a, direction, light_direction, above);
-      vec3 ray_scatter_b = ray_scatter_forward(b, direction, light_direction, above);
-      vec3 ray_scatter_atmosphere = ray_scatter_a - ray_scatter_b * transmittance_atmosphere;
+      vec3 ray_scatter_atmosphere = ray_scatter_track(light_direction, a, b);
       float density = cloud_density(c);
       float transmittance_cloud = exp(-density * stepsize);
       vec3 intensity = cloud_shadow(c, light_direction);
