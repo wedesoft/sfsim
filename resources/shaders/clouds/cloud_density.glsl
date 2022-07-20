b03fece451c6063678c4d7293817fddfd57577b8
Jan Wedekind (Dr) <jan@wedesoft.de>
2022-07-20 20:36:19 +0100

    Remove unnecessary cloud layer check from cloud_density function.

--- resources/shaders/clouds/cloud_density.glsl
+++ resources/shaders/clouds/cloud_density.glsl
@@ -11,10 +11,7 @@ uniform sampler1D cloud_profile;
 float cloud_density(vec3 point)
 {
   float dist = length(point);
-  if (dist >= radius + cloud_bottom && dist <= radius + cloud_top) {
-    float noise = texture(worley, point / cloud_size).r;
-    float threshold = 1 - texture(cloud_profile, (dist - radius - cloud_bottom) / (cloud_top - cloud_bottom)).r;
-    return max(noise - threshold, 0) * cloud_multiplier;
-  } else
-    return 0.0;
+  float noise = texture(worley, point / cloud_size).r;
+  float threshold = 1 - texture(cloud_profile, (dist - radius - cloud_bottom) / (cloud_top - cloud_bottom)).r;
+  return max(noise - threshold, 0) * cloud_multiplier;
 }
