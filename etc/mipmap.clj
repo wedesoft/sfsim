59f4bb344c53f555f21ddf8b62f557cd4a39104d
Jan Wedekind (Dr) <jan@wedesoft.de>
2022-07-30 14:27:09 +0100

    Change chessboard texture

--- etc/mipmap.clj
+++ etc/mipmap.clj
@@ -3,7 +3,7 @@
 (import '[org.lwjgl.opengl Display DisplayMode PixelFormat GL11 GL12 GL30]
         '[org.lwjgl.input Keyboard])
 
-(Display/setDisplayMode (DisplayMode. 640 480))
+(Display/setDisplayMode (DisplayMode. 256 256))
 (Display/create)
 (Keyboard/create)
 
@@ -28,9 +28,13 @@ void main()
   fragColor = textureLod(tex, uv_fragment, lod).rgb;
 }")
 
-(def data (for [i (range 4) j (range 4)] (if (zero? (mod (+ i j) 2)) 1 0)))
-(def tex (make-float-texture-2d {:width 4 :height 4 :data (float-array data)}))
-(with-2d-texture (:texture tex) (GL30/glGenerateMipmap GL11/GL_TEXTURE_2D))
+(def size 32)
+(def data (for [i (range size) j (range size)] (if (zero? (mod (+ (quot i 2) (quot j 2)) 2)) 1 0)))
+(def tex (make-float-texture-2d {:width size :height size :data (float-array data)}))
+(with-2d-texture (:texture tex)
+  (GL11/glTexParameteri GL11/GL_TEXTURE_2D GL11/GL_TEXTURE_MIN_FILTER GL11/GL_LINEAR_MIPMAP_LINEAR)
+  (GL11/glTexParameteri GL11/GL_TEXTURE_2D GL11/GL_TEXTURE_MAG_FILTER GL11/GL_LINEAR_MIPMAP_LINEAR)
+  (GL30/glGenerateMipmap GL11/GL_TEXTURE_2D GL11/GL_LINEAR_MIPMAP_LINEAR))
 
 (def indices [0 1 3 2])
 (def vertices [-1.0 -1.0 0.5 0.0 0.0, 1.0 -1.0 0.5 1.0 0.0, -1.0 1.0 0.5 0.0 1.0, 1.0 1.0 0.5 1.0 1.0])
@@ -46,8 +50,8 @@ void main()
                     event-key (Keyboard/getEventKey)]
                 (swap! keystates assoc event-key state)))
        (when (@keystates Keyboard/KEY_Q) (reset! lod (max 0.0 (- @lod 0.0005))))
-       (when (@keystates Keyboard/KEY_A) (reset! lod (min 4.0 (+ @lod 0.0005))))
-       (onscreen-render 640 480
+       (when (@keystates Keyboard/KEY_A) (reset! lod (min 8.0 (+ @lod 0.0005))))
+       (onscreen-render (Display/getWidth) (Display/getHeight)
                         (clear (matrix [0.0 0.0 0.0]))
                         (use-program program)
                         (uniform-sampler program :tex 0)
@@ -55,7 +59,6 @@ void main()
                         (use-textures tex)
                         (render-quads vao))
        (print "\r" (format "%5.3f    " @lod)))
-
 
 (destroy-texture tex)
 (destroy-vertex-array-object vao)
