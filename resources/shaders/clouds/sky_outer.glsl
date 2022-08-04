e1b6972b972dee9b2b2181c78e17557b515c497a
Jan Wedekind (Dr) <jan@wedesoft.de>
2022-08-04 22:41:30 +0100

    Minor changes

--- resources/shaders/clouds/sky_outer.glsl
+++ resources/shaders/clouds/sky_outer.glsl
@@ -19,10 +19,10 @@ vec3 sky_outer(vec3 light_direction, vec3 origin, vec3 direction, vec3 incoming)
       vec4 cloud_intersections = ray_shell(vec3(0, 0, 0), radius + cloud_bottom, radius + cloud_top, origin, direction);
       if (cloud_intersections.y > 0) {
         float a = cloud_intersections.x;
-        float b = a + cloud_intersections.y;
+        float b = cloud_intersections.x + cloud_intersections.y;
         if (cloud_intersections.w > 0) {
           float c = cloud_intersections.z;
-          float d = c + cloud_intersections.w;
+          float d = cloud_intersections.z + cloud_intersections.w;
           incoming = attenuation_outer(light_direction, origin, direction, d, incoming);
           incoming = cloud_track(light_direction, origin, direction, c, d, incoming);
           incoming = attenuation_track(light_direction, origin, direction, b, c, incoming);
