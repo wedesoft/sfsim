43d55f5a9a174c5d550d542e0f26955605584aa2
Jan Wedekind (Dr) <jan@wedesoft.de>
2022-08-25 23:19:36 +0100

    Add missing use of level of detail to cloud_density function.

--- resources/shaders/clouds/cloud_density.glsl
+++ resources/shaders/clouds/cloud_density.glsl
@@ -11,7 +11,7 @@ uniform sampler1D cloud_profile;
 float cloud_density(vec3 point, float lod)
 {
   float dist = length(point);
-  float noise = texture(worley, point / cloud_scale).r;
+  float noise = textureLod(worley, point / cloud_scale, lod).r;
   float threshold = 1 - texture(cloud_profile, (dist - radius - cloud_bottom) / (cloud_top - cloud_bottom)).r;
   return max(noise - threshold, 0) * cloud_multiplier;
 }
