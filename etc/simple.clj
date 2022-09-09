531fda46db7031a93b419970f413ea45d32fb4f9
Jan Wedekind (Dr) <jan@wedesoft.de>
2022-09-09 20:55:39 +0100

    Add 3D Worley noise to prototype.

--- etc/simple.clj
+++ etc/simple.clj
@@ -5,7 +5,8 @@
          '[sfsim25.matrix :refer :all]
          '[sfsim25.render :refer :all]
          '[sfsim25.atmosphere :refer :all]
-         '[sfsim25.shaders :as shaders])
+         '[sfsim25.shaders :as shaders]
+         '[sfsim25.util :refer :all])
 
 (import '[org.lwjgl.opengl Display DisplayMode PixelFormat]
         '[org.lwjgl.input Keyboard])
@@ -23,12 +24,17 @@
 (def light (atom 1.559))
 (def position (atom (matrix [0 (* -0 radius) (+ (* 1 radius) 500)])))
 (def orientation (atom (q/rotation (to-radians 90) (matrix [1 0 0]))))
+(def threshold (atom 0.15))
+(def multiplier (atom 1.2))
 (def z-near 1000)
 (def z-far (* 2.0 radius))
-
+(def worley-size 128)
 (def keystates (atom {}))
-
 (def projection (projection-matrix (Display/getWidth) (Display/getHeight) z-near z-far (to-radians 60)))
+
+(def data (slurp-floats "values.raw"))
+(def W (make-float-texture-3d {:width worley-size :height worley-size :depth worley-size :data data}))
+(generate-mipmap W)
 
 (def fragment
 "#version 410 core
@@ -41,6 +47,10 @@ uniform float max_height;
 uniform float cloud_step;
 uniform float cloud_bottom;
 uniform float cloud_top;
+uniform float cloud_scale;
+uniform float cloud_multiplier;
+uniform float threshold;
+uniform sampler3D worley;
 in highp vec3 point;
 
 in VS_OUT
@@ -69,10 +79,14 @@ void main()
   float step = atmosphere.y / steps;
   vec3 point = origin + direction * (atmosphere.x + atmosphere.y - step * 0.5);
   for (int i=0; i<steps; i++) {
-    float r = length(point - i * direction * step);
+    vec3 pos = point - i * direction * step;
+    float r = length(pos);
     if (r >= radius + cloud_bottom && r <= radius + cloud_top) {
-      float t = exp(-step * 0.0001);
-      background = background * t + vec3(1, 1, 1) * (1 - t);
+      float noise = (texture(worley, pos / cloud_scale).r - threshold) * cloud_multiplier;
+      if (noise > 0) {
+        float t = exp(-step * 0.0001 * noise);
+        background = background * t + vec3(1, 1, 1) * (1 - t);
+      };
     };
   };
   fragColor = background;
@@ -89,9 +103,11 @@ void main()
 (uniform-matrix4 program :projection projection)
 (uniform-float program :radius radius)
 (uniform-float program :max_height max-height)
-(uniform-float program :cloud_step 500)
+(uniform-float program :cloud_step 100)
 (uniform-float program :cloud_bottom 2000)
 (uniform-float program :cloud_top 5000)
+(uniform-float program :cloud_scale 10000)
+(uniform-sampler program :worley 0)
 
 (def t0 (atom (System/currentTimeMillis)))
 (while (not (Display/isCloseRequested))
@@ -101,6 +117,8 @@ void main()
                 (swap! keystates assoc event-key state)))
        (let [t1        (System/currentTimeMillis)
              dt        (- t1 @t0)
+             tr        (if (@keystates Keyboard/KEY_Q) 0.001 (if (@keystates Keyboard/KEY_A) -0.001 0))
+             tm        (if (@keystates Keyboard/KEY_E) 0.001 (if (@keystates Keyboard/KEY_D) -0.001 0))
              ra        (if (@keystates Keyboard/KEY_NUMPAD2) 0.001 (if (@keystates Keyboard/KEY_NUMPAD8) -0.001 0))
              rb        (if (@keystates Keyboard/KEY_NUMPAD4) 0.001 (if (@keystates Keyboard/KEY_NUMPAD6) -0.001 0))
              rc        (if (@keystates Keyboard/KEY_NUMPAD1) 0.001 (if (@keystates Keyboard/KEY_NUMPAD3) -0.001 0))
@@ -108,13 +126,22 @@ void main()
          (swap! orientation q/* (q/rotation (* dt ra) (matrix [1 0 0])))
          (swap! orientation q/* (q/rotation (* dt rb) (matrix [0 1 0])))
          (swap! orientation q/* (q/rotation (* dt rc) (matrix [0 0 1])))
+         (swap! threshold + (* dt tr))
+         (swap! multiplier + (* dt tm))
          (swap! position add (mul dt v (q/rotate-vector @orientation (matrix [0 0 -1]))))
          (onscreen-render (Display/getWidth) (Display/getHeight)
                           (clear (matrix [0 1 0]))
+                          (use-program program)
+                          (use-textures W)
                           (uniform-matrix4 program :transform (transformation-matrix (quaternion->matrix @orientation) @position))
                           (uniform-vector3 program :origin @position)
+                          (uniform-float program :threshold @threshold)
+                          (uniform-float program :cloud_multiplier @multiplier)
                           (render-quads vao))
-         (print "\rheight" (format "%.3f    " (- (norm @position) radius)))
+         (print "\rheight" (format "%.3f" (- (norm @position) radius))
+                "threshold" (format "%.3f" @threshold)
+                "multiplier" (format "%.3f" @multiplier)
+                "      ")
          (flush)
          (swap! t0 + dt)))
 
