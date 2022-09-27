8f095d5b945f5846dae6876d2bc9488a0483efc7
Jan Wedekind (Dr) <jan@wedesoft.de>
2022-09-27 22:35:01 +0100

    Shadow map creation not working

--- etc/shadow.clj
+++ etc/shadow.clj
@@ -20,9 +20,13 @@
 (GL42/glTexStorage2D GL11/GL_TEXTURE_2D 1 GL30/GL_DEPTH_COMPONENT32F size size)
 (GL11/glTexParameteri GL11/GL_TEXTURE_2D GL11/GL_TEXTURE_MIN_FILTER GL11/GL_LINEAR)
 (GL11/glTexParameteri GL11/GL_TEXTURE_2D GL11/GL_TEXTURE_MAG_FILTER GL11/GL_LINEAR)
+(GL11/glTexParameteri GL11/GL_TEXTURE_2D GL11/GL_TEXTURE_WRAP_S GL12/GL_CLAMP_TO_EDGE)
+(GL11/glTexParameteri GL11/GL_TEXTURE_2D GL11/GL_TEXTURE_WRAP_T GL12/GL_CLAMP_TO_EDGE)
 (GL11/glTexParameteri GL11/GL_TEXTURE_2D GL14/GL_TEXTURE_COMPARE_MODE GL14/GL_COMPARE_R_TO_TEXTURE)
 (GL11/glTexParameteri GL11/GL_TEXTURE_2D GL14/GL_TEXTURE_COMPARE_FUNC GL11/GL_LEQUAL)
 (GL32/glFramebufferTexture GL30/GL_FRAMEBUFFER GL30/GL_DEPTH_ATTACHMENT tex 0)
+(GL11/glDrawBuffer GL11/GL_NONE)
+(GL11/glReadBuffer GL11/GL_NONE)
 (GL30/glBindFramebuffer GL30/GL_FRAMEBUFFER 0)
 
 (def vert
@@ -37,6 +41,7 @@ void main(void)
 
 (def frag
 "#version 420 core
+in vec4 coord;
 void main(void)
 {
 }")
@@ -45,10 +50,13 @@ void main(void)
 (def vertices [-0.5 -0.5 0.5, 0.5 -0.5 0.5, -0.5 0.5 0.5, 0.5 0.5 0.5])
 (def program (make-program :vertex [vert] :fragment [frag]))
 (def vao (make-vertex-array-object program indices vertices [:point 3]))
-(GL11/glClearDepth 0.0)
+(GL11/glViewport 0 0 size size)
+(GL30/glBindFramebuffer GL30/GL_FRAMEBUFFER shadow)
+(GL11/glClearDepth 1.0)
 (GL11/glClear GL11/GL_DEPTH_BUFFER_BIT)
 (use-program program)
 (render-quads vao)
+(GL30/glBindFramebuffer GL30/GL_FRAMEBUFFER 0)
 
 (def img
   (with-texture GL11/GL_TEXTURE_2D tex
@@ -58,8 +66,9 @@ void main(void)
       (.get buf data)
       {:width size :height size :data data})))
 
-(show-floats {:width size :height size :data (float-array (map #(* 255 %) (:data img)))})
-
+(show-floats img)
+(apply min (:data img))
+(apply max (:data img))
 
 (destroy-vertex-array-object vao)
 (destroy-program program)
