f7f874bc118c7fcb5f40d019c636e48837e1ebfa
Jan Wedekind (Dr) <jan@wedesoft.de>
2022-09-27 22:15:07 +0100

    Trying to create shadow map

---
+++ etc/shadow.clj
@@ -0,0 +1,68 @@
+(require '[sfsim25.render :refer :all]
+         '[sfsim25.util :refer :all])
+
+(import '[org.lwjgl.opengl Pbuffer Display DisplayMode PixelFormat GL11 GL12 GL13 GL14 GL15 GL20 GL30 GL32 GL40 GL42 GL43 GL45]
+        '[org.lwjgl BufferUtils])
+
+(def width 320)
+(def height 240)
+
+(def size 256)
+
+(def pixels (BufferUtils/createIntBuffer (* width height)))
+(def pbuffer (Pbuffer. width height (PixelFormat. 24 8 24 0 0) nil nil))
+(.makeCurrent pbuffer)
+
+(def shadow (GL30/glGenFramebuffers))
+(GL30/glBindFramebuffer GL30/GL_FRAMEBUFFER shadow)
+(def tex (GL11/glGenTextures))
+(GL11/glBindTexture GL11/GL_TEXTURE_2D tex)
+(GL42/glTexStorage2D GL11/GL_TEXTURE_2D 1 GL30/GL_DEPTH_COMPONENT32F size size)
+(GL11/glTexParameteri GL11/GL_TEXTURE_2D GL11/GL_TEXTURE_MIN_FILTER GL11/GL_LINEAR)
+(GL11/glTexParameteri GL11/GL_TEXTURE_2D GL11/GL_TEXTURE_MAG_FILTER GL11/GL_LINEAR)
+(GL11/glTexParameteri GL11/GL_TEXTURE_2D GL14/GL_TEXTURE_COMPARE_MODE GL14/GL_COMPARE_R_TO_TEXTURE)
+(GL11/glTexParameteri GL11/GL_TEXTURE_2D GL14/GL_TEXTURE_COMPARE_FUNC GL11/GL_LEQUAL)
+(GL32/glFramebufferTexture GL30/GL_FRAMEBUFFER GL30/GL_DEPTH_ATTACHMENT tex 0)
+(GL30/glBindFramebuffer GL30/GL_FRAMEBUFFER 0)
+
+(def vert
+"#version 420 core
+in vec3 point;
+out vec4 coord;
+void main(void)
+{
+  gl_Position = vec4(point, 1);
+  coord = vec4(point, 1);
+}")
+
+(def frag
+"#version 420 core
+void main(void)
+{
+}")
+
+(def indices [0 1 3 2])
+(def vertices [-0.5 -0.5 0.5, 0.5 -0.5 0.5, -0.5 0.5 0.5, 0.5 0.5 0.5])
+(def program (make-program :vertex [vert] :fragment [frag]))
+(def vao (make-vertex-array-object program indices vertices [:point 3]))
+(GL11/glClearDepth 0.0)
+(GL11/glClear GL11/GL_DEPTH_BUFFER_BIT)
+(use-program program)
+(render-quads vao)
+
+(def img
+  (with-texture GL11/GL_TEXTURE_2D tex
+    (let [buf  (BufferUtils/createFloatBuffer (* size size))
+          data (float-array (* size size))]
+      (GL11/glGetTexImage GL11/GL_TEXTURE_2D 0 GL11/GL_RED GL11/GL_FLOAT buf)
+      (.get buf data)
+      {:width size :height size :data data})))
+
+(show-floats {:width size :height size :data (float-array (map #(* 255 %) (:data img)))})
+
+
+(destroy-vertex-array-object vao)
+(destroy-program program)
+(GL30/glDeleteFramebuffers shadow)
+(.releaseContext pbuffer)
+(.destroy pbuffer)
