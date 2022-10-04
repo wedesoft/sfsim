726c5775cf16b783eee5506f392c123190ba2ec5
Jan Wedekind (Dr) <jan@wedesoft.de>
2022-10-04 22:17:05 +0100

    Shadow prototype working with coordinate hack

--- etc/shadow.clj
+++ etc/shadow.clj
@@ -52,20 +52,21 @@ in vec4 pos;
 out vec3 fragColor;
 void main(void)
 {
-  // float shade = textureProj(shadow_map, shadow_map_matrix * pos);
-  // float brightness = max(light.z, 0) * (0.7 * shade + 0.3);
   vec4 pos2 = shadow_map_matrix * pos;
+  //float shade = textureProj(shadow_map, pos2);
+  //float brightness = max(light.z, 0) * (0.5 * shade + 0.1) + 0.1 * (4 + pos.z);
   vec2 p = pos2.xy / pos2.w;
+  p.y = 1.0 - p.y;
   float brightness = texture(shadow_map, p).r * 0.8 + 0.2 + 0.1 * (4 + pos.z);
+  float red = 0;
   if (p.x < 0 || p.x > 1 || p.y < 0 || p.y > 1)
-    brightness = brightness - 0.1;
-  fragColor = vec3(brightness, brightness, brightness);
+    red = 1;
+  fragColor = vec3(red, brightness, brightness);
 }")
 
 (def projection (projection-matrix size size 2 5 (to-radians 60)))
 (def transform (identity-matrix 4))
-;(def light-vector (normalize (matrix [1 1 2])))
-(def light-vector (normalize (matrix [0 0 1])))
+(def light-vector (normalize (matrix [1 1 2])))
 (def shadow (shadow-matrices projection transform light-vector))
 
 (def indices [0 1 3 2 4 5 7 6])
@@ -74,29 +75,33 @@ void main(void)
 (def program-main (make-program :vertex [vertex-scene] :fragment [fragment-scene]))
 (def vao (make-vertex-array-object program-main indices vertices [:point 3]))
 
+(def shadow-map (texture-render-depth
+                  size size
+                  (clear)
+                  (use-program program-shadow)
+                  (uniform-matrix4 program-shadow :shadow_ndc_matrix (:shadow-ndc-matrix shadow))
+                  (render-quads vao)))
+
+(show-floats
+ (with-texture (:target shadow-map) (:texture shadow-map)
+   (let [buf  (BufferUtils/createFloatBuffer (* size size))
+         data (float-array (* size size))]
+     (GL11/glGetTexImage GL11/GL_TEXTURE_2D 0 GL11/GL_DEPTH_COMPONENT GL11/GL_FLOAT buf)
+     (.get buf data)
+     {:width size :height size :data data})))
+
+
 (onscreen-render size size
-                 (let [shadow-map (texture-render-depth
-                                    size size
-                                    (clear)
-                                    (use-program program-shadow)
-                                    (uniform-matrix4 program-shadow :shadow_ndc_matrix (:shadow-ndc-matrix shadow))
-                                    (render-quads vao))]
-                   (show-floats
-                     (with-texture (:target shadow-map) (:texture shadow-map)
-                       (let [buf  (BufferUtils/createFloatBuffer (* size size))
-                             data (float-array (* size size))]
-                         (GL11/glGetTexImage GL11/GL_TEXTURE_2D 0 GL11/GL_DEPTH_COMPONENT GL11/GL_FLOAT buf)
-                         (.get buf data)
-                         {:width size :height size :data data})))
-                   (clear (matrix [0.0 0.0 0.0]))
-                   (use-program program-main)
-                   (uniform-sampler program-main :shadow_map 0)
-                   (uniform-matrix4 program-main :projection projection)
-                   (uniform-matrix4 program-main :shadow_map_matrix (:shadow-map-matrix shadow))
-                   (uniform-vector3 program-main :light light-vector)
-                   (use-textures shadow-map)
-                   (render-quads vao)
-                   (destroy-texture shadow-map)))
+                 (clear (matrix [0.0 0.0 0.0]))
+                 (use-program program-main)
+                 (uniform-sampler program-main :shadow_map 0)
+                 (uniform-matrix4 program-main :projection projection)
+                 (uniform-matrix4 program-main :shadow_map_matrix (:shadow-map-matrix shadow))
+                 (uniform-vector3 program-main :light light-vector)
+                 (use-textures shadow-map)
+                 (render-quads vao))
+
+(destroy-texture shadow-map)
 
 (destroy-vertex-array-object vao)
 (destroy-program program-main)
