a7601becf328c2e05f93135e7ec2a90bdda413ce
Jan Wedekind (Dr) <jan@wedesoft.de>
2022-10-10 22:40:06 +0100

    Trying out non-linear mappings from Bruneton's paper

---
+++ etc/m.clj
@@ -0,0 +1,32 @@
+(require '[clojure.math :refer (sqrt exp)])
+(require '[clojure.core.matrix :refer (matrix dot)])
+(require '[clojure.core.matrix.linear :refer (norm)])
+(require '[gnuplot.core :as g])
+(def Rg 6360000)
+(def Rt 6420000)
+(def H (sqrt (- (* Rt Rt) (* Rg Rg))))
+
+(defn sqr [v] (* v v))
+(defn r [x] (norm x))
+(defn rho [r] (sqrt (- (sqr r) (sqr Rg))))
+(defn mu [x v] (/ (dot v x) (r x)))
+(defn mu-s [x s] (/ (dot s x) (r x)))
+(defn nu [v s] (dot v s))
+(defn Delta [r mu] (- (* (sqr r) (sqr mu)) (sqr (rho r))))
+
+(defn u-r [r] (/ (rho r) H))
+(defn u-mu [r mu]
+  (let [D (Delta r mu)]
+    (if (and (< mu 0) (> D 0))
+      (+ 0.5 (/ (+ (* r mu) (sqrt D)) (* 2 (rho r))))
+      (- 0.5 (/ (- (* r mu) (sqrt (+ D (sqr H)))) (+ (* 2 (rho r)) (* 2 H)))))))
+(defn u-mu-s [mu-s] (/ (- 1 (exp (- 0 (* 3 mu-s) 0.6))) (- 1 (exp -3.6))))
+(defn u-nu [nu] (/ (+ 1 nu) 2))
+
+
+(g/raw-plot! [[:plot (g/list ["-" :title "u-mu-s" :with :lines]
+                             ["-" :title "u-r" :with :lines]
+                             ["-" :title "u-mu" :with :lines])]]
+             [(mapv (fn [mu-s] [mu-s (u-mu-s mu-s)]) (range -0.2 1.0 0.001))
+              (mapv (fn [r] [(- (* 2.0 (/ (- r Rg) (- Rt Rg))) 1) (u-r r)]) (range Rg Rt 100))
+              (mapv (fn [mu] [mu (u-mu 6361000 mu)]) (range -1.0 1.0 0.001))])
