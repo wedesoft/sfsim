b6e20561a15f0d811381d45c42d82b3ef8f81568
Jan Wedekind (Dr) <jan@wedesoft.de>
2023-01-07 22:23:20 +0000

    Render white sphere

---
+++ etc/cloudy.clj
@@ -0,0 +1,132 @@
+(require '[clojure.math :refer (to-radians)]
+         '[clojure.core.matrix :refer (matrix add mul)]
+         '[clojure.core.matrix.linear :refer (norm)]
+         '[sfsim25.render :refer :all]
+         '[sfsim25.atmosphere :refer :all]
+         '[sfsim25.matrix :refer :all]
+         '[sfsim25.quaternion :as q]
+         '[sfsim25.util :refer :all]
+         '[sfsim25.shaders :as shaders])
+
+(import '[org.lwjgl.opengl Display DisplayMode]
+        '[org.lwjgl.input Keyboard])
+
+(set! *unchecked-math* true)
+
+(Display/setTitle "scratch")
+(Display/setDisplayMode (DisplayMode. (/ 1920 4) (/ 1080 4)))
+;(Display/setDisplayMode (DisplayMode. 1280 720))
+(Display/create)
+
+(Keyboard/create)
+
+(def radius 6378000.0)
+(def max-height 35000.0)
+(def worley-size 128)
+(def z-near 1000.0)
+(def z-far 125000.0)
+(def fov 45.0)
+(def height-size 32)
+(def elevation-size 127)
+(def light-elevation-size 32)
+(def heading-size 8)
+(def transmittance-height-size 64)
+(def transmittance-elevation-size 255)
+
+(def projection (projection-matrix (Display/getWidth) (Display/getHeight) z-near z-far (to-radians fov)))
+
+(def position (atom (matrix [0 (* -0 radius) (+ (* 1 radius) 1000)])))
+(def orientation (atom (q/rotation (to-radians 90) (matrix [1 0 0]))))
+(def keystates (atom {}))
+
+(def data (slurp-floats "data/worley.raw"))
+(def W (make-float-texture-3d :linear :repeat {:width worley-size :height worley-size :depth worley-size :data data}))
+(generate-mipmap W)
+
+(def data (float-array [0.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 0.7 0.3 0.0]))
+(def P (make-float-texture-1d :linear :clamp data))
+
+(def data (slurp-floats "data/atmosphere/transmittance.scatter"))
+(def T (make-vector-texture-2d :linear :clamp {:width transmittance-elevation-size :height transmittance-height-size :data data}))
+
+(def data (slurp-floats "data/atmosphere/ray-scatter.scatter"))
+(def S (make-vector-texture-2d :linear :clamp {:width (* elevation-size heading-size) :height (* height-size light-elevation-size) :data data}))
+
+(def data (slurp-floats "data/atmosphere/mie-strength.scatter"))
+(def M (make-vector-texture-2d :linear :clamp {:width (* elevation-size heading-size) :height (* height-size light-elevation-size) :data data}))
+
+(def fragment
+"#version 410 core
+
+uniform mat4 projection;
+uniform mat4 transform;
+uniform vec3 origin;
+uniform float radius;
+
+in VS_OUT
+{
+  vec3 direction;
+} fs_in;
+
+out vec3 fragColor;
+
+vec2 ray_sphere(vec3 centre, float radius, vec3 origin, vec3 direction);
+
+void main()
+{
+  vec3 direction = normalize(fs_in.direction);
+  vec2 planet = ray_sphere(vec3(0, 0, 0), radius, origin, direction);
+  if (planet.y > 0) {
+    fragColor = vec3(1, 1, 1);
+  } else {
+    fragColor = vec3(0, 0, 0);
+  };
+}
+")
+
+(def program (make-program :vertex [vertex-atmosphere] :fragment [fragment shaders/ray-sphere]))
+
+(def indices [0 1 3 2])
+(def vertices (map #(* % z-far) [-4 -4 -1, 4 -4 -1, -4  4 -1, 4  4 -1]))
+(def vao (make-vertex-array-object program indices vertices [:point 3]))
+
+(use-program program)
+(uniform-matrix4 program :projection projection)
+(uniform-float program :radius radius)
+
+(def t0 (atom (System/currentTimeMillis)))
+(while (not (Display/isCloseRequested))
+       (while (Keyboard/next)
+              (let [state     (Keyboard/getEventKeyState)
+                    event-key (Keyboard/getEventKey)]
+                (swap! keystates assoc event-key state)))
+       (let [t1        (System/currentTimeMillis)
+             dt        (- t1 @t0)
+             ra        (if (@keystates Keyboard/KEY_NUMPAD2) 0.001 (if (@keystates Keyboard/KEY_NUMPAD8) -0.001 0))
+             rb        (if (@keystates Keyboard/KEY_NUMPAD4) 0.001 (if (@keystates Keyboard/KEY_NUMPAD6) -0.001 0))
+             rc        (if (@keystates Keyboard/KEY_NUMPAD1) 0.001 (if (@keystates Keyboard/KEY_NUMPAD3) -0.001 0))
+             v         (if (@keystates Keyboard/KEY_PRIOR) 5 (if (@keystates Keyboard/KEY_NEXT) -5 0))]
+         (swap! orientation q/* (q/rotation (* dt ra) (matrix [1 0 0])))
+         (swap! orientation q/* (q/rotation (* dt rb) (matrix [0 1 0])))
+         (swap! orientation q/* (q/rotation (* dt rc) (matrix [0 0 1])))
+         (swap! position add (mul dt v (q/rotate-vector @orientation (matrix [0 0 -1]))))
+         (onscreen-render (Display/getWidth) (Display/getHeight)
+                          (clear (matrix [0 1 0]))
+                          (use-program program)
+                          (uniform-matrix4 program :transform (transformation-matrix (quaternion->matrix @orientation) @position))
+                          (uniform-vector3 program :origin @position)
+                          (render-quads vao))
+         (print "\rheight" (format "%.3f" (- (norm @position) radius))
+                "dt" (format "%.3f" (* dt 0.001))
+                "      ")
+         (flush)
+         (swap! t0 + dt)))
+
+(destroy-texture W)
+(destroy-texture P)
+(destroy-texture T)
+(destroy-texture S)
+(destroy-vertex-array-object vao)
+(destroy-program program)
+
+(Display/destroy)
