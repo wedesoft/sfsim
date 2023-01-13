fce892e24eee6f4e3a119486f8af2aef930cc269
Jan Wedekind (Dr) <jan@wedesoft.de>
2023-01-13 22:43:16 +0000

    Compare splits with negative z.

--- resources/shaders/clouds/opacity-cascade-lookup.glsl
+++ resources/shaders/clouds/opacity-cascade-lookup.glsl
@@ -10,7 +10,7 @@ uniform mat4 shadow_map_matrix<%= i %>;
 float opacity_lookup(sampler2D offsets, sampler3D layers, vec3 opacity_map_coords);
 float opacity_cascade_lookup(vec4 point)
 {
-  float z = point.z;
+  float z = -point.z;
 <% (doseq [i (range n)] %>if (z >= split<%= i %> && z <= split<%= (inc i) %>) {
     vec4 map_coords = shadow_map_matrix<%= i %> * point;
     return opacity_lookup(offset<%= i %>, opacity<%= i %>, map_coords.xyz);
