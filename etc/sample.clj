2f98df8740e12773019836eb50d389b48658d653
Jan Wedekind (Dr) <jan@wedesoft.de>
2023-02-21 20:52:41 +0000

    Generate 2D Worley noise in prototype.

--- etc/sample.clj
+++ etc/sample.clj
@@ -1,9 +1,67 @@
 (require '[clojure.math :refer (to-radians cos sin PI round floor pow)]
-         '[clojure.core.matrix :refer (matrix add mul inverse mmul mget)]
+         '[clojure.core.matrix :refer (matrix array dimension-count eseq slice-view add sub mul inverse mmul mget)]
          '[clojure.core.matrix.linear :refer (norm)]
-         '[sfsim25.worley :refer :all]
          '[sfsim25.matrix :refer :all]
          '[sfsim25.util :refer :all])
+
+(defn random-point-grid
+  "Create a 3D grid with a random point in each cell"
+  ([divisions size]
+   (random-point-grid divisions size rand))
+  ([divisions size random]
+   (let [cellsize (/ size divisions)]
+     (array
+       (map (fn [j]
+                (map (fn [i]
+                         (add (matrix [(* i cellsize) (* j cellsize)])
+                              (matrix (repeatedly 2 #(random cellsize)))))
+                     (range divisions)))
+            (range divisions))))))
+
+(defn- clipped-index-and-offset
+  "Return index modulo dimension of grid and offset for corresponding coordinate"
+  [grid size dimension index]
+  (let [divisions     (dimension-count grid dimension)
+        clipped-index (if (< index divisions) (if (>= index 0) index (+ index divisions)) (- index divisions))
+        offset        (if (< index divisions) (if (>= index 0) 0 (- size)) size)]
+    [clipped-index offset]))
+
+(defn extract-point-from-grid
+  "Extract the random point from the given grid cell"
+  [grid size j i]
+  (let [[i-clip x-offset] (clipped-index-and-offset grid size 1 i)
+        [j-clip y-offset] (clipped-index-and-offset grid size 0 j)]
+    (add (slice-view (slice-view grid j-clip) i-clip) (matrix [x-offset y-offset]))))
+
+(defn closest-distance-to-point-in-grid
+  "Return distance to closest point in 3D grid"
+  [grid divisions size point]
+  (let [cellsize (/ size divisions)
+        [x y]  (eseq point)
+        [i j]  [(quot x cellsize) (quot y cellsize)]
+        points   (for [dj [-1 0 1] di [-1 0 1]] (extract-point-from-grid grid size (+ j dj) (+ i di)))]
+    (apply min (map #(norm (sub point %)) points))))
+
+(defn normalise-vector
+  "Normalise the values of a vector"
+  [values]
+  (let [maximum (apply max values)]
+    (vec (pmap #(/ % maximum) values))))
+
+(defn invert-vector
+  "Invert values of a vector"
+  [values]
+  (mapv #(- 1 %) values))
+
+(defn worley-noise
+  "Create 3D Worley noise"
+  [divisions size]
+  (let [grid (random-point-grid divisions size)]
+    (invert-vector
+      (normalise-vector
+        (for [j (range size) i (range size)]
+              (do
+                (closest-distance-to-point-in-grid grid divisions size (matrix [(+ j 0.5) (+ i 0.5)]))))))))
 
 (defn mix
   "Linear mixing of values"
@@ -25,13 +83,22 @@
 
 (def size 64)
 (def divisions 8)
-(def data (worley-noise divisions size true))
 (defn parcel [size x] (mapv vec (partition size x)))
-(def worley (mapv #(parcel size %) (parcel (* size size) data)))
+(def worley (parcel size (worley-noise divisions size)))
 (defn worley-smooth [point] (interpolate-value worley point))
+(def potential (parcel size (worley-noise divisions size)))
+(defn potential-smooth [point] (interpolate-value potential point))
 (def octaves [0.5 0.25 0.125 0.0625])
 (defn worley-octaves [point]
   (apply + (map-indexed #(* %2 (worley-smooth (mul (pow 2 %1) point))) octaves)))
+(defn potential-octaves [point]
+  (apply + (map-indexed #(* %2 (potential-smooth (mul (pow 2 %1) point))) octaves)))
+
+(def epsilon (pow 0.5 3))
+(defn gradient-x [point]
+  (- (potential-octaves (add point (matrix [1 0]))) (potential-octaves (sub point (matrix [1 0])))))
+(defn gradient-y [point]
+  (- (potential-octaves (add point (matrix [0 1]))) (potential-octaves (sub point (matrix [0 1])))))
 
 (def w 640)
-(show-floats {:width w :height w :data (float-array (for [j (range w) i (range w)] (worley-octaves (mul 0.1 (matrix [i j 0])))))})
+(show-floats {:width w :height w :data (float-array (for [j (range w) i (range w)] (worley-octaves (mul 0.1 (matrix [i j])))))})
