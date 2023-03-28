84dfee254cb22741fe9f8dfbebd534f05d768eb2
Jan Wedekind (Dr) <jan@wedesoft.de>
2023-03-28 21:38:20 +0100

    Create prototype method for generating cloud cover

--- etc/cover.clj
+++ etc/cover.clj
@@ -20,12 +20,11 @@
 (def flow-field
 "#version 410 core
 #define M_PI 3.1415926535897932384626433832795
-uniform sampler3D worley_north;
-uniform sampler3D worley_south;
 uniform float curl_scale;
 uniform float prevailing;
 uniform float whirl;
-float octaves(sampler3D noise, vec3 idx, float lod);
+float octaves_north(vec3 idx, float lod);
+float octaves_south(vec3 idx, float lod);
 float spin(float y)
 {
   float angle = asin(y);
@@ -34,8 +33,8 @@ float spin(float y)
 float flow_field(vec3 point)
 {
   float m = spin(point.y);
-  float w1 = octaves(worley_north, point / (2 * curl_scale), 0.0) * whirl;
-  float w2 = octaves(worley_south, point / (2 * curl_scale), 0.0) * whirl;
+  float w1 = octaves_north(point / (2 * curl_scale), 0.0) * whirl;
+  float w2 = octaves_south(point / (2 * curl_scale), 0.0) * whirl;
   return (w1 + prevailing) * (1 + m) / 2 - (w2 + prevailing) * (1 - m) / 2;
 }")
 
@@ -43,39 +42,53 @@ float flow_field(vec3 point)
 ; TODO: function to read out texture without lod and uniform scale
 ; TODO: noise-octaves with scale on arbitrary function
 ; TODO: varargs for noise-octaves to include lod or not
-(def noisy-potential
+(def cover-noise
 "#version 410 core
-uniform float cloud_scale;
+uniform float cover_scale;
 float clouds(vec3 idx, float lod);
-float potential(vec3 point)
+float cover(vec3 point)
 {
-  return clouds(point / (2 * cloud_scale), 0.0);
+  return clouds(point / (2 * cover_scale), 0.0);
 }")
 
-(defn cloud-cover-cubemap [& {:keys [size worley-size worley-south worley-north worley-flow flow-octaves cloud-octaves
-                                     whirl prevailing curl-scale]}]
-  (let [warp        (atom (identity-cubemap size))
+(defn cloud-cover-cubemap [& {:keys [size worley-size worley-south worley-north worley-cover flow-octaves cloud-octaves
+                                     whirl prevailing curl-scale cover-scale num-iterations flow-scale]}]
+  (let [result      (promise)
+        warp        (atom (identity-cubemap size))
         update-warp (make-iterate-cubemap-warp-program
                       "current" "curl"
                       [(curl-vector "curl" "gradient") (shaders/gradient-3d "gradient" "flow_field" "epsilon")
-                       (shaders/noise-octaves "octaves" flow-octaves) shaders/rotate-vector shaders/oriented-matrix
-                       shaders/orthogonal-vector shaders/project-vector flow-field])
+                       (shaders/noise-octaves "octaves_north" "worley_north" flow-octaves)
+                       (shaders/noise-octaves "octaves_south" "worley_south" flow-octaves)
+                       shaders/rotate-vector shaders/oriented-matrix shaders/orthogonal-vector shaders/project-vector
+                       flow-field])
         lookup      (make-cubemap-warp-program
-                      "current" "potential"
-                      [(shaders/noise-octaves "clouds" "worley" cloud-octaves) noisy-potential])]
+                      "current" "cover"
+                      [(shaders/noise-octaves "clouds" "worley" cloud-octaves) cover-noise])
+        epsilon       (/ 1.0 worley-size (pow 2.0 (count flow-octaves)))]
     (use-program update-warp)
     (uniform-sampler update-warp "current" 0)
     (uniform-sampler update-warp "worley_north" 1)
     (uniform-sampler update-warp "worley_south" 2)
-    (uniform-float update-warp "epsilon" (/ 1.0 worley-size (pow 2.0 (count flow-octaves))))
+    (uniform-float update-warp "epsilon" epsilon)
     (uniform-float update-warp "whirl" whirl)
     (uniform-float update-warp "prevailing" prevailing)
     (uniform-float update-warp "curl_scale" curl-scale)
-    ; TODO: iteratively increment warp
-    ; TODO: warp clouds
+    (dotimes [iteration num-iterations]
+      (let [updated (iterate-cubemap size flow-scale update-warp (use-textures @warp worley-north worley-south))]
+        (destroy-texture @warp)
+        (reset! warp updated)))
+    (deliver
+      result
+      (cubemap-warp size lookup
+                    (uniform-sampler lookup "current" 0)
+                    (uniform-sampler lookup "worley" 1)
+                    (uniform-float lookup "cover_scale" cover-scale)
+                    (use-textures @warp worley-cover)))
     (destroy-program lookup)
     (destroy-program update-warp)
-    (destroy-texture @warp)))
+    (destroy-texture @warp)
+    @result))
 
 (def worley-size 64)
 
@@ -88,30 +101,22 @@ float potential(vec3 point)
 (def data (slurp-floats "w2.raw"))
 (def worley-south (make-float-texture-3d :linear :repeat {:width worley-size :height worley-size :depth worley-size :data data}))
 (def data (slurp-floats "w3.raw"))
-(def worley-flow (make-float-texture-3d :linear :repeat {:width worley-size :height worley-size :depth worley-size :data data}))
+(def worley-cover (make-float-texture-3d :linear :repeat {:width worley-size :height worley-size :depth worley-size :data data}))
 
-(cloud-cover-cubemap :size 512
-                     :worley-size worley-size
-                     :worley-south worley-south
-                     :worley-north worley-north
-                     :worley-flow worley-flow
-                     :flow-octaves [0.5 0.25 0.125]
-                     :cloud-octaves [0.25 0.25 0.125 0.125 0.0625 0.0625]
-                     :whirl 1.0
-                     :prevailing 0.1
-                     :curl-scale 4.0
-                     )
-
-(destroy-texture worley-flow)
-(destroy-texture worley-south)
-(destroy-texture worley-north)
-
-(Display/destroy)
-
-(System/exit 0)
-
-; TODO: use uniform in noise octaves and allow specification of scale
-; --------------------------------------------------------------------------------
+(def cloud-cover
+  (cloud-cover-cubemap :size 512
+                       :worley-size worley-size
+                       :worley-south worley-south
+                       :worley-north worley-north
+                       :worley-cover worley-cover
+                       :flow-octaves [0.5 0.25 0.125]
+                       :cloud-octaves [0.25 0.25 0.125 0.125 0.0625 0.0625]
+                       :whirl 1.0
+                       :prevailing 0.1
+                       :curl-scale 4.0
+                       :cover-scale 2.0
+                       :num-iterations 30
+                       :flow-scale 1e-2))
 
 (def vertex-shader
 "#version 410 core
@@ -160,10 +165,6 @@ void main()
 (def beta (atom 0))
 (def threshold (atom 0.4))
 (def multiplier (atom 4.0))
-(def curl-scale-exp (atom (log 4)))
-(def cloud-scale-exp (atom (log 2)))
-(def prevailing (atom 0.1))
-(def whirl (atom 1.0))
 
 (def t0 (atom (System/currentTimeMillis)))
 (while (not (Display/isCloseRequested))
@@ -176,41 +177,12 @@ void main()
              ra (if (@keystates Keyboard/KEY_NUMPAD2) 0.001 (if (@keystates Keyboard/KEY_NUMPAD8) -0.001 0))
              rb (if (@keystates Keyboard/KEY_NUMPAD4) 0.001 (if (@keystates Keyboard/KEY_NUMPAD6) -0.001 0))
              tr (if (@keystates Keyboard/KEY_Q) 0.001 (if (@keystates Keyboard/KEY_A) -0.001 0))
-             ta (if (@keystates Keyboard/KEY_W) 0.001 (if (@keystates Keyboard/KEY_S) -0.001 0))
-             cs (if (@keystates Keyboard/KEY_E) 0.001 (if (@keystates Keyboard/KEY_D) -0.001 0))
-             os (if (@keystates Keyboard/KEY_R) 0.001 (if (@keystates Keyboard/KEY_F) -0.001 0))
-             pw (if (@keystates Keyboard/KEY_T) 0.001 (if (@keystates Keyboard/KEY_G) -0.001 0))
-             ps (if (@keystates Keyboard/KEY_Y) 0.001 (if (@keystates Keyboard/KEY_H) -0.001 0))]
-         (swap! alpha + ra)
-         (swap! beta + rb)
+             ta (if (@keystates Keyboard/KEY_W) 0.001 (if (@keystates Keyboard/KEY_S) -0.001 0))]
+         (swap! alpha + (* dt ra))
+         (swap! beta + (* dt rb))
          (swap! threshold + (* dt tr))
          (swap! multiplier + (* dt ta))
-         (swap! whirl + (* dt pw))
-         (swap! prevailing + (* dt ps))
-         (swap! curl-scale-exp + (* dt cs))
-         (swap! cloud-scale-exp + (* dt os))
-         (let [mat (mmul (rotation-y @beta) (rotation-x @alpha))
-               updated (if (or (@keystates Keyboard/KEY_SPACE)
-                               (@keystates Keyboard/KEY_W) (@keystates Keyboard/KEY_S)
-                               (@keystates Keyboard/KEY_R) (@keystates Keyboard/KEY_F)
-                               (@keystates Keyboard/KEY_T) (@keystates Keyboard/KEY_G))
-                         (identity-cubemap cubemap-size)
-                         (iterate-cubemap cubemap-size (* 0.00001 (exp @curl-scale-exp)) update-warp
-                                          (uniform-sampler update-warp "current" 0)
-                                          (uniform-sampler update-warp "worley_north" 1)
-                                          (uniform-sampler update-warp "worley_south" 2)
-                                          (uniform-float update-warp "epsilon" (/ 1.0 worley-size 8))
-                                          (uniform-float update-warp "whirl" @whirl)
-                                          (uniform-float update-warp "prevailing" @prevailing)
-                                          (uniform-float update-warp "curl_scale" (exp @curl-scale-exp))
-                                          (use-textures @warp W1 W2)))
-               warped (cubemap-warp cubemap-size lookup
-                                    (uniform-sampler lookup "current" 0)
-                                    (uniform-sampler lookup "worley" 1)
-                                    (uniform-float lookup "cloud_scale" (exp @cloud-scale-exp))
-                                    (use-textures updated W3))]
-           (destroy-texture @warp)
-           (reset! warp updated)
+         (let [mat (mmul (rotation-y @beta) (rotation-x @alpha))]
            (onscreen-render (Display/getWidth) (Display/getHeight)
                             (clear (matrix [0 0 0]))
                             (use-program program)
@@ -218,12 +190,22 @@ void main()
                             (uniform-matrix3 program "rotation" mat)
                             (uniform-float program "threshold" @threshold)
                             (uniform-float program "multiplier" @multiplier)
-                            (use-textures warped)
-                            (render-quads vao))
-           (destroy-texture warped))
-         (print (format "\rthreshold = %.3f, multiplier = %.3f, curlscale = %.3f, cloudscale = %.3f, whirl = %.3f, prevailing = %.3f, fps = %.1f"
-                        @threshold @multiplier (exp @curl-scale-exp) (exp @cloud-scale-exp) @whirl @prevailing (/ 1000.0 dt)))
+                            (use-textures cloud-cover)
+                            (render-quads vao)))
+         (print (format "\rthreshold = %.3f, multiplier = %.3f, fps = %.1f"
+                        @threshold @multiplier (/ 1000.0 dt)))
          (flush)
+         (Thread/sleep 10)
          (swap! t0 + dt)))
 
 (destroy-program program)
+(destroy-texture cloud-cover)
+(destroy-texture worley-cover)
+(destroy-texture worley-south)
+(destroy-texture worley-north)
+
+(Display/destroy)
+
+(System/exit 0)
+
+; TODO: use uniform in noise octaves and allow specification of scale
