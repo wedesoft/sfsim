2b1b8ddb946d1e9c3865b7785f88371cc210f7fe
Jan Wedekind (Dr) <jan@wedesoft.de>
2023-05-20 22:59:04 +0100

    Port Worley noise to fastmath.

--- src/sfsim25/worley.clj
+++ src/sfsim25/worley.clj
@@ -1,8 +1,7 @@
 (ns sfsim25.worley
     "Create Worley noise"
-    (:require [clojure.core.matrix :refer (matrix add sub dimension-count eseq)]
-              [clojure.core.matrix.linear :refer (norm)]
-              [sfsim25.util :refer (make-progress-bar tick-and-print spit-floats)]
+    (:require [fastmath.vector :refer (vec3 add sub mag)]
+              [sfsim25.util :refer (make-progress-bar tick-and-print spit-floats dimension-count)]
               [com.climate.claypoole :refer (pfor ncpus)]))
 
 (set! *unchecked-math* true)
@@ -16,8 +15,8 @@
      (map (fn [k]
               (map (fn [j]
                        (map (fn [i]
-                                (add (matrix [(* i cellsize) (* j cellsize) (* k cellsize)])
-                                     (matrix (repeatedly 3 #(random cellsize)))))
+                                (add (vec3 (* i cellsize) (* j cellsize) (* k cellsize))
+                                     (apply vec3 (repeatedly 3 #(random cellsize)))))
                             (range divisions)))
                    (range divisions)))
           (range divisions)))))
@@ -36,16 +35,16 @@
   (let [[i-clip x-offset] (clipped-index-and-offset grid size 2 i)
         [j-clip y-offset] (clipped-index-and-offset grid size 1 j)
         [k-clip z-offset] (clipped-index-and-offset grid size 0 k)]
-    (add (reduce nth grid [k-clip j-clip i-clip]) (matrix [x-offset y-offset z-offset]))))
+    (add (reduce nth grid [k-clip j-clip i-clip]) (vec3 x-offset y-offset z-offset))))
 
 (defn closest-distance-to-point-in-grid
   "Return distance to closest point in 3D grid"
   [grid divisions size point]
   (let [cellsize (/ size divisions)
-        [x y z]  (eseq point)
+        [x y z]  point
         [i j k]  [(quot x cellsize) (quot y cellsize) (quot z cellsize)]
         points   (for [dk [-1 0 1] dj [-1 0 1] di [-1 0 1]] (extract-point-from-grid grid size (+ k dk) (+ j dj) (+ i di)))]
-    (apply min (map #(norm (sub point %)) points))))
+    (apply min (map #(mag (sub point %)) points))))
 
 (defn normalize-vector
   "Normalize the values of a vector by scaling the maximum down to 1.0"
@@ -70,6 +69,6 @@
          (pfor (+ 2 (ncpus)) [k (range size) j (range size) i (range size)]
                (do
                  (if progress (send bar tick-and-print))
-                 (closest-distance-to-point-in-grid grid divisions size (matrix [(+ k 0.5) (+ j 0.5) (+ i 0.5)])))))))))
+                 (closest-distance-to-point-in-grid grid divisions size (vec3 (+ k 0.5) (+ j 0.5) (+ i 0.5))))))))))
 
 (set! *unchecked-math* false)
