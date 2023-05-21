e294cad4accfd6374727a3d57aed5c7c9afd3e60
Jan Wedekind (Dr) <jan@wedesoft.de>
2023-05-21 20:49:26 +0100

    Port ellipsoid to fastmath.

--- src/sfsim25/ellipsoid.clj
+++ src/sfsim25/ellipsoid.clj
@@ -1,12 +1,12 @@
 (ns sfsim25.ellipsoid
   "Functions dealing with ellipsoids"
-  (:require [clojure.core.matrix :refer (matrix mget)]
+  (:require [fastmath.vector :refer (vec3)]
             [sfsim25.sphere :refer :all]))
 
 (defn ray-ellipsoid-intersection
   "Compute intersection of line with ellipsoid"
   [{:sfsim25.ellipsoid/keys [centre major-radius minor-radius]} {:sfsim25.ray/keys [origin direction]}]
   (let [factor (/ major-radius minor-radius)
-        scale  (fn [v] (matrix [(mget v 0) (mget v 1) (* factor (mget v 2))]))]
+        scale  (fn [v] (vec3 (v 0) (v 1) (* factor (v 2))))]
   (ray-sphere-intersection #:sfsim25.sphere{:centre (scale centre) :radius major-radius}
                            #:sfsim25.ray{:origin (scale origin) :direction (scale direction)})))
