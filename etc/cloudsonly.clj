1d37bda1332fd0d8d97409542d82072e1dabc6dc
Jan Wedekind (Dr) <jan@wedesoft.de>
2023-06-12 21:05:46 +0100

    Copy planet fragment shader into prototype.

--- etc/cloudsonly.clj
+++ etc/cloudsonly.clj
@@ -24,7 +24,7 @@
 (GLFW/glfwDefaultWindowHints)
 (def window (GLFW/glfwCreateWindow width height "scratch" 0 0))
 
-(def fragment-clouds
+(def fragment-atmosphere-enhanced
 "#version 410 core
 uniform float stepsize;
 uniform float radius;
@@ -147,6 +147,56 @@ void main()
   fragColor = (background * transmittance + ray_scatter) * cloud_scatter.a + cloud_scatter.rgb;
 }")
 
+(def fragment-planet-enhanced
+"#version 410 core
+
+uniform sampler2D colors;
+uniform sampler2D normals;
+uniform sampler2D water;
+uniform float specular;
+uniform float radius;
+uniform float max_height;
+uniform float amplification;
+uniform vec3 water_color;
+uniform vec3 position;
+uniform vec3 light_direction;
+
+in GEO_OUT
+{
+  vec2 colorcoord;
+  vec2 heightcoord;
+  vec3 point;
+} fs_in;
+
+out vec3 fragColor;
+
+vec2 ray_sphere(vec3 centre, float radius, vec3 origin, vec3 direction);
+vec3 ground_radiance(vec3 point, vec3 light_direction, float water, float cos_incidence, float highlight,
+                     vec3 land_color, vec3 water_color);
+vec3 attenuation_track(vec3 light_direction, vec3 origin, vec3 direction, float a, float b, vec3 incoming);
+
+// Render planet surface as seen through the atmosphere.
+void main()
+{
+  vec3 normal = texture(normals, fs_in.colorcoord).xyz;
+  vec3 direction = normalize(fs_in.point - position);
+  vec3 land_color = texture(colors, fs_in.colorcoord).rgb;
+  float wet = texture(water, fs_in.colorcoord).r;
+  float cos_incidence = dot(light_direction, normal);
+  float highlight;
+  if (cos_incidence > 0) {
+    highlight = pow(max(dot(reflect(light_direction, normal), direction), 0), specular);
+  } else {
+    cos_incidence = 0.0;
+    highlight = 0.0;
+  };
+  vec2 atmosphere_intersection = ray_sphere(vec3(0, 0, 0), radius + max_height, position, direction);
+  vec3 incoming = ground_radiance(fs_in.point, light_direction, wet, cos_incidence, highlight, land_color, water_color);
+  float a = atmosphere_intersection.x;
+  float b = distance(position, fs_in.point);
+  fragColor = amplification * attenuation_track(light_direction, position, direction, a, b, incoming);
+}")
+
 (def fov (to-radians 60.0))
 (def radius 6378000.0)
 (def tilesize 33)
@@ -233,7 +283,7 @@ void main()
 
 (def program-atmosphere
   (make-program :vertex [vertex-atmosphere]
-                :fragment [fragment-clouds cloud-density shaders/remap
+                :fragment [fragment-atmosphere-enhanced cloud-density shaders/remap
                            (shaders/noise-octaves-lod "cloud_octaves" "lookup_3d" octaves)
                            cloud-base cloud-cover cloud-noise
                            (shaders/lookup-3d-lod "lookup_3d" "worley")
@@ -311,7 +361,8 @@ void main()
                 :tess-control [tess-control-planet]
                 :tess-evaluation [tess-evaluation-planet]
                 :geometry [geometry-planet]
-                :fragment [fragment-planet attenuation-track shaders/ray-sphere ground-radiance shaders/transmittance-forward
+                :fragment [fragment-planet-enhanced attenuation-track shaders/ray-sphere ground-radiance
+                           shaders/transmittance-forward
                            transmittance-track shaders/height-to-index shaders/horizon-distance shaders/sun-elevation-to-index
                            shaders/limit-quot shaders/sun-angle-to-index shaders/interpolate-2d shaders/interpolate-4d
                            ray-scatter-track shaders/elevation-to-index shaders/convert-2d-index shaders/ray-scatter-forward
