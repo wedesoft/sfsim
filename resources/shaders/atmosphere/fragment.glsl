531747fb0c22aa7ba1a391b8b864ccca903c04ce
Jan Wedekind (Dr) <jan@wedesoft.de>
2023-06-24 21:26:47 +0100

    Put sun color into small function.

--- resources/shaders/atmosphere/fragment.glsl
+++ resources/shaders/atmosphere/fragment.glsl
@@ -16,12 +16,17 @@ out vec3 fragColor;
 vec2 ray_sphere(vec3 centre, float radius, vec3 origin, vec3 direction);
 vec3 attenuation_outer(vec3 light_direction, vec3 origin, vec3 direction, float a, vec3 incoming);
 
+vec3 sun_color(vec3 direction)
+{
+  float glare = pow(max(0, dot(direction, light_direction)), specular);
+  return vec3(glare, glare, glare);
+}
+
 // Fragment shader to render atmosphere on a background quad.
 void main()
 {
   vec3 direction = normalize(fs_in.direction);
-  float glare = pow(max(0, dot(direction, light_direction)), specular);
-  vec3 incoming = vec3(glare, glare, glare);
+  vec3 incoming = sun_color(direction);
   vec2 atmosphere_intersection = ray_sphere(vec3(0, 0, 0), radius + max_height, origin, direction);
   if (atmosphere_intersection.y > 0) {
     incoming = attenuation_outer(light_direction, origin, direction, atmosphere_intersection.x, incoming);
