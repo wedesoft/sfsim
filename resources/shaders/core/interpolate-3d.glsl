83fb65e48b1b37eedad206e854a53ef277c7fe51
Jan Wedekind <jan@wedesoft.de>
2023-06-30 19:30:29 +0100

    Bugfix for return type of interpolate_3d.

--- resources/shaders/core/interpolate-3d.glsl
+++ resources/shaders/core/interpolate-3d.glsl
@@ -3,8 +3,8 @@
 vec3 convert_3d_index(vec3 idx, int size_z, int size_y, int size_x);
 
 // Perform 2D texture lookup using coordinates between 0 and 1.
-vec4 interpolate_3d(sampler3D table, int size_z, int size_y, int size_x, vec3 idx)
+float interpolate_3d(sampler3D table, int size_z, int size_y, int size_x, vec3 idx)
 {
   vec3 indices = convert_3d_index(idx, size_z, size_y, size_x);
-  return texture(table, indices);
+  return texture(table, indices).r;
 }
