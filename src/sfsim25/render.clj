fe0be1c27b079de59396f0dfab52f5e963f59023
Jan Wedekind (Dr) <jan@wedesoft.de>
2023-07-17 22:07:35 +0100

    Minor performance improvement

--- src/sfsim25/render.clj
+++ src/sfsim25/render.clj
@@ -36,8 +36,7 @@
   `(let [width#  (int-array 1)
          height# (int-array 1)]
      (GLFW/glfwGetWindowSize ~window width# height#)
-     (GLFW/glfwMakeContextCurrent ~window)
-     (GL/createCapabilities)
+     ; call (GLFW/glfwMakeContextCurrent ~window) and (GL/createCapabilities) after creating window
      (setup-rendering (aget width# 0) (aget height# 0) :cullback)
      ~@body
      (GLFW/glfwSwapBuffers ~window)))
