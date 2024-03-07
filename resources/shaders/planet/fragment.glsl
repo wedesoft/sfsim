68fe755d6466171057d8870abeb73fc9c79f58ec
Jan Wedekind (Dr) <jan@wedesoft.de>
2024-03-07 20:19:32 +0000

    Reorder some statement

--- resources/shaders/planet/fragment.glsl
+++ resources/shaders/planet/fragment.glsl
@@ -28,15 +28,15 @@ vec4 cloud_overlay();
 // Render planet surface as seen through the atmosphere.
 void main()
 {
+  float wet = texture(water, fs_in.colorcoord).r;
   vec3 land_normal = texture(normals, fs_in.colorcoord).xyz;
   vec3 water_normal = normalize(fs_in.point);
-  vec3 day_color = texture(day, fs_in.colorcoord).rgb;
-  vec3 night_color = max(texture(night, fs_in.colorcoord).rgb - 0.3, 0.0) / 0.7;
-  float wet = texture(water, fs_in.colorcoord).r;
   vec3 normal = mix(land_normal, water_normal, wet);
   vec3 light = direct_light(fs_in.point);
   vec3 ambient_light = surface_radiance_function(fs_in.point, light_direction);
+  vec3 day_color = texture(day, fs_in.colorcoord).rgb;
   vec3 color = mix(day_color, water_color, wet);
+  vec3 night_color = max(texture(night, fs_in.colorcoord).rgb - 0.3, 0.0) / 0.7;
   vec3 emissive = clamp(remap(dot(light_direction, water_normal), dawn_start, dawn_end, 1.0, 0.0), 0.0, 1.0) * night_color;
   vec3 phong = phong(ambient_light, light, fs_in.point, normal, color, wet * reflectivity);
   vec3 incoming = attenuation_point(fs_in.point, phong + emissive);
