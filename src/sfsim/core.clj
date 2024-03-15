592a6a5d2b69663139e0b0473d7a45ac1c6d0cc1
Jan Wedekind (Dr) <jan@wedesoft.de>
2024-03-15 22:21:31 +0000

    Make distance to object configurable

--- src/sfsim/core.clj
+++ src/sfsim/core.clj
@@ -1,6 +1,6 @@
 (ns sfsim.core
   "Space flight simulator main program."
-  (:require [clojure.math :refer (cos sin to-radians)]
+  (:require [clojure.math :refer (cos sin to-radians exp)]
             [fastmath.matrix :refer (eye inverse)]
             [fastmath.vector :refer (vec3 add mult)]
             [sfsim.texture :refer (destroy-texture)]
@@ -88,6 +88,8 @@
 
 (GLFW/glfwSetKeyCallback window keyboard-callback)
 
+(def dist (atom 10.0))
+
 (defn -main
   "Space flight simulator main function"
   [& _args]
@@ -105,6 +107,7 @@
                  rc (if (@keystates GLFW/GLFW_KEY_KP_1) 0.001 (if (@keystates GLFW/GLFW_KEY_KP_3) -0.001 0.0))
                  v  (if (@keystates GLFW/GLFW_KEY_PAGE_UP) @speed (if (@keystates GLFW/GLFW_KEY_PAGE_DOWN) (- @speed) 0))
                  l  (if (@keystates GLFW/GLFW_KEY_KP_ADD) 0.005 (if (@keystates GLFW/GLFW_KEY_KP_SUBTRACT) -0.005 0))
+                 d  (if (@keystates GLFW/GLFW_KEY_Q) 0.05 (if (@keystates GLFW/GLFW_KEY_A) -0.05 0))
                  to (if (@keystates GLFW/GLFW_KEY_W) 0.05 (if (@keystates GLFW/GLFW_KEY_S) -0.05 0))]
              (swap! orientation q/* (q/rotation (* dt ra) (vec3 1 0 0)))
              (swap! orientation q/* (q/rotation (* dt rb) (vec3 0 1 0)))
@@ -112,7 +115,8 @@
              (swap! position add (mult (q/rotate-vector @orientation (vec3 0 0 -1)) (* dt v)))
              (swap! light + (* l 0.1 dt))
              (swap! opacity-base + (* dt to))
-             (let [origin       (add @position (mult (q/rotate-vector @orientation (vec3 0 0 -1)) -20.0))
+             (swap! dist * (exp d))
+             (let [origin       (add @position (mult (q/rotate-vector @orientation (vec3 0 0 -1)) (* -1.0 @dist)))
                    object-position @position
                    render-vars  (make-render-vars config/planet-config cloud-data config/render-config (aget w 0) (aget h 0)
                                                   origin @orientation (vec3 (cos @light) (sin @light) 0) 1.0)
@@ -176,7 +180,7 @@
              (GLFW/glfwPollEvents)
              (swap! n inc)
              (when (zero? (mod @n 10))
-               (print (format "\ro.-step (w/s) %.0f, dt %.3f" @opacity-base (* dt 0.001)))
+               (print (format "\ro.-step (w/s) %.0f, dist (q/a) %.0f dt %.3f" @opacity-base @dist (* dt 0.001)))
                (flush))
              (swap! t0 + dt))))
   ; TODO: unload all planet tiles (vaos and textures)
