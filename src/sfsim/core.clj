9613c17d4eb75032e319111aaf4a7e3147947f5b
Jan Wedekind (Dr) <jan@wedesoft.de>
2024-03-26 22:02:30 +0000

    Independent object and camera rotation

--- src/sfsim/core.clj
+++ src/sfsim/core.clj
@@ -7,7 +7,7 @@
             [sfsim.render :refer (make-window destroy-window clear onscreen-render texture-render-color-depth with-stencils
                                   write-to-stencil-buffer mask-with-stencil-buffer)]
             [sfsim.atmosphere :as atmosphere]
-            [sfsim.matrix :refer (transformation-matrix)]
+            [sfsim.matrix :refer (transformation-matrix quaternion->matrix)]
             [sfsim.planet :as planet]
             [sfsim.clouds :as clouds]
             [sfsim.model :as model]
@@ -30,9 +30,10 @@
 
 (def opacity-base (atom 250.0))
 (def position (atom (vec3 (+ 3.0 6378000.0) 0 0)))
-(def orientation (atom (q/rotation (to-radians 270) (vec3 0 0 1))))
+(def camera-orientation (atom (q/rotation (to-radians 270) (vec3 0 0 1))))
+(def object-orientation (atom (q/rotation (to-radians 270) (vec3 0 0 1))))
 (def light (atom 0.0))
-(def speed (atom 10.0))
+(def speed (atom 0.3))
 
 (GLFW/glfwInit)
 
@@ -98,32 +99,39 @@
            (planet/update-tile-tree planet-renderer tile-tree (aget w 0) @position)
            (let [t1 (System/currentTimeMillis)
                  dt (- t1 @t0)
+                 u  (if (@keystates GLFW/GLFW_KEY_S) 0.001 (if (@keystates GLFW/GLFW_KEY_W) -0.001 0.0))
+                 r  (if (@keystates GLFW/GLFW_KEY_A) 0.001 (if (@keystates GLFW/GLFW_KEY_D) -0.001 0.0))
+                 t  (if (@keystates GLFW/GLFW_KEY_C) 0.001 (if (@keystates GLFW/GLFW_KEY_Z) -0.001 0.0))
                  ra (if (@keystates GLFW/GLFW_KEY_KP_2) 0.001 (if (@keystates GLFW/GLFW_KEY_KP_8) -0.001 0.0))
                  rb (if (@keystates GLFW/GLFW_KEY_KP_4) 0.001 (if (@keystates GLFW/GLFW_KEY_KP_6) -0.001 0.0))
                  rc (if (@keystates GLFW/GLFW_KEY_KP_1) 0.001 (if (@keystates GLFW/GLFW_KEY_KP_3) -0.001 0.0))
                  v  (if (@keystates GLFW/GLFW_KEY_PAGE_UP) @speed (if (@keystates GLFW/GLFW_KEY_PAGE_DOWN) (- @speed) 0))
                  l  (if (@keystates GLFW/GLFW_KEY_KP_ADD) 0.005 (if (@keystates GLFW/GLFW_KEY_KP_SUBTRACT) -0.005 0))
-                 d  (if (@keystates GLFW/GLFW_KEY_Q) 0.05 (if (@keystates GLFW/GLFW_KEY_A) -0.05 0))
-                 to (if (@keystates GLFW/GLFW_KEY_W) 0.05 (if (@keystates GLFW/GLFW_KEY_S) -0.05 0))]
-             (swap! orientation q/* (q/rotation (* dt ra) (vec3 1 0 0)))
-             (swap! orientation q/* (q/rotation (* dt rb) (vec3 0 1 0)))
-             (swap! orientation q/* (q/rotation (* dt rc) (vec3 0 0 1)))
-             (swap! position add (mult (q/rotate-vector @orientation (vec3 0 0 -1)) (* dt v)))
+                 d  (if (@keystates GLFW/GLFW_KEY_R) 0.05 (if (@keystates GLFW/GLFW_KEY_F) -0.05 0))
+                 to (if (@keystates GLFW/GLFW_KEY_T) 0.05 (if (@keystates GLFW/GLFW_KEY_G) -0.05 0))]
+             (swap! object-orientation q/* (q/rotation (* dt u) (vec3 0 0 1)))
+             (swap! object-orientation q/* (q/rotation (* dt r) (vec3 0 1 0)))
+             (swap! object-orientation q/* (q/rotation (* dt t) (vec3 1 0 0)))
+             (swap! camera-orientation q/* (q/rotation (* dt ra) (vec3 1 0 0)))
+             (swap! camera-orientation q/* (q/rotation (* dt rb) (vec3 0 1 0)))
+             (swap! camera-orientation q/* (q/rotation (* dt rc) (vec3 0 0 1)))
+             (swap! position add (mult (q/rotate-vector @object-orientation (vec3 1 0 0)) (* dt v)))
              (swap! light + (* l 0.1 dt))
              (swap! opacity-base + (* dt to))
              (swap! dist * (exp d))
-             (let [origin             (add @position (mult (q/rotate-vector @orientation (vec3 0 0 -1)) (* -1.0 @dist)))
+             (let [origin             (add @position (mult (q/rotate-vector @camera-orientation (vec3 0 0 -1)) (* -1.0 @dist)))
                    object-position    @position
                    light-direction    (vec3 (cos @light) (sin @light) 0)
                    planet-render-vars (planet/make-planet-render-vars config/planet-config cloud-data config/render-config
-                                                                      (aget w 0) (aget h 0) origin @orientation light-direction)
+                                                                      (aget w 0) (aget h 0) origin @camera-orientation
+                                                                      light-direction)
                    scene-render-vars  (model/make-scene-render-vars config/render-config (aget w 0) (aget h 0) origin
-                                                                    @orientation light-direction object-position
+                                                                    @camera-orientation light-direction object-position
                                                                     config/object-radius)
                    shadow-vars        (opacity/opacity-and-shadow-cascade opacity-renderer planet-shadow-renderer shadow-data
                                                                           cloud-data planet-render-vars
                                                                           (planet/get-current-tree tile-tree) @opacity-base)
-                   object-to-world    (transformation-matrix (eye 3) object-position)
+                   object-to-world    (transformation-matrix (quaternion->matrix @object-orientation) object-position)
                    moved-scene        (assoc-in scene [:sfsim.model/root :sfsim.model/transform] object-to-world)
                    w2                 (quot (:sfsim.render/window-width planet-render-vars) 2)
                    h2                 (quot (:sfsim.render/window-height planet-render-vars) 2)
@@ -163,7 +171,7 @@
              (GLFW/glfwPollEvents)
              (swap! n inc)
              (when (zero? (mod @n 10))
-               (print (format "\ro.-step (w/s) %.0f, dist (q/a) %.0f dt %.3f" @opacity-base @dist (* dt 0.001)))
+               (print (format "\ro.-step (t/g) %.0f, dist (r/f) %.0f dt %.3f" @opacity-base @dist (* dt 0.001)))
                (flush))
              (swap! t0 + dt))))
   (planet/destroy-tile-tree tile-tree)
