45bd4b6626ab2df91dbae88d704dd493544cc6a7
Jan Wedekind (Dr) <jan@wedesoft.de>
2024-04-28 21:06:27 +0100

    Add z-thresholding to convert_shadow_index shader

--- resources/shaders/core/convert-shadow-index.glsl
+++ resources/shaders/core/convert-shadow-index.glsl
@@ -1,8 +1,10 @@
 #version 410 core
 
+uniform float shadow_bias;
+
 // Move shadow index out of clamping region
 vec4 convert_shadow_index(vec4 idx, int size_y, int size_x)
 {
   vec2 pixel = idx.xy * (vec2(size_x, size_y) - 1);
-  return vec4((pixel + 0.5) / vec2(size_x, size_y), idx.z, idx.w);
+  return vec4((pixel + 0.5) / vec2(size_x, size_y), max(idx.z, 2 * shadow_bias), idx.w);
 }
