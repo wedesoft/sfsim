bacac640231dd5ea3bc43ec2627e4831414aeee9
Jan Wedekind (Dr) <jan@wedesoft.de>
2024-10-16 21:22:42 +0100

    Initialize Jolt physics system

--- src/c/sfsim/jolt.cc
+++ src/c/sfsim/jolt.cc
@@ -7,6 +7,7 @@
 #include <Jolt/Core/JobSystemThreadPool.h>
 #include <Jolt/Physics/PhysicsSettings.h>
 #include <Jolt/Physics/PhysicsSystem.h>
+#include <Jolt/Physics/Collision/BroadPhase/BroadPhaseLayer.h>
 #include "sfsim/jolt.hh"
 
 
@@ -50,6 +51,67 @@ class ObjectLayerPairFilterImpl: public JPH::ObjectLayerPairFilter
     }
 };
 
+namespace BroadPhaseLayers
+{
+  static constexpr JPH::BroadPhaseLayer NON_MOVING(0);
+  static constexpr JPH::BroadPhaseLayer MOVING(1);
+  static constexpr uint NUM_LAYERS(2);
+};
+
+class BPLayerInterfaceImpl final: public JPH::BroadPhaseLayerInterface
+{
+public:
+  BPLayerInterfaceImpl()
+  {
+    mObjectToBroadPhase[NON_MOVING] = BroadPhaseLayers::NON_MOVING;
+    mObjectToBroadPhase[MOVING] = BroadPhaseLayers::MOVING;
+  }
+
+  virtual uint GetNumBroadPhaseLayers() const override
+  {
+    return BroadPhaseLayers::NUM_LAYERS;
+  }
+
+  virtual JPH::BroadPhaseLayer GetBroadPhaseLayer(JPH::ObjectLayer inLayer) const override
+  {
+    JPH_ASSERT(inLayer < NUM_LAYERS);
+    return mObjectToBroadPhase[inLayer];
+  }
+
+#if defined(JPH_EXTERNAL_PROFILE) || defined(JPH_PROFILE_ENABLED)
+  virtual const char *GetBroadPhaseLayerName(JPH::BroadPhaseLayer inLayer) const override
+  {
+    switch ((JPH::BroadPhaseLayer::Type)inLayer)
+    {
+      case (JPH::BroadPhaseLayer::Type)BroadPhaseLayers::NON_MOVING: return "NON_MOVING";
+      case (JPH::BroadPhaseLayer::Type)BroadPhaseLayers::MOVING: return "MOVING";
+      default: JPH_ASSERT(false); return "INVALID";
+    }
+  }
+#endif // JPH_EXTERNAL_PROFILE || JPH_PROFILE_ENABLED
+
+private:
+  JPH::BroadPhaseLayer mObjectToBroadPhase[NUM_LAYERS];
+};
+
+class ObjectVsBroadPhaseLayerFilterImpl: public JPH::ObjectVsBroadPhaseLayerFilter
+{
+public:
+  virtual bool ShouldCollide(JPH::ObjectLayer inLayer1, JPH::BroadPhaseLayer inLayer2) const override
+  {
+    switch (inLayer1)
+    {
+    case NON_MOVING:
+      return inLayer2 == BroadPhaseLayers::MOVING;
+    case MOVING:
+      return true;
+    default:
+      JPH_ASSERT(false);
+      return false;
+    }
+  }
+};
+
 JPH::TempAllocatorMalloc *temp_allocator = nullptr;
 JPH::JobSystemThreadPool *job_system = nullptr;
 
@@ -57,6 +119,12 @@ const uint cMaxBodies = 1024;
 const uint cNumBodyMutexes = 0;
 const uint cMaxBodyPairs = 1024;
 const uint cMaxContactConstraints = 1024;
+
+BPLayerInterfaceImpl *broad_phase_layer_interface = nullptr;
+ObjectLayerPairFilterImpl *object_vs_object_layer_filter = nullptr;
+ObjectVsBroadPhaseLayerFilterImpl *object_vs_broadphase_layer_filter = nullptr;
+
+JPH::PhysicsSystem *physics_system = nullptr;
 
 void jolt_init(void)
 {
@@ -68,10 +136,28 @@ void jolt_init(void)
 
   temp_allocator = new JPH::TempAllocatorMalloc;
   job_system = new JPH::JobSystemThreadPool(JPH::cMaxPhysicsJobs, JPH::cMaxPhysicsBarriers, std::thread::hardware_concurrency() - 1);
+
+  broad_phase_layer_interface = new BPLayerInterfaceImpl;
+  object_vs_object_layer_filter = new ObjectLayerPairFilterImpl;
+  object_vs_broadphase_layer_filter = new ObjectVsBroadPhaseLayerFilterImpl;
+
+  physics_system = new JPH::PhysicsSystem;
+  physics_system->Init(cMaxBodies, cNumBodyMutexes, cMaxBodyPairs, cMaxContactConstraints, *broad_phase_layer_interface,
+                       *object_vs_broadphase_layer_filter, *object_vs_object_layer_filter);
 }
 
 void jolt_destroy(void)
 {
+  delete physics_system;
+  physics_system = nullptr;
+
+  delete object_vs_broadphase_layer_filter;
+  object_vs_broadphase_layer_filter = nullptr;
+  delete object_vs_object_layer_filter;
+  object_vs_object_layer_filter = nullptr;
+  delete broad_phase_layer_interface;
+  broad_phase_layer_interface = nullptr;
+
   delete job_system;
   job_system = nullptr;
   delete temp_allocator;
