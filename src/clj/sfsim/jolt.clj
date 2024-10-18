08fe5ba673c93017c6a66cf87cd59e49fe2c0f74
Jan Wedekind (Dr) <jan@wedesoft.de>
2024-10-18 12:23:29 +0100

    Add custom type for fastmath vector

--- src/clj/sfsim/jolt.clj
+++ src/clj/sfsim/jolt.clj
@@ -44,11 +44,23 @@
   "Destruct Jolt library setup"
   jolt_destroy [] ::mem/void)
 
-(mem/defalias ::vec3
-  [::mem/struct
-   [[:x ::mem/double]
-    [:y ::mem/double]
-    [:z ::mem/double]]])
+(def vec3-struct [::mem/struct
+                  [[:x ::mem/double]
+                   [:y ::mem/double]
+                   [:z ::mem/double]]])
+
+(defmethod mem/c-layout ::vec3
+  [_vec3]
+  (mem/c-layout vec3-struct))
+
+(defmethod mem/serialize-into ::vec3
+  [obj _vec3 segment arena]
+  (mem/serialize-into {:x (obj 0) :y (obj 1) :z (obj 2)} vec3-struct segment arena))
+
+(defmethod mem/deserialize-from ::vec3
+  [segment _vec3]
+  (let [result (mem/deserialize-from segment vec3-struct)]
+    (vec3 (:x result) (:y result) (:z result))))
 
 (mem/defalias ::quaternion
   [::mem/struct
@@ -64,21 +76,15 @@
   "Create sphere body"
   [radius center rotation]
   (make-sphere_ radius
-                {:x (center 0) :y (center 1) :z (center 2)}
+                center
                 {:real (.real rotation) :imag (.imag rotation) :jmag (.jmag rotation) :kmag (.kmag rotation)}))
 
 (defcfn remove-and-destroy-body
   "Remove body from physics system and destroy it"
   remove_and_destroy_body [::mem/int] ::mem/void)
 
-(defcfn get-translation_
+(defcfn get-translation
   get_translation [::mem/int] ::vec3)
-
-(defn get-translation
-  "Get translation vector of a body's world transform"
-  [id]
-  (let [result (get-translation_ id)]
-    (vec3 (result :x) (result :y) (result :z))))
 
 (mem/defalias ::mat3x3
   [::mem/struct
