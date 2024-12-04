3c371ee777ade6524ed4d58cefbc3d2ba2449c0c
Jan Wedekind (Dr) <jan@wedesoft.de>
2024-12-04 21:13:47 +0000

    Set mix for pyramid splits to 0.8

--- src/clj/sfsim/config.clj
+++ src/clj/sfsim/config.clj
@@ -45,7 +45,7 @@
                                    :num-steps 3
                                    :scene-shadow-size 512
                                    :scene-shadow-counts [0 1]
-                                   :mix 0.4
+                                   :mix 0.8
                                    :shadow-bias 1e-6})
 
 (def object-radius 30.0)
