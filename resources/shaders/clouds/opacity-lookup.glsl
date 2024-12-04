33cd6eb2eed6b5753588b82991da513ebabd4f03
Jan Wedekind (Dr) <jan@wedesoft.de>
2024-12-04 22:14:39 +0000

    1 -> 1.0

--- resources/shaders/clouds/opacity-lookup.glsl
+++ resources/shaders/clouds/opacity-lookup.glsl
@@ -11,7 +11,7 @@ float opacity_lookup(sampler3D layers, float depth, vec4 opacity_map_coords)
 {
   vec2 offset_index = convert_2d_index(opacity_map_coords.xy, shadow_size, shadow_size);
   float offset_ = texture(layers, vec3(offset_index, 0.0)).r - opacity_map_coords.z;
-  float z_index = max(offset_ * depth / opacity_step, 0) + 1;
+  float z_index = max(offset_ * depth / opacity_step, 0) + 1.0;
   vec3 idx = vec3(opacity_map_coords.xy, z_index / num_opacity_layers);
   vec3 opacity_index = convert_3d_index(idx, num_opacity_layers + 1, shadow_size, shadow_size);
   float transmittance = texture(layers, opacity_index).r;
