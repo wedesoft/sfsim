eda03e3863404347cd685cc6f4a387e88c98aa8a
Jan Wedekind (Dr) <jan@wedesoft.de>
2024-12-10 22:16:37 +0000

    Bugfix for offsets

--- resources/shaders/clouds/opacity-cascade-lookup.glsl
+++ resources/shaders/clouds/opacity-cascade-lookup.glsl
@@ -19,9 +19,9 @@ float opacity_cascade_lookup(vec4 point)
   float z = -(world_to_camera * point).z;
 <% (doseq [i (range n)] %>
   if (z <= split<%= (inc i) %>) {
-    vec4 map_coords = world_to_shadow_map<%= i %> * point;
-    vec4 offset_map_coords = vec4(map_coords.xyz + light_direction * bias<%= i %>, 1.0);
-    return <%= base-function %>(opacity<%= i %>, depth<%= i %>, offset_map_coords);
+    vec4 offset_point = vec4(point.xyz + light_direction * bias<%= i %>, 1.0);
+    vec4 map_coords = world_to_shadow_map<%= i %> * offset_point;
+    return <%= base-function %>(opacity<%= i %>, depth<%= i %>, map_coords);
   };
 <% ) %>
   return 1.0;
