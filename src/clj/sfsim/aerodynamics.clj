ea96fd5d8fbf954ffc6d79c30b16c9f4f3ac1fd3
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-03-05 22:13:47 +0000

    Working on refinement of lift function

--- src/clj/sfsim/aerodynamics.clj
+++ src/clj/sfsim/aerodynamics.clj
@@ -1,6 +1,7 @@
 (ns sfsim.aerodynamics
     (:require
-      [clojure.math :refer (cos sin)]))
+      [clojure.math :refer (cos sin sqrt)]
+      [sfsim.util :refer (sqr)]))
 
 
 (set! *unchecked-math* true)
@@ -19,6 +20,27 @@
   {:malli/schema [:=> [:cat :double] [:=> [:cat :double] :double]]}
   [max-lift]
   (fn [angle-of-attack] (* max-lift (sin (* 2 angle-of-attack)))))
+
+
+(defn fall-off
+  "Ellipse-like fall-off function"
+  [reduced-increase interval]
+  (fn [angle-of-attack]
+      (let [relative-position (/ (- interval angle-of-attack) interval)]
+        (if (>= relative-position 0.0)
+          (* reduced-increase (- 1.0 (sqrt (- 1.0 (sqr relative-position)))))
+          0.0))))
+
+
+(defn glide
+  "Increase of lift for small angles of attack before stall"
+  [max-increase stall-angle reduced-increase fall-off-interval]
+  (fn glide-fn [angle-of-attack]
+      (if (neg? angle-of-attack)
+        (- (glide-fn (- angle-of-attack)))
+        (if (<= angle-of-attack stall-angle)
+          (* (/ max-increase stall-angle) angle-of-attack)
+          ((fall-off reduced-increase fall-off-interval) (- angle-of-attack stall-angle))))))
 
 
 (set! *warn-on-reflection* false)
--- test/clj/sfsim/t_aerodynamics.clj
+++ test/clj/sfsim/t_aerodynamics.clj
@@ -20,3 +20,19 @@
        ((basic-lift 1.1) 0.0) => 0.0
        ((basic-lift 1.1) (/ PI 4)) => 1.1
        ((basic-lift 1.1) (/ PI 2)) => (roughly 0.0 1e-6))
+
+
+(facts "Ellipse-like fall-off function"
+       ((fall-off 0.8 0.5) 0.0) => 0.8
+       ((fall-off 0.8 0.5) 0.5) => 0.0
+       ((fall-off 0.8 0.5) 0.2) => (roughly 0.16 1e-6)
+       ((fall-off 0.8 0.6) 1.0) => 0.0)
+
+(facts "Increase of lift for small angles of attack before stall"
+       ((glide 0.8 0.6 0.4 0.5) 0.0) => 0.0
+       ((glide 0.8 0.6 0.4 0.5) 0.6) => 0.8
+       ((glide 0.8 0.6 0.4 0.5) 1.1) => 0.0
+       ((glide 0.8 0.6 0.4 0.5) 0.8) => (roughly 0.08 1e-6)
+       ((glide 0.8 0.6 0.4 0.5) -0.6) => -0.8
+       ((glide 0.8 0.6 0.4 0.5) -1.1) => 0.0
+       ((glide 0.8 0.6 0.4 0.5) -0.8) => (roughly -0.08 1e-6))
