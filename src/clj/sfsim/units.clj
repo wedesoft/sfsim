ea9aef3438da658f8bfd623acb3b6ce05cab2ac3
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-04-04 21:30:04 +0100

    Add some more units

---
+++ src/clj/sfsim/units.clj
@@ -0,0 +1,15 @@
+(ns sfsim.units)
+
+
+(set! *unchecked-math* true)
+(set! *warn-on-reflection* true)
+
+
+(def rankin (/ 5.0 9.0))
+(def foot 0.3048)
+(def gravitation 9.8066500286389)
+(def pound (* 0.45359237 gravitation))
+
+
+(set! *warn-on-reflection* false)
+(set! *unchecked-math* false)
