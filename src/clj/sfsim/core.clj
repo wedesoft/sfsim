81c6e5a818a4d90242c3691054c25e299224b922
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-07-02 19:09:26 +0100

    Clojure now supports passing functions as Java callbacks

--- src/clj/sfsim/core.clj
+++ src/clj/sfsim/core.clj
@@ -26,18 +26,13 @@
                           texture-render-color write-to-stencil-buffer mask-with-stencil-buffer joined-render-vars
                           setup-rendering quad-splits-orientations)]
     [sfsim.image :refer (spit-png)]
-    [sfsim.util :refer (sqr)]
     [sfsim.texture :refer (destroy-texture texture->image)])
   (:import
     (fastmath.vector
       Vec3)
     (org.lwjgl.glfw
       GLFW
-      GLFWVidMode
-      GLFWCharCallbackI
-      GLFWCursorPosCallbackI
-      GLFWKeyCallbackI
-      GLFWMouseButtonCallbackI)
+      GLFWVidMode)
     (org.lwjgl.opengl
       GL11)
     (org.lwjgl.nuklear
@@ -271,25 +266,19 @@
 
 (GLFW/glfwSetCharCallback
   @window
-  (reify GLFWCharCallbackI
-    (invoke
-      [_this _window codepoint]
-      (Nuklear/nk_input_unicode (:sfsim.gui/context gui) codepoint))))
+  (fn [_window codepoint]
+      (Nuklear/nk_input_unicode (:sfsim.gui/context gui) codepoint)))
 
 
 (GLFW/glfwSetCursorPosCallback
   @window
-  (reify GLFWCursorPosCallbackI
-    (invoke
-      [_this _window xpos ypos]
-      (Nuklear/nk_input_motion (:sfsim.gui/context gui) (int xpos) (int ypos)))))
+  (fn [_window xpos ypos]
+      (Nuklear/nk_input_motion (:sfsim.gui/context gui) (int xpos) (int ypos))))
 
 
 (GLFW/glfwSetMouseButtonCallback
   @window
-  (reify GLFWMouseButtonCallbackI
-    (invoke
-      [_this _window button action _mods]
+  (fn [_window button action _mods]
       (let [stack (MemoryStack/stackPush)
             cx    (.mallocDouble stack 1)
             cy    (.mallocDouble stack 1)]
@@ -301,7 +290,7 @@
                          (= button GLFW/GLFW_MOUSE_BUTTON_MIDDLE) Nuklear/NK_BUTTON_MIDDLE
                          :else Nuklear/NK_BUTTON_LEFT)]
           (Nuklear/nk_input_button (:sfsim.gui/context gui) nkbutton x y (= action GLFW/GLFW_PRESS))
-          (MemoryStack/stackPop))))))
+          (MemoryStack/stackPop)))))
 
 
 (def menu (atom nil))
