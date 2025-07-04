194dac1190e495a2d91c5997a2bbd7fcb3ed967a
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-07-04 12:34:44 +0100

    Display joystick button states

--- etc/scratch.clj
+++ etc/scratch.clj
@@ -6,9 +6,13 @@
 (GLFW/glfwShowWindow window)
 
 (while (not (GLFW/glfwWindowShouldClose window))
-        (GLFW/glfwPollEvents)
-       (print "\ropen")
-       (flush))
+       (GLFW/glfwWaitEventsTimeout 0.04)  ; use glfwPollEvents for non-blocking input
+       (let [present (GLFW/glfwJoystickPresent GLFW/GLFW_JOYSTICK_1)
+             buffer  (GLFW/glfwGetJoystickButtons GLFW/GLFW_JOYSTICK_1)
+             buttons (byte-array (.limit buffer))]
+         (.get buffer buttons)
+         (print (format "\rjoystick: %d, buttons: %s" (if present 1 0) (str (seq buttons))))
+         (flush)))
 
 (GLFW/glfwDestroyWindow window)
 
