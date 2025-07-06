be954301b9824a8bc61ce24622e0fa09bad76f72
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-07-06 23:05:50 +0100

    Add license header

--- etc/matperf.clj
+++ etc/matperf.clj
@@ -1,3 +1,9 @@
+;; Copyright (C) 2025 Jan Wedekind <jan@wedesoft.de>
+;; SPDX-License-Identifier: LGPL-3.0-or-later OR EPL-1.0+
+;;
+;; This source code is licensed under the Eclipse Public License v1.0
+;; which you can obtain at https://www.eclipse.org/legal/epl-v10.html
+
 ; Linear algebra performance testing by wedesoft and genmeblog
 (require '[clojure.math :refer (sqrt)])
 (require '[clojure.core.matrix :refer (matrix mget add mul inverse det set-current-implementation dot mmul eseq)])
