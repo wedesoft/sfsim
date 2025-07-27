a706f442d632ec1575853a0bf86eb8f75fd4d150
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-07-27 23:27:44 +0100

    Add configuration directory

--- src/clj/sfsim/config.clj
+++ src/clj/sfsim/config.clj
@@ -13,21 +13,22 @@
 (set! *unchecked-math* :warn-on-boxed)
 (set! *warn-on-reflection* true)
 
+
 (def tmpdir "/tmp")
+(def separator "/")
+(def appdata (str (System/getenv "HOME") separator ".config"))
+
 
 (def config (immuconf/load "resources/config.edn"))
 
+
 (def max-height 35000.0)
-
 (def render-config (immuconf/get config :sfsim.render-config))
-
 (def planet-config (immuconf/get config :sfsim.planet-config))
-
 (def cloud-config (immuconf/get config :sfsim.cloud-config))
-
 (def shadow-config (immuconf/get config :sfsim.shadow-config))
-
 (def object-radius (immuconf/get config :sfsim.object-radius))
+
 
 (set! *warn-on-reflection* false)
 (set! *unchecked-math* false)
