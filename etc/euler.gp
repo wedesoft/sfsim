e2c49b8e6c685c523aaca859c24db717300dd051
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-08-08 00:34:05 +0100

    Plot orbit height variation for different time steps

--- etc/euler.gp
+++
@@ -1,5 +0,0 @@
-set terminal pngcairo size 640,480
-set output "integration.png"
-set xlabel "t"
-set ylabel "h"
-plot "/tmp/euler.dat" using 1:2 with lines title "Euler", "/tmp/rk.dat" using 1:2 with lines title "Runge-Kutta"
--- etc/scratch.clj
+++ etc/scratch.clj
@@ -22,15 +22,22 @@
 (def orbit-radius (+ ^double radius ^double height))
 (def speed (sqrt (/ (* ^double earth-mass ^double g) ^double orbit-radius)))
 (def period (* 2.0 PI (sqrt (/ (cube orbit-radius) (* ^double g ^double earth-mass)))))
-(def dt (/ 1.0 60.0))
-(def n 3)
+; (def dt (/ 1.0 60.0))
+(def dt 10.0)
+(def n 1)
 
 (def sphere (jolt/create-and-add-dynamic-body (jolt/sphere-settings 0.5 1000.0) (vec3 orbit-radius 0 0) (q/->Quaternion 1 0 0 0)))
 (def mass (jolt/get-mass sphere))
 (jolt/set-linear-velocity sphere (vec3 0 speed 0))
 
-(doseq [euler [true false]]
-       (with-open [f (clojure.java.io/writer (if euler "/tmp/euler.dat" "/tmp/rk.dat"))]
+(def euler false)
+
+(java.util.Locale/setDefault java.util.Locale/US)
+
+(doseq [dt [0.01 0.05 0.125 0.25 0.5 1.0 2.0 4.0 8.0 16.0 32.0 64.0]]
+       (with-open [f (clojure.java.io/writer (if euler (format "/tmp/euler%f.dat" dt) (format "/tmp/rk%5.3f.dat" dt)))]
+         (jolt/set-translation sphere (vec3 orbit-radius 0 0))
+         (jolt/set-linear-velocity sphere (vec3 0 speed 0))
          (dotimes [t (long (* n (/ period dt)))]
            (let [state  {:position (jolt/get-translation sphere) :speed (jolt/get-linear-velocity sphere)}
                  state2 (physics/runge-kutta state dt (physics/state-change (physics/gravitation earth-mass))
@@ -45,9 +52,9 @@
              (jolt/update-system dt 1)
              (when (not euler)
                (jolt/add-impulse sphere (mult dv2 mass)))
-             (.write f (format "%f %f\n" (* t dt) (- (mag (jolt/get-translation sphere)) radius)))))))
+             (.write f (format "%f %f\n" (* t dt) (- (mag (jolt/get-translation sphere)) orbit-radius)))))))
 
-(sh "gnuplot" "-c" "etc/euler.gp")
+(sh "gnuplot" "-c" "etc/orbit.gp")
 
 (jolt/jolt-destroy)
 (System/exit 0)
