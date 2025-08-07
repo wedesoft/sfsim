02e99f243ebe109ffef50020b1ccb485fde6da2f
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-08-07 23:31:08 +0100

    Testing Euler and Runge Kutta

--- etc/scratch.clj
+++ etc/scratch.clj
@@ -4,28 +4,50 @@
 ;; This source code is licensed under the Eclipse Public License v1.0
 ;; which you can obtain at https://www.eclipse.org/legal/epl-v10.html
 
-(import '[org.lwjgl.glfw GLFW])
+(require '[clojure.math :refer (sqrt PI)]
+         '[fastmath.vector :refer (vec3 mag add mult sub)]
+         '[sfsim.jolt :as jolt]
+         '[sfsim.util :refer (cube)]
+         '[sfsim.quaternion :as q]
+         '[sfsim.config :as config]
+         '[sfsim.physics :as physics])
+(use '[clojure.java.shell :only [sh]])
 
-(GLFW/glfwInit)
+(jolt/jolt-init)
 
-(def window (GLFW/glfwCreateWindow 320 240 "joystick test" 0 0))
-(GLFW/glfwShowWindow window)
+(def height 408000.0)
+(def earth-mass (config/planet-config :sfsim.planet/mass))
+(def radius (config/planet-config :sfsim.planet/radius))
+(def g physics/gravitational-constant)
+(def orbit-radius (+ ^double radius ^double height))
+(def speed (sqrt (/ (* ^double earth-mass ^double g) ^double orbit-radius)))
+(def period (* 2.0 PI (sqrt (/ (cube orbit-radius) (* ^double g ^double earth-mass)))))
+(def dt (/ 1.0 60.0))
+(def n 3)
 
-(while (not (GLFW/glfwWindowShouldClose window))
-       (GLFW/glfwWaitEventsTimeout 0.04)  ; use glfwPollEvents for non-blocking input
-       (let [present (GLFW/glfwJoystickPresent GLFW/GLFW_JOYSTICK_1)
-             buffer1 (GLFW/glfwGetJoystickButtons GLFW/GLFW_JOYSTICK_1)
-             buttons (byte-array (.limit buffer1))
-             buffer2 (GLFW/glfwGetJoystickAxes GLFW/GLFW_JOYSTICK_1)
-             axes    (float-array (.limit buffer2))
-             buffer3 (GLFW/glfwGetJoystickHats GLFW/GLFW_JOYSTICK_1)
-             hats    (byte-array (.limit buffer3))]
-         (.get buffer1 buttons)
-         (.get buffer2 axes)
-         (.get buffer3 hats)
-         (print (format "\rjoystick: %d, buttons: %s, axes: %s, hats: %s" (if present 1 0) (str (seq buttons)) (str (seq axes)) (str (seq hats))))
-         (flush)))
+(def sphere (jolt/create-and-add-dynamic-body (jolt/sphere-settings 0.5 1000.0) (vec3 orbit-radius 0 0) (q/->Quaternion 1 0 0 0)))
+(def mass (jolt/get-mass sphere))
+(jolt/set-linear-velocity sphere (vec3 0 speed 0))
 
-(GLFW/glfwDestroyWindow window)
+(doseq [euler [true false]]
+       (with-open [f (clojure.java.io/writer (if euler "/tmp/euler.dat" "/tmp/rk.dat"))]
+         (dotimes [t (long (* n (/ period dt)))]
+           (let [state  {:position (jolt/get-translation sphere) :speed (jolt/get-linear-velocity sphere)}
+                 state2 (physics/runge-kutta state dt (physics/state-change (physics/gravitation earth-mass))
+                                             (fn [x y] (merge-with add x y))
+                                             (fn [s x] (into {} (for [[k v] x] [k (mult v s)]))))
+                 [dv1 dv2] (physics/matching-scheme state dt state2 #(mult %2 %1) sub)]
+             (if euler
+               (jolt/set-gravity ((physics/gravitation earth-mass) (jolt/get-translation sphere)))
+               (jolt/set-gravity (vec3 0 0 0)))
+             (when (not euler)
+               (jolt/add-impulse sphere (mult dv1 mass)))
+             (jolt/update-system dt 1)
+             (when (not euler)
+               (jolt/add-impulse sphere (mult dv2 mass)))
+             (.write f (format "%f %f\n" (* t dt) (- (mag (jolt/get-translation sphere)) radius)))))))
 
-(GLFW/glfwTerminate)
+(sh "gnuplot" "-c" "etc/euler.gp")
+
+(jolt/jolt-destroy)
+(System/exit 0)
