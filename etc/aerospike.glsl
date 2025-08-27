6a255626517c71317aa97482d5835d9d090c7dcd
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-08-27 22:47:41 +0100

    Create thicker boundary

--- etc/aerospike.glsl
+++ etc/aerospike.glsl
@@ -6,9 +6,14 @@ uniform float iTime;
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
   float x = fragCoord.x / iResolution.x;
-  float y = fragCoord.y / iResolution.y - 0.5;
-  float s = 0.2 + 0.1 * abs(sin(10 * x));
-  float luminocity = max(0, 1 - abs(y) / s);
-  vec3 color = vec3(luminocity);
+  float y = 2 * fragCoord.y / iResolution.y - 1.0;
+  float s = 0.4 + 0.15 * abs(sin(10 * x));
+  float s2 = s * 0.8;
+  float dy = abs(y) / s;
+  float dx = sqrt(max(0, 1 - dy * dy));
+  float dy2 = abs(y) / s2;
+  float dx2 = sqrt(max(0, 1 - dy2 * dy2));
+  float luminocity = 1.0 * dx - 0.8 * dx2;
+  vec3 color = vec3(0.8, 0.8, 1.0) * luminocity;
   fragColor = vec4(color, 1);
 }
