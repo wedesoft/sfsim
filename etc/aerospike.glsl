16d07bd4c057940cffce0b982205f6e919bf65fa
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-08-29 23:07:51 +0100

    Minor simplifications

--- etc/aerospike.glsl
+++ etc/aerospike.glsl
@@ -44,22 +44,23 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
   vec2 uv = vec2(fragCoord.x / iResolution.x, 2 * fragCoord.y / iResolution.y - 1.0);
   float left = uv.x;
   float t = iTime;
-  uv += .03*perlin(t*.5+left)*(.5+left);
-  uv += .01*perlin(t*7.+left)*(.5+left);
-  uv.y += .01*perlin(t*67.+left)*(.5+left);
-  uv.y += .005*perlin(t*101.+left)*(.5+left);
-  uv.x += .02*(.2-abs(uv.y))*perlin(t*3.);
+  uv.y += .01*perlin(t*67.+left)*(.1+left);
+  uv.y += .005*perlin(t*101.+left)*(.1+left);
   float period = 10;
   float phase = period * uv.x;
   float min_radius = 0.3;
   float bulge = 0.2;
   float radius = min_radius + bulge * abs(sin(phase));
-  float diamond_longitudinal = mod(phase - 0.5 * M_PI, M_PI) - 0.5 * M_PI;
+  float diamond_longitudinal = mod(phase - 0.3 * M_PI, M_PI) - 0.7 * M_PI;
   float inner_radius = radius * 0.9;
   float radial_coord = abs(uv.y);
   float outer_cross_section = sqrt(max(0, radius * radius - radial_coord * radial_coord));
   float inner_cross_section = sqrt(max(0, inner_radius * inner_radius - radial_coord * radial_coord));
-  float luminocity = 2.0 * outer_cross_section - 1.6 * inner_cross_section;
+  vec3 fringe_color = vec3(0.71, 0.53, 0.66);
+  vec3 core_color = vec3(0.58, 0.67, 0.85);
+  float outer_plume_strength = 1 - smoothstep(radius - 0.1, radius, abs(uv.y));
+  float inner_plume_strength = 1 - smoothstep(inner_radius - 0.1, inner_radius, abs(uv.y));
+  vec3 luminocity = fringe_color * outer_cross_section * outer_plume_strength + inner_cross_section * inner_plume_strength * (core_color - fringe_color);
   float diamond_front_length = min_radius / (bulge * period);
   float diamond_back_length = diamond_front_length * 0.7;
   float tail_start = 0.3 * diamond_front_length;
@@ -76,12 +77,11 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
   } else {
     diamond_strength = 1.0;
   };
-  float diamond = 0.0;
-  if (diamond_radius > 0) {
-    diamond_strength = diamond_strength * (1 - smoothstep(diamond_radius - 0.1, diamond_radius, abs(uv.y)));
-    float diamond_cross_section = sqrt(max(0, diamond_radius * diamond_radius - radial_coord * radial_coord));
-    diamond = 1.5 * diamond_cross_section * diamond_strength;
-  };
-  vec3 color = vec3(0.60, 0.62, 0.63) * (1.0 - luminocity - diamond) + vec3(0.77, 0.75, 0.76) * luminocity + vec3(0.90, 0.75, 0.71) * diamond;
+  diamond_strength = diamond_strength * (1 - smoothstep(diamond_radius - 0.1, diamond_radius, abs(uv.y)));
+  float diamond_cross_section = sqrt(max(0, diamond_radius * diamond_radius - radial_coord * radial_coord));
+  float diamond = 1.5 * diamond_cross_section * diamond_strength;
+  vec3 background = 0.4 * vec3(0.39, 0.39, 0.40);
+  vec3 diamond_color = diamond * vec3(0.98, 0.93, 1.0);
+  vec3 color = background + luminocity + diamond_color;
   fragColor = vec4(color, 1);
 }
