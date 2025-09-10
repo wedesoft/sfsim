2b141ecc65305ea0b4b53b7c43c714e1be6600b0
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-09-10 21:44:43 +0100

    Working on plume

--- etc/plume.glsl
+++ etc/plume.glsl
@@ -9,13 +9,23 @@ float parabola(float x)
   return 0.2 / (0.1 + exp(-x * 10));
 }
 
-float bumps(float x)
+float bumps2(float x)
 {
   float transition = 3 * iMouse.y / iResolution.y;
   float omega = 20.0 * pow(0.1, transition);
   float decay = exp(-x * 3);
   float bell = 2 * transition * (1.0 - exp(-x * 2 / (transition + 0.1)));
   return 0.1 + 0.1 * abs(cos(x * omega)) * decay + bell;
+}
+
+float bumps(float x)
+{
+  float slider = iMouse.y / iResolution.y;
+  float pressure = pow(0.001, slider);
+  float limit = 0.1 * sqrt(1.0 / pressure);
+  float derivative = 3;
+  float c = exp(-derivative / limit);
+  return limit - limit * pow(c, x);
 }
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
@@ -26,9 +36,10 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
   bool inside = abs(uv.y) <= radius;
   vec3 color;
   if (inside) {
-    float a = radius * radius;
-    float d = sqrt(radius * radius - uv.y * uv.y);
-    color = 0.1 * vec3(1, 1, 1) * d / a;
+    // float a = radius * radius;
+    // float d = sqrt(radius * radius - uv.y * uv.y);
+    // color = 0.1 * vec3(1, 1, 1) * d / a;
+    color = vec3(1, 0.5, 0.5);
   } else {
     color = vec3(0, 0, 0);
   };
