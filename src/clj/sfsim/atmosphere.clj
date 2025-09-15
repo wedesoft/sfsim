fdda1904a4b8777e45024b02a6637d882398aa33
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-09-15 21:45:00 +0100

    Update Malli schema

--- src/clj/sfsim/atmosphere.clj
+++ src/clj/sfsim/atmosphere.clj
@@ -113,7 +113,7 @@
 
 (defn transmittance
   "Compute transmissiveness of atmosphere between two points x and x0 considering specified scattering effects"
-  {:malli/schema [:function [:=> [:cat atmosphere [:vector scatter] N fvec3 fvec3] fvec3]
+  {:malli/schema [:function [:=> [:cat atmosphere [:or [:tuple scatter] [:tuple scatter scatter]] N fvec3 fvec3] fvec3]
                   [:=> [:cat atmosphere [:vector scatter] N fvec3 fvec3 :boolean] fvec3]]}
   ([planet scatter steps x x0]
    (let [overall-extinction (if (= (count scatter) 1)
