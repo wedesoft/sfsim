745e89c8da459ad3e1dce5576a554edce87dc7e6
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-10-23 21:21:22 +0100

    Update etc/source.clj

--- etc/source.clj
+++ etc/source.clj
@@ -1,6 +1,12 @@
 (require '[clojure.repl :refer (source source-fn)]
          '[sfsim.quaternion :as q]
-         '[clojure.edn :as edn])
+         '[rewrite-clj.zip :as z])
 
 (source q/+)
-(read-string (source-fn 'q/+))
+
+(binding [*ns* (the-ns 'sfsim.quaternion)]
+         (read-string (source-fn '+)))
+(binding [*ns* (the-ns 'sfsim.quaternion)]
+         (read-string "::k"))
+
+(z/of-string (source-fn 'q/+))
