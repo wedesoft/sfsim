7e51d560ac190aca60c9969de69abddaccd9887e
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-10-25 22:12:18 +0100

    Playing around with Numpy

--- etc/torch.clj
+++ etc/torch.clj
@@ -4,7 +4,22 @@
 (require-python '[torch])
 (require-python '[torch.nn :as nn])
 
-(py. torch/cuda is_available)
-(py. torch/cuda device_count)
-(for [i (range (py. torch/cuda device_count))]
-      (py. torch/cuda get_device_name i))
+(println (py. torch/cuda is_available))
+(println (py. torch/cuda device_count))
+(println
+  (for [i (range (py. torch/cuda device_count))]
+       (py. torch/cuda get_device_name i)))
+
+(def a (np/array [[1 2 3] [4 5 6]]))
+(def b (np/array [[1 2.5 3] [4 5 6]]))
+
+(println (py.- a dtype))
+(println (py.- b dtype))
+
+(println (py.- a shape))
+(println (py. a __add__ a))
+
+(require-python '[operator :as op])
+(op/add a a)
+
+(System/exit 0)
