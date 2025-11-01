721c7e364371cac69c54d72d31ef06de60f968c8
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-11-01 23:10:30 +0000

    Align cloud_point and cloud_outer

--- resources/shaders/clouds/cloud-outer.glsl
+++ resources/shaders/clouds/cloud-outer.glsl
@@ -16,12 +16,12 @@ vec4 cloud_outer(vec3 fs_in_direction)
   vec2 atmosphere_intersection = ray_sphere(vec3(0, 0, 0), radius + max_height, origin, direction);
   vec4 cloud_scatter = vec4(0, 0, 0, 1);
   if (atmosphere_intersection.y > 0) {
-    vec4 intersect = ray_shell(vec3(0, 0, 0), radius + cloud_bottom, radius + cloud_top, origin, direction);
+    vec4 intersection = ray_shell(vec3(0, 0, 0), radius + cloud_bottom, radius + cloud_top, origin, direction);
     vec3 start = origin + atmosphere_intersection.x * direction;
-    if (intersect.t > 0)
-      cloud_scatter = sample_cloud(origin, start, direction, intersect.st, cloud_scatter);
-    if (intersect.q > 0)
-      cloud_scatter = sample_cloud(origin, start, direction, intersect.pq, cloud_scatter);
+    if (intersection.t > 0)
+      cloud_scatter = sample_cloud(origin, start, direction, intersection.st, cloud_scatter);
+    if (intersection.q > 0)
+      cloud_scatter = sample_cloud(origin, start, direction, intersection.pq, cloud_scatter);
   };
   return vec4(cloud_scatter.rgb, 1 - cloud_scatter.a);
 }
--- resources/shaders/clouds/cloud-point.glsl
+++ resources/shaders/clouds/cloud-point.glsl
@@ -15,11 +15,17 @@ vec4 clip_shell_intersections(vec4 intersections, float limit);
 vec4 cloud_point(vec3 point)
 {
   vec3 direction = normalize(point - origin);
-  vec2 atmosphere = ray_sphere(vec3(0, 0, 0), radius + max_height, origin, direction);
-  atmosphere.y = min(distance(origin, point) - atmosphere.x, depth);
-  vec4 intersection = ray_shell(vec3(0, 0, 0), radius + cloud_bottom, radius + cloud_top, origin, direction);
-  intersection = clip_shell_intersections(intersection, atmosphere.x + atmosphere.y);
-  vec3 start = origin + atmosphere.x * direction;
-  vec4 cloud_scatter = sample_cloud(origin, start, direction, intersection.st, vec4(0, 0, 0, 1));
+  vec2 atmosphere_intersection = ray_sphere(vec3(0, 0, 0), radius + max_height, origin, direction);
+  atmosphere_intersection.y = min(distance(origin, point) - atmosphere_intersection.x, depth);
+  vec4 cloud_scatter = vec4(0, 0, 0, 1);
+  if (atmosphere_intersection.y > 0) {
+    vec4 intersection = ray_shell(vec3(0, 0, 0), radius + cloud_bottom, radius + cloud_top, origin, direction);
+    intersection = clip_shell_intersections(intersection, atmosphere_intersection.x + atmosphere_intersection.y);
+    vec3 start = origin + atmosphere_intersection.x * direction;
+    if (intersection.t > 0)
+      cloud_scatter = sample_cloud(origin, start, direction, intersection.st, cloud_scatter);
+    if (intersection.q > 0)
+      cloud_scatter = sample_cloud(origin, start, direction, intersection.pq, cloud_scatter);
+  };
   return vec4(cloud_scatter.rgb, 1 - cloud_scatter.a);
 }
