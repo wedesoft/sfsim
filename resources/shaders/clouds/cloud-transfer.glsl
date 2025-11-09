e87b23c81fdb76b5583040ddc758f63487cef724
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-11-09 20:11:49 +0000

    Minor fix

--- resources/shaders/clouds/cloud-transfer.glsl
+++ resources/shaders/clouds/cloud-transfer.glsl
@@ -14,7 +14,7 @@ vec4 cloud_transfer(vec3 start, vec3 point, float scatter_amount, float stepsize
   vec3 light_received = planet_and_cloud_shadows(vec4(point, 1)) * transmittance_outer(point, light_direction) * transmittance_track(start, point) * scatter_amount;
   vec3 in_scatter = ray_scatter_track(light_direction, start, point) * amplification;
   float cloud_transmittance = exp(-density * stepsize);
-  cloud_scatter.rgb += cloud_scatter.a * (1 - cloud_transmittance) * (light_received + in_scatter) * powder(density * stepsize);
+  cloud_scatter.rgb += cloud_scatter.a * (1 - cloud_transmittance) * (light_received * powder(density * stepsize) + in_scatter);
   cloud_scatter.a *= cloud_transmittance;
   return cloud_scatter;
 }
