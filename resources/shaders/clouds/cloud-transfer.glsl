5ad11aa89d21b279650195c8c5d67750b1a94fa6
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-11-09 20:18:54 +0000

    Make cloud_transfer code more readable

--- resources/shaders/clouds/cloud-transfer.glsl
+++ resources/shaders/clouds/cloud-transfer.glsl
@@ -11,10 +11,12 @@ float powder(float d);
 
 vec4 cloud_transfer(vec3 start, vec3 point, float scatter_amount, float stepsize, vec4 cloud_scatter, float density)
 {
-  vec3 light_received = planet_and_cloud_shadows(vec4(point, 1)) * transmittance_outer(point, light_direction) * transmittance_track(start, point) * scatter_amount;
+  vec3 illumination = planet_and_cloud_shadows(vec4(point, 1)) * transmittance_outer(point, light_direction);
+  vec3 cloud_color = illumination * scatter_amount * powder(density * stepsize);
   vec3 in_scatter = ray_scatter_track(light_direction, start, point) * amplification;
+  vec3 transmittance = transmittance_track(start, point);
   float cloud_transmittance = exp(-density * stepsize);
-  cloud_scatter.rgb += cloud_scatter.a * (1 - cloud_transmittance) * (light_received * powder(density * stepsize) + in_scatter);
+  cloud_scatter.rgb += cloud_scatter.a * (1 - cloud_transmittance) * (cloud_color * transmittance + in_scatter);
   cloud_scatter.a *= cloud_transmittance;
   return cloud_scatter;
 }
