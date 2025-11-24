e5430b4e3f19671aeb02ac7091b8c946ee64e7bb
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-11-24 14:38:45 +0000

    Prototype depth-aware upsampling

--- etc/scratch.clj
+++ etc/scratch.clj
@@ -1,6 +1,7 @@
 (require '[sfsim.render :refer :all])
 (require '[sfsim.texture :refer :all])
 (require '[sfsim.image :refer :all])
+(require '[comb.template :as template])
 (require '[fastmath.vector :refer (vec3)])
 (import '[org.lwjgl.glfw GLFW])
 (import '[org.lwjgl.opengl GL GL30])
@@ -9,6 +10,7 @@
 
 (def big 256)
 (def small 32)
+(def rate (quot big small))
 
 (setup-window-hints true true)
 (def window (GLFW/glfwCreateWindow big big "sfsim" 0 0))
@@ -30,12 +32,12 @@ void main()
 (def fragment-source
 "#version 410 core
 layout (location = 0) out vec4 color;
-layout (location = 1) out vec4 vertex;
+layout (location = 1) out float dist;
 in vec3 fs_point;
 void main()
 {
   color = vec4(1.0, 0.0, 0.0, 1.0);
-  vertex = vec4(fs_point, 1.0);
+  dist = distance(fs_point.xyz, vec3(0, 0, 1));
 }")
 
 (def program (make-program :sfsim.render/vertex [vertex-source] :sfsim.render/fragment [fragment-source]))
@@ -46,30 +48,33 @@ void main()
 
 (def depth (make-empty-depth-texture-2d :sfsim.texture/nearest :sfsim.texture/clamp big big))
 (def color (make-empty-texture-2d :sfsim.texture/nearest :sfsim.texture/clamp GL30/GL_RGBA32F big big))
-(def vertex (make-empty-texture-2d :sfsim.texture/nearest :sfsim.texture/clamp GL30/GL_RGBA32F big big))
+(def dist (make-empty-texture-2d :sfsim.texture/nearest :sfsim.texture/clamp GL30/GL_R32F big big))
 
-(framebuffer-render big big :sfsim.render/noculling depth [color vertex]
+(framebuffer-render big big :sfsim.render/noculling depth [color dist]
                     (use-program program)
                     (clear (vec3 0 0 0))
                     (render-triangles vao))
 
 ; write out red triangle
-(spit-png "/tmp/triangle.png" (texture->image color))
+; (spit-png "/tmp/triangle.png" (texture->image color))
 
 (def vertex-depth-source vertex-source)
 (def fragment-depth-source
 "#version 410 core
 layout (location = 0) out vec4 vertex;
+layout (location = 1) out float dist;
 in vec3 fs_point;
 void main()
 {
   vertex = vec4(fs_point, 1.0);
+  dist = distance(fs_point.xyz, vec3(0, 0, 1));
 }")
 
 (def program-depth (make-program :sfsim.render/vertex [vertex-depth-source] :sfsim.render/fragment [fragment-depth-source]))
 (def depth-small (make-empty-depth-texture-2d :sfsim.texture/nearest :sfsim.texture/clamp small small))
 (def vertex-small (make-empty-texture-2d :sfsim.texture/nearest :sfsim.texture/clamp GL30/GL_RGBA32F small small)) ; TODO: use nearest
-(framebuffer-render small small :sfsim.render/noculling depth-small [vertex-small]
+(def dist-small (make-empty-texture-2d :sfsim.texture/nearest :sfsim.texture/clamp GL30/GL_R32F small small)) ; TODO: use nearest
+(framebuffer-render small small :sfsim.render/noculling depth-small [vertex-small dist-small]
                     (use-program program-depth)
                     (clear)
                     (render-triangles vao))
@@ -123,22 +128,55 @@ void main()
                     (use-textures {0 vertex-small})
                     (render-quads vao-full))
 
-(spit-png "/tmp/fog.png" (texture->image fog-small))
+; (spit-png "/tmp/fog.png" (texture->image fog-small))
+
+; 0.25 0.75
+; 0.75 0.25
+
+; 0.125 0.875
+; 0.375 0.625
+; 0.625 0.375
+; 0.875 0.125
 
 (def vertex-final-source vertex-fog-source)
 
 (def fragment-final-source
+(template/eval
 "#version 410 core
 uniform sampler2D color;
 uniform sampler2D fog;
+uniform sampler2D dist;
+uniform sampler2D dist_small;
 in vec2 fs_uv;
 out vec4 fragColor;
 void main()
 {
+  vec2 p = gl_FragCoord.xy;
+  vec2 q = p / <%= rate %>;
+  vec2 q0 = floor(q);
+  vec2 q1 = q0 + vec2(1.0);
+
+  float d00 = texture(dist_small, q0 + vec2(0.5, 0.5)).r;
+  float d01 = texture(dist_small, q0 + vec2(1.5, 0.5)).r;
+  float d10 = texture(dist_small, q0 + vec2(0.5, 1.5)).r;
+  float d11 = texture(dist_small, q0 + vec2(1.5, 1.5)).r;
+  float d = texture(dist, p).r;
+
+  vec4 f00 = texture(fog, q0 + vec2(0.5, 0.5));
+  vec4 f01 = texture(fog, q0 + vec2(1.5, 0.5));
+  vec4 f10 = texture(fog, q0 + vec2(0.5, 1.5));
+  vec4 f11 = texture(fog, q0 + vec2(1.5, 1.5));
+
+  float w00 = abs(d - d00) < 0.1 ? (q1.x - q.x) * (q1.y - q.y) : 0.0;
+  float w01 = abs(d - d01) < 0.1 ? (q.x - q0.x) * (q1.y - q.y) : 0.0;
+  float w10 = abs(d - d10) < 0.1 ? (q1.x - q.x) * (q.y - q0.y) : 0.0;
+  float w11 = abs(d - d11) < 0.1 ? (q.x - q0.x) * (q.y - q0.y) : 0.0;
+
+  vec4 fog_ = (f00 * w00 + f01 * w01 + f10 * w10 + f11 * w11) / (w00 + w01 + w10 + w11);
+
   vec4 color_ = texture(color, fs_uv);
-  vec4 fog_ = texture(fog, fs_uv);
   fragColor = vec4(color_.rgb * (1.0 - fog_.a) + fog_.rgb * fog_.a, 1.0);
-}")
+}" {:rate rate}))
 
 (def program-final (make-program :sfsim.render/vertex [vertex-final-source] :sfsim.render/fragment [fragment-final-source]))
 
@@ -146,7 +184,9 @@ void main()
                  (use-program program-final)
                  (uniform-sampler program-final "color" 0)
                  (uniform-sampler program-final "fog" 1)
-                 (use-textures {0 color 1 fog-small})
+                 (uniform-sampler program-final "dist" 2)
+                 (uniform-sampler program-final "dist_small" 3)
+                 (use-textures {0 color 1 fog-small 2 dist 3 dist-small})
                  (render-quads vao-full))
 
 
@@ -162,7 +202,8 @@ void main()
 (destroy-vertex-array-object vao-full)
 
 (destroy-texture color)
-(destroy-texture vertex)
+(destroy-texture dist)
+(destroy-texture dist-small)
 (destroy-texture vertex-small)
 (destroy-texture fog-small)
 
