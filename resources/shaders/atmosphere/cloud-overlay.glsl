366e0ee5602585f506bf86a02ecf04937f7c10a7
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-12-18 22:32:42 +0000

    Working on improving cloud_overlay function

--- resources/shaders/atmosphere/cloud-overlay.glsl
+++ resources/shaders/atmosphere/cloud-overlay.glsl
@@ -1,8 +1,11 @@
 #version 450 core
 
 uniform sampler2D clouds;
+uniform sampler2D dist;
 uniform int window_width;
 uniform int window_height;
+
+const vec2 vector01 = vec2(0.0, 1.0);
 
 vec4 cloud_overlay(float depth)
 {
