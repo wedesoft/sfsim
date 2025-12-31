de2b596bff90b8f430b6e4da9c8bbbdd6c080e2a
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-12-31 00:02:59 +0000

    Change scaling of noise

--- resources/shaders/plume/rcs-transfer.glsl
+++ resources/shaders/plume/rcs-transfer.glsl
@@ -20,8 +20,8 @@ vec4 rcs_transfer(vec3 point, float rcs_step, vec4 rcs_scatter)
     float end = mix(START, RCS_END, rcs_throttle);
     float fade = clamp((point.x - end) / (START - end), 0.0, 1.0);
     float density = BASE_DENSITY / (radius * radius) * fade;
-    vec3 scale = 100.0 * vec3(0.1, rcs_nozzle / radius, rcs_nozzle / radius);
-    float attenuation = 0.7 + 0.3 * noise3d(point * scale + time * vec3(-SPEED, 0.0, 0.0));
+    vec3 scale = 25.0 * vec3(0.01, rcs_nozzle / radius, rcs_nozzle / radius);
+    float attenuation = 0.7 + 0.3 * noise3d(point * scale + time * vec3(SPEED, 0.0, 0.0));
     float emitting = density * rcs_step * attenuation;
     float rcs_transmittance = exp(-density * rcs_step);
     return vec4(rcs_scatter.rgb + emitting, rcs_scatter.a * rcs_transmittance);
