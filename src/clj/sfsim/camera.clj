a98c61010eb64b5475647e225b24b3b281ecce3a
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-01-28 21:02:44 +0000

    Add camera module

---
+++ src/clj/sfsim/camera.clj
@@ -0,0 +1,16 @@
+;; Copyright (C) 2026 Jan Wedekind <jan@wedesoft.de>
+;; SPDX-License-Identifier: LGPL-3.0-or-later OR EPL-1.0+
+;;
+;; This source code is licensed under the Eclipse Public License v1.0
+;; which you can obtain at https://www.eclipse.org/legal/epl-v10.html
+
+(ns sfsim.camera
+    "Camera movement math")
+
+
+(set! *unchecked-math* :warn-on-boxed)
+(set! *warn-on-reflection* true)
+
+
+(set! *warn-on-reflection* false)
+(set! *unchecked-math* false)
