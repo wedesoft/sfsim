e8a665cd4fff31e4f83e21148ef7e1cfa2e54abf
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-01-28 23:09:55 +0000

    Working on camera module

--- src/clj/sfsim/camera.clj
+++ src/clj/sfsim/camera.clj
@@ -1,15 +1,38 @@
-;; Copyright (C) 2026 Jan Wedekind <jan@wedesoft.de>
-;; SPDX-License-Identifier: LGPL-3.0-or-later OR EPL-1.0+
+;; Copyright (C) 2026 Jan Wedekind <jan@wedesoft.de> SPDX-License-Identifier: LGPL-3.0-or-later OR EPL-1.0+
 ;;
 ;; This source code is licensed under the Eclipse Public License v1.0
 ;; which you can obtain at https://www.eclipse.org/legal/epl-v10.html
 
 (ns sfsim.camera
-    "Camera movement math")
+    "Camera movement math"
+    (:require
+      [fastmath.vector :refer (vec3 mult add)]
+      [sfsim.physics :as physics]))
 
 
 (set! *unchecked-math* :warn-on-boxed)
 (set! *warn-on-reflection* true)
+
+
+(defn make-camera-state
+  []
+  (atom {::distance 60.0
+         ::roll 0.0
+         ::pitch 10.0
+         ::yaw 0.0
+         ::target-roll 0.0
+         ::target-pitch 10.0
+         ::target-yaw 0.0}))
+
+
+(defn get-camera-pose
+  "Get camera pose in surface coordinates"
+  [camera-state physics-state jd-ut]
+  (let [position           (physics/get-position :sfsim.physics/surface jd-ut physics-state)
+        orientation        (physics/get-orientation :sfsim.physics/surface jd-ut physics-state)
+        forward            (vec3 1 0 0)
+        relative-position  (mult forward (- (::distance @camera-state)))]
+    {::position (add position relative-position)}))
 
 
 (set! *warn-on-reflection* false)
--- test/clj/sfsim/t_camera.clj
+++ test/clj/sfsim/t_camera.clj
@@ -6,10 +6,66 @@
 
 (ns sfsim.t-camera
   (:require
+    [clojure.math :refer (cos sin to-radians)]
     [malli.dev.pretty :as pretty]
     [malli.instrument :as mi]
     [midje.sweet :refer :all]
+    [fastmath.vector :refer (vec3 mult sub)]
+    [sfsim.conftest :refer (roughly-vector)]
+    [sfsim.quaternion :as q]
+    [sfsim.astro :as astro]
+    [sfsim.jolt :as jolt]
+    [sfsim.physics :refer (make-physics-state get-position get-orientation set-pose)]
     [sfsim.camera :refer :all]))
+
+
+(mi/collect! {:ns (all-ns)})
+(mi/instrument! {:report (pretty/thrower)})
+
+
+(jolt/jolt-init)
+
+
+(def radius 6378000.0)
+
+(defn position-from-lon-lat ;; TODO: remove this
+  [longitude latitude height]
+  (let [point (vec3 (* (cos longitude) (cos latitude)) (* (sin longitude) (cos latitude)) (sin latitude))]
+    (mult point (+ radius height))))
+
+
+(defn orientation-from-lon-lat ;; TODO: remove this
+  [longitude latitude]
+  (let [radius-vector (position-from-lon-lat longitude latitude 1.0)]
+    (q/vector-to-vector-rotation (vec3 0 0 1) (sub radius-vector))))
+
+
+(def state (make-camera-state))
+(def sphere (jolt/create-and-add-dynamic-body (jolt/sphere-settings 1.0 1000.0) (vec3 0 0 0) (q/->Quaternion 1 0 0 0)))
+(def physics-state (make-physics-state sphere))
+(set-pose :sfsim.physics/surface physics-state (vec3 0 0 (- radius)) (q/->Quaternion 1 0 0 0))
+
+(facts "Camera initialisation"
+       (:sfsim.camera/distance @state) => 60.0
+       (:sfsim.camera/roll @state) => 0.0
+       (:sfsim.camera/pitch @state) => 10.0
+       (:sfsim.camera/yaw @state) => 0.0
+       (:sfsim.camera/target-roll @state) => 0.0
+       (:sfsim.camera/target-pitch @state) => 10.0
+       (:sfsim.camera/target-yaw @state) => 0.0)
+
+(facts "Get camera pose"
+       (swap! state assoc :sfsim.camera/pitch 0.0)
+       (swap! state assoc :sfsim.camera/target-pitch 0.0)
+       (swap! state assoc :sfsim.camera/distance 0.0)
+       (:sfsim.camera/position (get-camera-pose state physics-state astro/T0)) => (roughly-vector (vec3 0 0 (- radius)) 1e-3)
+       (swap! state assoc :sfsim.camera/distance 60.0)
+       (:sfsim.camera/position (get-camera-pose state physics-state astro/T0)) => (roughly-vector (vec3 -60 0 (- radius)) 1e-3)
+       )
+
+
+(jolt/remove-and-destroy-body sphere)
+(jolt/jolt-destroy)
 
 
 (mi/collect! {:ns (all-ns)})
