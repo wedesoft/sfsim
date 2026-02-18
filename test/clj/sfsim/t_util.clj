e40bd55c305e11c8ffcd2c089b6df3d4c1f9a59f
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-02-18 17:51:10 +0000

    Add some more tests for ignore-nil threading macro

--- test/clj/sfsim/t_util.clj
+++ test/clj/sfsim/t_util.clj
@@ -269,4 +269,6 @@
        (ignore-nil-> 42 x nil) => 42
        (ignore-nil-> 42 x (inc (inc x))) => 44
        (ignore-nil-> 42 x (inc x) (and x nil) (inc x)) => 44
-       (let [x 42] (ignore-nil-> x x (inc x))) => 43)
+       (let [x 0] (ignore-nil-> 42 x (inc x))) => 43
+       (let [x 42] (ignore-nil-> x x (inc x))) => 43
+       (let [y 0] (ignore-nil-> 42 y (inc y))) => 43)
