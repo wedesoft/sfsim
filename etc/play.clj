26bc746e0870d00540738d09d743cd6b0f4e7ad1
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-02-22 23:38:45 +0000

    Play Ogg Vorbis audio file

--- etc/play.clj
+++ etc/play.clj
@@ -1,16 +1,73 @@
 ; https://github.com/LWJGL/lwjgl3/blob/master/modules/samples/src/test/java/org/lwjgl/demo/openal/ALCDemo.java
 (ns play
-    (:import (org.lwjgl.stb STBVorbis STBVorbisInfo)))
+    (:import (org.lwjgl.stb STBVorbis STBVorbisInfo)
+             (org.lwjgl BufferUtils)
+             (org.lwjgl.system MemoryUtil)
+             (org.lwjgl.openal ALC10 ALC AL AL10 EXTThreadLocalContext)))
 
-(defn read-ogg-file [file-path]
-  (let [error  (int-array 1)
-        vorbis (STBVorbis/stb_vorbis_open_filename file-path error nil)]
-    (if (zero? vorbis)
-      (println "Failed to open Ogg Vorbis file.")
-      (let [info (STBVorbisInfo/malloc)]
-        (STBVorbis/stb_vorbis_get_info vorbis info)
-        (println "Sample rate:" (.sample_rate info))
-        (println "Channels:" (.channels info))
-        (STBVorbis/stb_vorbis_close vorbis)))))
+(def file-path "etc/audio.ogg")
+(def error  (int-array 1))
+(def vorbis (STBVorbis/stb_vorbis_open_filename file-path error nil))
+; check not zero
+(def info (STBVorbisInfo/malloc))
+(STBVorbis/stb_vorbis_get_info vorbis info)
+(def sample-rate (.sample_rate info))
+(def num-channels (.channels info))
+(def num-samples (STBVorbis/stb_vorbis_stream_length_in_samples vorbis))
 
-(read-ogg-file "etc/audio.ogg")
+(def pcm (BufferUtils/createShortBuffer (* num-samples num-channels)))
+(STBVorbis/stb_vorbis_get_samples_short_interleaved vorbis num-channels pcm)
+
+; convert to integer array
+(def pcm-arr (short-array (* num-channels num-samples)))
+(.get pcm pcm-arr)
+(.flip pcm)
+
+(STBVorbis/stb_vorbis_close vorbis)
+
+
+(def device (ALC10/alcOpenDevice ""))
+
+(def device-caps (ALC/createCapabilities device))
+
+(def default-device (ALC10/alcGetString device ALC10/ALC_DEVICE_SPECIFIER))
+
+(def context (ALC10/alcCreateContext device nil))
+
+(def use-tlc (and (.ALC_EXT_thread_local_context device-caps)
+                  (EXTThreadLocalContext/alcSetThreadContext context)))
+
+(when-not use-tlc
+          (ALC10/alcMakeContextCurrent context))
+
+(def caps (AL/createCapabilities device-caps))
+
+(def buffer (AL10/alGenBuffers))
+
+(def source (AL10/alGenSources))
+
+(AL10/alBufferData buffer (if (= num-channels 1) AL10/AL_FORMAT_MONO16 AL10/AL_FORMAT_STEREO16) pcm sample-rate)
+
+(AL10/alSourcei source AL10/AL_BUFFER buffer)
+
+(AL10/alSourcePlay source)
+
+(while (= (AL10/alGetSourcei source AL10/AL_SOURCE_STATE) AL10/AL_PLAYING)
+       (Thread/sleep 100))
+
+(AL10/alSourceStop source)
+
+(AL10/alDeleteSources source)
+
+(AL10/alDeleteBuffers buffer)
+
+(ALC10/alcMakeContextCurrent 0)
+
+(if use-tlc
+  (AL/setCurrentThread nil)
+  (AL/setCurrentProcess nil))
+
+(MemoryUtil/memFree (.getAddressBuffer caps))
+
+(ALC10/alcDestroyContext context)
+(ALC10/alcCloseDevice device)
