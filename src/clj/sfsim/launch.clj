c6e5afd68f8853b3d129b0429c4639dd608249d5
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-04-27 22:17:49 +0100

    Add gravitation to launch optimization

--- src/clj/sfsim/launch.clj
+++ src/clj/sfsim/launch.clj
@@ -1,12 +1,18 @@
 (ns sfsim.launch
     "Optimize launch trajectory"
     (:require
-      [fastmath.vector :refer (vec3 mult)]
-      [sfsim.physics :refer (geographic->vector)]))
+      [fastmath.vector :refer (vec3 mult add)]
+      [sfsim.physics :refer (geographic->vector gravitation state-add state-scale runge-kutta)]))
 
 
 (set! *unchecked-math* :warn-on-boxed)
 (set! *warn-on-reflection* true)
+
+
+(def config
+  {:radius 6378000.0
+   :mass 5.9742e+24
+   :dt 1.0})
 
 
 (defn setup
@@ -16,6 +22,19 @@
         point  (geographic->vector longitude latitude)]
     {:position (mult point (+ ^double radius ^double height))
      :speed (vec3 0 0 0)}))
+
+
+(defn state-change
+  [acceleration]
+  (fn [{:keys [position speed]} _dt]
+      {:position speed :speed (acceleration position speed)}))
+
+
+(defn update-state
+  "Perform simulation step for spacecraft"
+  [{:keys [position speed] :as state} {:keys []} {:keys [dt mass]}]
+  (let [gravitation (gravitation (vec3 0 0 0) mass)]
+    (runge-kutta state dt (state-change gravitation) state-add state-scale)))
 
 
 (set! *warn-on-reflection* false)
--- test/clj/sfsim/t_launch.clj
+++ test/clj/sfsim/t_launch.clj
@@ -15,7 +15,9 @@
 
 (def test-config
   {:radius 6378000.0
-   :mass 5.9722e+24})
+   :mass 5.9722e+24
+   :dt 1.0
+   })
 
 
 (fact "Launch rocket"
@@ -25,3 +27,16 @@
       (:position (setup test-config :latitude (/ PI 2) :longitude (/ PI 2) :height 0.0)) => (roughly-vector (vec3 0 0 6378000) 1e-6)
       (:position (setup test-config :latitude 0.0 :longitude 0.0 :height 500.0)) => (roughly-vector (vec3 6378500 0 0) 1e-6)
       (:speed (setup test-config :latitude 0.0 :longitude 0.0 :height 0.0)) => (roughly-vector (vec3 0 0 0) 1e-6))
+
+
+(fact "Test gravitation"
+      (:speed (update-state (setup test-config :latitude 0.0 :longitude 0.0 :height 0.0) {} (assoc test-config :dt 0.0)))
+      => (vec3 0.0 0.0 0.0)
+      (:position (update-state (setup test-config :latitude 0.0 :longitude 0.0 :height 0.0) {} (assoc test-config :dt 0.0)))
+      => (vec3 6378000.0 0.0 0.0)
+      (:speed (update-state (setup test-config :latitude 0.0 :longitude 0.0 :height 0.0) {} test-config))
+      => (roughly-vector (vec3 -9.799 0.0 0.0) 1e-3)
+      (:speed (update-state (update-state (setup test-config :latitude 0.0 :longitude 0.0 :height 0.0) {} test-config) {} test-config))
+      => (roughly-vector (vec3 -19.598 0.0 0.0) 1e-3)
+      (:position (update-state (setup test-config :latitude 0.0 :longitude 0.0 :height 0.0) {} test-config))
+      => (roughly-vector (vec3 (- 6378000.0 4.899) 0.0 0.0) 1e-3))
