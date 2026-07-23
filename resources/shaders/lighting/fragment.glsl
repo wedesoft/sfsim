744a8b4dd8c3239129a32b39e4c7c1c72f44e271
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-07-23 23:11:11 +0100

    Create lighting module with shader

---
+++ resources/shaders/lighting/fragment.glsl
@@ -0,0 +1,39 @@
+#version 450 core
+
+uniform sampler2D camera_point;
+uniform sampler2D camera_normal;
+uniform sampler2D diffuse_material;
+uniform sampler2D specular_material;
+uniform sampler2D emissive_material;
+uniform mat4 camera_to_world;
+uniform mat4 camera_to_shadow_map;
+uniform vec3 light_direction;
+uniform int width;
+uniform int height;
+
+out vec4 fragColor;
+
+vec3 overall_shading(vec3 world_point, vec4 object_shadow_pos_1);
+vec3 phong(vec3 ambient, vec3 light, vec3 point, vec3 normal, vec3 color, float reflectivity);
+vec4 attenuation_point(vec3 point, vec4 incoming);
+vec3 surface_radiance_function(vec3 point, vec3 light_direction);
+vec4 cloud_overlay(float depth);
+
+void main()
+{
+  vec2 uv = vec2(gl_FragCoord.x / width, gl_FragCoord.y / height);
+  vec4 point = texture(camera_point, uv);
+  vec4 normal = texture(camera_normal, uv);
+  vec3 diffuse_color = texture(diffuse_material, uv).rgb;
+  if (point.w > 0.0) {
+    vec3 world_point = (camera_to_world * point).xyz;
+    vec4 object_shadow_pos = camera_to_shadow_map * point;
+    vec3 ambient_light = surface_radiance_function(world_point, light_direction);
+    vec3 light = overall_shading(world_point, object_shadow_pos);
+    vec3 incoming = phong(ambient_light, light, world_point, normal.xyz, diffuse_color, 0.0);
+    incoming = attenuation_point(world_point, vec4(incoming, 1.0)).rgb;
+    vec4 cloud_scatter = cloud_overlay(length(point.xyz));
+    fragColor = vec4(incoming.rgb * (1 - cloud_scatter.a) + cloud_scatter.rgb, 1.0);
+  } else
+    fragColor = vec4(0.0, 0.0, 0.0, 1.0);
+}
---
+++ src/clj/sfsim/lighting.clj
@@ -0,0 +1,12 @@
+;; Copyright (C) 2026 Jan Wedekind <jan@wedesoft.de>
+;; SPDX-License-Identifier: LGPL-3.0-or-later OR EPL-1.0+
+;;
+;; This source code is licensed under the Eclipse Public License v1.0
+;; which you can obtain at https://www.eclipse.org/legal/epl-v10.html
+
+(ns sfsim.lighting
+    "Shaders and methods for lighting pass")
+
+
+(def fragment-lighting
+  (slurp "resources/shaders/lighting/fragment.glsl"))
