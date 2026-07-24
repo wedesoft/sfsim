246f1a487c81861696152840add3fe74f13242b4
Jan Wedekind <jan@wedesoft.de>
2026-07-24 21:57:07 +0100

    Working on aligning shaders

--- resources/shaders/lighting/fragment.glsl
+++ resources/shaders/lighting/fragment.glsl
@@ -6,9 +6,13 @@ uniform sampler2D diffuse_material;
 uniform sampler2D specular_material;
 uniform sampler2D emissive_material;
 uniform mat4 camera_to_world;
+uniform vec3 origin;
 <% (doseq [i (range num-scene-shadows)] %>
 uniform mat4 camera_to_shadow_map_<%= (inc ^long i) %>;
 <% ) %>
+uniform float radius;
+uniform float max_height;
+uniform float z_far;
 uniform vec3 light_direction;
 uniform int width;
 uniform int height;
@@ -19,15 +23,17 @@ vec3 overall_shading(vec3 world_point<%= (apply str (map #(str ", vec4 object_sh
 vec3 phong(vec3 ambient, vec3 light, vec3 point, vec3 normal, vec3 color, float reflectivity);
 vec4 attenuation_point(vec3 point, vec4 incoming);
 vec3 surface_radiance_function(vec3 point, vec3 light_direction);
+vec2 ray_sphere(vec3 centre, float radius, vec3 origin, vec3 direction);
+vec3 attenuation_outer(vec3 light_direction, vec3 origin, vec3 direction, float a, vec3 incoming);
 vec4 cloud_overlay(float depth);
 
 void main()
 {
   vec2 uv = vec2(gl_FragCoord.x / width, gl_FragCoord.y / height);
   vec4 point = texture(camera_point, uv);
-  vec4 normal = texture(camera_normal, uv);
-  vec3 diffuse_color = texture(diffuse_material, uv).rgb;
   if (point.w > 0.0) {
+    vec4 normal = texture(camera_normal, uv);
+    vec3 diffuse_color = texture(diffuse_material, uv).rgb;
     vec3 world_point = (camera_to_world * point).xyz;
     vec3 ambient_light = surface_radiance_function(world_point, light_direction);
 <% (doseq [i (range num-scene-shadows)] %>
@@ -40,6 +46,7 @@ void main()
     vec3 incoming = attenuation_point(world_point, vec4(phong + emissive, 1.0)).rgb;
     vec4 cloud_scatter = cloud_overlay(length(point.xyz));
     fragColor = vec4(incoming.rgb * (1 - cloud_scatter.a) + cloud_scatter.rgb, 1.0);
-  } else
+  } else {
     fragColor = vec4(0.0, 0.0, 0.0, 1.0);
+  }
 }
