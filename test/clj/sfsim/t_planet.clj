d43c7c60a6ec866bd043f9856125d9e719452fa9
Jan Wedekind <jan@wedesoft.de>
2026-07-24 13:06:01 +0100

    Use fragment shader from lighting module in some planet rendering tests

--- test/clj/sfsim/t_planet.clj
+++ test/clj/sfsim/t_planet.clj
@@ -21,6 +21,7 @@
     [sfsim.interpolate :refer :all]
     [sfsim.model :refer (make-geometry-buffers destroy-geometry-buffers render-geometry render-lighting)]
     [sfsim.matrix :refer :all :as matrix]
+    [sfsim.lighting :as lighting]
     [sfsim.planet :refer :all :as planet]
     [sfsim.quaternion :as q]
     [sfsim.render :refer :all]
@@ -670,44 +671,6 @@ void main()
 }")
 
 
-(def fragment-lighting-mock
-"#version 450 core
-uniform sampler2D camera_point;
-uniform sampler2D camera_normal;
-uniform sampler2D diffuse_material;
-uniform sampler2D specular_material;
-uniform sampler2D emissive_material;
-uniform mat4 camera_to_world;
-uniform vec3 light_direction;
-uniform int width;
-uniform int height;
-out vec4 fragColor;
-vec3 overall_shading(vec3 world_point);
-vec3 phong(vec3 ambient, vec3 light, vec3 point, vec3 normal, vec3 color, float reflectivity);
-vec4 attenuation_point(vec3 point, vec4 incoming);
-vec3 surface_radiance_function(vec3 point, vec3 light_direction);
-vec4 cloud_overlay(float depth);
-void main()
-{
-  vec2 uv = vec2(gl_FragCoord.x / width, gl_FragCoord.y / height);
-  vec4 point = texture(camera_point, uv);
-  vec4 normal = texture(camera_normal, uv);
-  vec3 diffuse_color = texture(diffuse_material, uv).rgb;
-  if (point.w > 0.0) {
-    vec3 world_point = (camera_to_world * point).xyz;
-    vec3 ambient_light = surface_radiance_function(world_point, light_direction);
-    vec3 light = overall_shading(world_point);
-    float specular = texture(specular_material, uv).r;
-    vec3 phong = phong(ambient_light, light, world_point, (camera_to_world * normal).xyz, diffuse_color, specular);
-    vec3 emissive = texture(emissive_material, uv).rgb;
-    vec3 incoming = attenuation_point(world_point, vec4(phong + emissive, 1.0)).rgb;
-    vec4 cloud_scatter = cloud_overlay(length(point.xyz));
-    fragColor = vec4(incoming.rgb * (1 - cloud_scatter.a) + cloud_scatter.rgb, 1.0);
-  } else
-    fragColor = vec4(0, 0, 0, 1);
-}")
-
-
 (defn make-planet-geometry-textures
   [program colors nx ny nz water]
   (let [day-night     (make-rgb-texture-array :sfsim.texture/linear :sfsim.texture/clamp
@@ -789,7 +752,7 @@ void main()
 (defn make-lighting-program
   []
   (make-program :sfsim.render/vertex [shaders/vertex-passthrough]
-                :sfsim.render/fragment [fragment-lighting-mock shaders/phong shaders/ray-shell
+                :sfsim.render/fragment [(lighting/fragment-lighting 0) shaders/phong shaders/ray-shell
                                         fake-attenuation fake-transmittance fake-ray-scatter planet-and-cloud-shadows-mock
                                         atmosphere/transmittance-point surface-radiance-function cloud-overlay-mock
                                         shaders/is-above-horizon shaders/limit-interval (last atmosphere/attenuation-point)
