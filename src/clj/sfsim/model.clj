c3f97a1d5dfd6523ab23fc7d38b9fb733641aa50
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-08-19 22:42:43 +0100

    Remove unused object_to_scene

--- src/clj/sfsim/model.clj
+++ src/clj/sfsim/model.clj
@@ -645,7 +645,6 @@
   (let [object-to-camera (mulm (inverse camera-to-world) transform)]
     (use-program program)
     (uniform-matrix4 program "object_to_world" transform)
-    (uniform-matrix4 program "object_to_scene" (mulm gltf-to-aerodynamic internal-transform))
     (uniform-matrix4 program "object_to_camera" object-to-camera)
     (doseq [i (range (count scene-shadow-matrices))]
            (uniform-matrix4 program
