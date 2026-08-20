60bd96a84807af837aeac73d01ce993fb904d851
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-08-20 17:49:04 +0100

    Render geometry buffer with square

--- test/clj/sfsim/t_decal.clj
+++ test/clj/sfsim/t_decal.clj
@@ -6,10 +6,14 @@
 
 (ns sfsim.t-decal
   (:require
+    [clojure.math :refer (to-radians)]
     [malli.dev.pretty :as pretty]
     [malli.instrument :as mi]
     [midje.sweet :refer :all]
-    [sfsim.render :refer (with-invisible-window)]
+    [fastmath.vector :refer (vec3)]
+    [fastmath.matrix :refer (inverse mulm eye rotation-matrix-3d-x)]
+    [sfsim.matrix :as matrix]
+    [sfsim.render :refer (with-invisible-window clear)]
     [sfsim.model :as model])
   (:import
     (org.lwjgl.glfw
@@ -26,7 +30,18 @@
         (let [geometry-renderer (model/make-scene-geometry-renderer true)
               plane             (model/read-gltf "test/clj/sfsim/fixtures/decal/plane.glb")
               scene             (model/load-scene-into-opengl (model/geometry-program-selection geometry-renderer) plane)
-              ]
+              translation       (matrix/translation-matrix (vec3 0 0 -4))
+              rotation          (matrix/rotation-matrix (rotation-matrix-3d-x (to-radians 60.0)))
+              camera-to-world   (mulm rotation translation)
+              projection        (matrix/projection-matrix 160 120 0.1 10.0 (to-radians 60))
+              light-direction   (vec3 0 1 0)
+              geometry-buffers  (model/make-geometry-buffers 160 120)]
+          (model/render-geometry geometry-buffers
+                                 (clear)
+                                 (model/render-scene-geometry2 geometry-renderer projection
+                                                               {:sfsim.render/camera-to-world camera-to-world}
+                                                               scene))
+          (model/destroy-geometry-buffers geometry-buffers)
           (model/destroy-scene scene)
           (model/destroy-scene-geometry-renderer geometry-renderer))))
 
