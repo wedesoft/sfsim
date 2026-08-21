09f74149333f37e9116eb7526860a6be5c059502
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-08-21 22:36:12 +0100

    Remove decal test in planet test suite

--- test/clj/sfsim/t_planet.clj
+++ test/clj/sfsim/t_planet.clj
@@ -805,19 +805,6 @@ void main()
 }")
 
 
-(def fragment-grey
-"#version 450 core
-in VS_OUT
-{
-  vec4 camera_point;
-} fs_in;
-out vec4 fragColor;
-void main()
-{
-  fragColor = vec4(0.5, 0.5, 0.5, 1.0);
-}")
-
-
 (def vertex-decal
 "#version 450 core
 uniform mat4 projection;
@@ -830,23 +817,6 @@ void main()
 }")
 
 
-(def fragment-decal
-"#version 450 core
-uniform sampler2D camera_point;
-uniform mat4 camera_to_object;
-out vec4 fragColor;
-void main()
-{
-  vec2 uv = vec2(gl_FragCoord.x / 320, gl_FragCoord.y / 240);
-  vec4 cam_point = texture(camera_point, uv);
-  vec4 point = camera_to_object * cam_point;
-  if (point.w > 0.0 && abs(point.x) <= 0.5 && abs(point.y) <= 0.5) {
-    fragColor = vec4(1, 1, 1, 1);
-  } else
-    discard;
-}")
-
-
 (def decal-indices
   [4 5 7 6    ; front (+z)
    1 0 2 3    ; back  (-z)
@@ -855,57 +825,6 @@ void main()
    2 6 7 3    ; top   (+y)
    0 1 5 4])  ; bottom (-y)
 
-
-(def decal-vertices
-  [-0.5 -0.5 -0.2
-    0.5 -0.5 -0.2
-   -0.5  0.5 -0.2
-    0.5  0.5 -0.2
-   -0.5 -0.5  0.2
-    0.5 -0.5  0.2
-   -0.5  0.5  0.2
-    0.5  0.5  0.2])
-
-
-(fact "Render a decal"
-      (with-invisible-window
-        (let [point-texture    (make-empty-texture-2d :sfsim.texture/nearest :sfsim.texture/clamp GL30/GL_RGBA32F 320 240)
-              indices          [0 1 3 2]
-              vertices         [-1.0 -1.0 0.0, +1.0 -1.0 0.0, -1.0  1.0 0.0, +1.0  1.0 0.0]
-              projection       (projection-matrix 320 240 0.1 10.0 (to-radians -45))
-              object-to-camera (transformation-matrix (rotation-matrix-3d-x (to-radians 60.0)) (vec3 0 0 -4))
-              program-geometry (make-program :sfsim.render/vertex [vertex-plane] :sfsim.render/fragment [fragment-plane])
-              program-plane    (make-program :sfsim.render/vertex [vertex-plane] :sfsim.render/fragment [fragment-grey])
-              plane            (make-vertex-array-object program-geometry indices vertices ["point" 3])]
-          (framebuffer-render 320 240 :sfsim.render/cullback nil [point-texture]
-                              (use-program program-geometry)
-                              (uniform-matrix4 program-geometry "projection" projection)
-                              (uniform-matrix4 program-geometry "object_to_camera" object-to-camera)
-                              (render-quads plane))
-          (let [program-decal (make-program :sfsim.render/vertex [vertex-decal] :sfsim.render/fragment [fragment-decal])
-                cube          (make-vertex-array-object program-decal decal-indices decal-vertices ["point" 3])
-                img           (make-empty-texture-2d :sfsim.texture/nearest :sfsim.texture/clamp GL11/GL_RGB8 320 240)]
-            (framebuffer-render 320 240 :sfsim.render/cullback nil [img]
-                                (clear (vec3 0 0 0))
-                                (use-program program-plane)
-                                (uniform-matrix4 program-plane "projection" projection)
-                                (uniform-matrix4 program-plane "object_to_camera" object-to-camera)
-                                (render-quads plane)
-                                (use-program program-decal)
-                                (uniform-sampler program-decal "camera_point" 0)
-                                (uniform-matrix4 program-decal "projection" projection)
-                                (uniform-matrix4 program-decal "object_to_camera" object-to-camera)
-                                (uniform-matrix4 program-decal "camera_to_object" (inverse object-to-camera))
-                                (use-textures {0 point-texture})
-                                (render-quads cube))
-            (texture->image img) => (is-image "test/clj/sfsim/fixtures/planet/decal.png" 0.5)
-            (destroy-texture img)
-            (destroy-vertex-array-object cube)
-            (destroy-program program-decal)
-            (destroy-vertex-array-object plane)
-            (destroy-program program-geometry)
-            (destroy-program program-plane))
-          (destroy-texture point-texture))))
 
 
 (def fragment-darkgrey
