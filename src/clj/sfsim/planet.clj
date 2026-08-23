293bc85c1b79628cf0a836d2504063f10d4256f7
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-08-23 22:54:30 +0100

    Using asphalt texture for runway

--- src/clj/sfsim/planet.clj
+++ src/clj/sfsim/planet.clj
@@ -301,24 +301,7 @@
 
 
 (def overlay-shader
-"#version 450 core
-
-uniform mat4 camera_to_overlay;
-uniform float overlay_dx;
-uniform float overlay_dy;
-uniform sampler2D diffuse_tex;
-
-vec4 overlay_color(vec4 camera_point)
-{
-  vec4 overlay_point = camera_to_overlay * camera_point;
-  if (overlay_point.x >= 0 && overlay_point.x <= overlay_dx && overlay_point.y >= 0 && overlay_point.y <= overlay_dy) {
-    float scale = 1.0 / overlay_dx;
-    vec2 uv = vec2(overlay_point.x * scale, overlay_point.y * scale);
-    return vec4(texture(diffuse_tex, uv).rgb, 1.0);
-  } else {
-    return vec4(0.0, 0.0, 0.0, 0.0);
-  };
-}")
+  (slurp "resources/shaders/planet/overlay.glsl"))
 
 
 (defn setup-overlay-uniforms
