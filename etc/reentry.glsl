0ed63df2bb973fb31ad9762c8b0208dfdecf2f21
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-08-30 23:49:39 +0100

    Try Billig's model

--- etc/reentry.glsl
+++ etc/reentry.glsl
@@ -16,18 +16,21 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
   vec2 uv = (fragCoord - 0.5 * iResolution.xy) / iResolution.y;
   float aa = fwidth(uv.y);
+  // Billig's model
+  // https://share.gemini.google/kvJVQsbTIRqJ
   float sphereRadius = 0.22;
-  float shockRadius = 0.1;
+  float apex = 0.1;
+  float curvature = iMouse.y / iResolution.y;
+  float machAngle = iMouse.x / iResolution.x;
+  float k = tan(machAngle) * tan(machAngle);
+  float x0 = sphereRadius + apex + curvature;
   float dSphere = sdSphere(uv, sphereRadius);
-  float shockDist = abs(dSphere - shockRadius);
   float layer = 0.025;
   float sphereMask = smoothstep(aa, 0.0, dSphere);
   vec2 surfNorm = getSphereNormal(uv);
-  vec2 windTo = -normalize(iMouse.xy - 0.5 * iResolution.xy);
   float sinT = uv.y / length(uv);
   float cosT = uv.x / length(uv);
-  float parabola = (-cosT + sqrt(4 - 3 * cosT * cosT)) / (2 * sinT * sinT);
-  float shockWave = parabola * (sphereRadius + shockRadius);
+  float shockWave = (x0 * x0 - curvature * curvature) / (x0 * cosT + sqrt(curvature * curvature * cosT * cosT + k * (x0 * x0 - curvature * curvature) * sinT * sinT));
   float shockMask = max(0, layer - abs(shockWave - length(uv))) / layer;
   vec3 col = vec3(clamp(sphereMask + shockMask, 0.0, 1.0));
   fragColor = vec4(col, 1.0);
