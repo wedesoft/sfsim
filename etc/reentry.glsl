792d6e1097b780bf44647ed69b811598fb3cac00
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-08-31 22:22:56 +0100

    Revert back to ring around sphere

--- etc/reentry.glsl
+++ etc/reentry.glsl
@@ -20,18 +20,11 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
   // https://share.gemini.google/kvJVQsbTIRqJ
   float sphereRadius = 0.22;
   float apex = 0.1;
-  float curvature = iMouse.y / iResolution.y;
-  float machAngle = iMouse.x / iResolution.x;
-  float k = tan(machAngle) * tan(machAngle);
-  float x0 = sphereRadius + apex + curvature;
-  float dSphere = sdSphere(uv, sphereRadius);
   float layer = 0.025;
+  float dSphere = sdSphere(uv, sphereRadius);
   float sphereMask = smoothstep(aa, 0.0, dSphere);
-  vec2 surfNorm = getSphereNormal(uv);
-  float sinT = uv.y / length(uv);
-  float cosT = uv.x / length(uv);
-  float shockWave = (x0 * x0 - curvature * curvature) / (x0 * cosT + sqrt(curvature * curvature * cosT * cosT + k * (x0 * x0 - curvature * curvature) * sinT * sinT));
-  float shockMask = max(0, layer - abs(shockWave - length(uv))) / layer;
+  float shockWave = dSphere - apex;
+  float shockMask = max(0, layer - abs(shockWave)) / layer;
   vec3 col = vec3(clamp(sphereMask + shockMask, 0.0, 1.0));
   fragColor = vec4(col, 1.0);
 }
