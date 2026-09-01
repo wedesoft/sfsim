1f7d6019003827ac12863bfaf1aba6b29896042b
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-09-01 23:50:45 +0100

    Hacked virtual wind tunnel

--- etc/windtunnel.clj
+++ etc/windtunnel.clj
@@ -1,4 +1,10 @@
-(require '[sfsim.graphics :as graphics])
+(require '[clojure.math :refer (PI)]
+         '[fastmath.vector :refer (vec3 normalize)]
+         '[sfsim.config :as config]
+         '[sfsim.quaternion :as q]
+         '[sfsim.model :as model]
+         '[sfsim.render :as render]
+         '[sfsim.graphics :as graphics])
 (import '[org.lwjgl.glfw GLFW GLFWCursorPosCallbackI GLFWMouseButtonCallbackI]
         '[org.lwjgl.opengl GL GL11 GL15 GL20 GL30]
         '[org.lwjgl BufferUtils])
@@ -7,8 +13,7 @@
 
 (GLFW/glfwDefaultWindowHints)
 (def width 1280)
-(def height 720)
-(def window (GLFW/glfwCreateWindow width height "Windtunnel" 0 0))
+(def height 720) (def window (GLFW/glfwCreateWindow width height "Windtunnel" 0 0))
 (def mouse-pos (atom [0.0 0.0]))
 (def mouse-button (atom false))
 
@@ -31,15 +36,37 @@
       [_this _window _button action _mods]
       (reset! mouse-button (= action GLFW/GLFW_PRESS)))))
 
-(while (not (GLFW/glfwWindowShouldClose window))
-  ; (GL20/glUniform1f (GL20/glGetUniformLocation program "iTime") (GLFW/glfwGetTime))
-  ; (when @mouse-button
-  ;   (GL20/glUniform2f (GL20/glGetUniformLocation program "iMouse") (@mouse-pos 0) (@mouse-pos 1))
-  ;   (GL20/glUniform1f (GL20/glGetUniformLocation program "pressure") (pow 0.001 (/ (@mouse-pos 1) height))))
-  ; (GL11/glDrawElements GL11/GL_QUADS 4 GL11/GL_UNSIGNED_INT 0)
-  (GLFW/glfwSwapBuffers window)
-  (GLFW/glfwPollEvents))
+(def object-radius (:sfsim.model/object-radius config/model-config))
+(def graphics (graphics/make-graphics2 [{:sfsim.graphics/model-file "data/models/venturestar.glb"
+                                         :sfsim.graphics/object-radius object-radius}]
+                                       []))
 
+
+(while (not (GLFW/glfwWindowShouldClose window))
+       (let [dist        (* 2 6378000)
+             origin      (vec3 dist 0 100)
+             orientation (q/->Quaternion 1 0 0 0)
+             light       (normalize (vec3 1 1 1))
+             yaw         (* 4 PI (/ (@mouse-pos 0) (double width)))
+             pitch       (* PI (- (/ (@mouse-pos 1) (double height)) 0.5))
+             obj-orient  (q/* (q/rotation yaw (vec3 0 1 0)) (q/rotation pitch (vec3 0 0 1)))
+             model-vars  (model/make-model-vars (GLFW/glfwGetTime) 0.0 (:sfsim.physics/throttle 0.0))
+             object      [{:sfsim.graphics/object-position (vec3 dist 0 0)
+                           :sfsim.graphics/object-orientation obj-orient}]
+             frame       (-> (graphics/make-frame graphics width height origin orientation
+                                                  light object model-vars)
+                             (graphics/render-shadows graphics nil)
+                             (graphics/render-scene-shadows graphics)
+                             (graphics/render-cloud-geometry graphics nil)
+                             (graphics/render-clouds graphics [])
+                             (graphics/render-geometry graphics nil))]
+         (render/onscreen-render window
+                                 (render/clear (vec3 0 1 0) 0.0)
+                                 (graphics/render-lighting frame graphics))
+         (graphics/destroy-frame frame)
+         (GLFW/glfwPollEvents)))
+
+(graphics/destroy-graphics2 graphics)
 
 (GLFW/glfwDestroyWindow window)
 
