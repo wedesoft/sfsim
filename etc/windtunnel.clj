d6cc1ad0e1058c08234f97cd7e10dcc064151cb0
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-09-09 12:19:07 +0100

    Scale curvature radius by slope

--- etc/windtunnel.clj
+++ etc/windtunnel.clj
@@ -83,10 +83,11 @@ float curvature(vec4 N)
   vec3 dNdy = dFdy(N.xyz) / h;
   float dw = max(abs(dFdx(N.w)), abs(dFdy(N.w)));
   float k = sqrt(max(dot(dNdx, dNdx), dot(dNdy, dNdy)));
+  float scale_radius = N.z * N.z;
   if (dw > 0.0)
     return 0.01;
   else
-    return 1.0 / max(k, 1.0 / max_radius);
+    return scale_radius / max(k, 1.0 / max_radius);
 }")
 
 
